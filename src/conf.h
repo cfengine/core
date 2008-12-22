49a2b991b812f4ace73e0017e1aacbdb808005cb
Mark Burgess <mark@cfengine.com>
2008-12-22 21:42:19 +0000

    Converging

--- src/conf.h
+++ src/conf.h
@@ -467,7 +467,7 @@
 /* #undef USE_PCRE */
 
 /* Version number of package */
-#define VERSION "3.0.0b8"
+#define VERSION "3.0.0b9"
 
 /* Define if you want to use SELINUX */
 /* #undef WITH_SELINUX */

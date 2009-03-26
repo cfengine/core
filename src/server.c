7e2976aafda4988b6a8d0088a74392b96f273bfb
Mark Burgess <mark@cfengine.com>
2009-03-26 17:08:50 +0000

    Possible protocol error fix

--- src/server.c
+++ src/server.c
@@ -901,7 +901,8 @@ int BusyWithConnection(struct cfd_connection *conn)
   char recvbuffer[CF_BUFSIZE+CF_BUFEXT], sendbuffer[CF_BUFSIZE],check[CF_BUFSIZE];  
   char filename[CF_BUFSIZE],buffer[CF_BUFSIZE],args[CF_BUFSIZE],out[CF_BUFSIZE];
   long time_no_see = 0;
-  int len=0, drift, plainlen, received, encrypted = 0;
+  unsigned int len=0;
+  int drift, plainlen, received, encrypted = 0;
   struct cfd_get_arg get_args;
 
 memset(recvbuffer,0,CF_BUFSIZE+CF_BUFEXT);
@@ -1045,7 +1046,7 @@ switch (GetCommand(recvbuffer))
        
    case cfd_sget:
        memset(buffer,0,CF_BUFSIZE);
-       sscanf(recvbuffer,"SGET %d %d",&len,&(get_args.buf_size));
+       sscanf(recvbuffer,"SGET %u %d",&len,&(get_args.buf_size));
        if (received != len+CF_PROTO_OFFSET)
           {
           CfOut(cf_verbose,"","Protocol error SGET\n");
@@ -1102,7 +1103,7 @@ switch (GetCommand(recvbuffer))
    case cfd_sopendir:
 
        memset(buffer,0,CF_BUFSIZE);
-       sscanf(recvbuffer,"SOPENDIR %d",&len);
+       sscanf(recvbuffer,"SOPENDIR %u",&len);
 
        if (received != len+CF_PROTO_OFFSET)
           {
@@ -1169,7 +1170,7 @@ switch (GetCommand(recvbuffer))
    case cfd_ssynch:
        
        memset(buffer,0,CF_BUFSIZE);
-       sscanf(recvbuffer,"SSYNCH %d",&len);
+       sscanf(recvbuffer,"SSYNCH %u",&len);
 
        if (received != len+CF_PROTO_OFFSET)
           {
@@ -1245,7 +1246,7 @@ switch (GetCommand(recvbuffer))
 
    case cfd_smd5:
 
-       sscanf(recvbuffer,"SMD5 %d",&len);
+       sscanf(recvbuffer,"SMD5 %u",&len);
        
        if (received != len+CF_PROTO_OFFSET)
           {
@@ -1278,7 +1279,7 @@ switch (GetCommand(recvbuffer))
 
    case cfd_svar:
 
-       sscanf(recvbuffer,"SVAR %d",&len);
+       sscanf(recvbuffer,"SVAR %u",&len);
 
        if (received != len+CF_PROTO_OFFSET)
           {

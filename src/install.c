1e63113d7821a3a7ed84050b118cfbde421f21ef
Mark Burgess <mark@cfengine.com>
2009-09-22 05:50:08 +0000

    report promise message change

--- src/install.c
+++ src/install.c
@@ -279,7 +279,7 @@ else
 
 if ((strcmp(spe,"any") == 0) && (strcmp(type->name,"reports") == 0))
    {
-   yyerror("reports promises may not be in class \'any\' - too dangerous");
+   yyerror("reports promises may not be in class \'any\' - risk of a notification explosion");
    }
    
 if (type->promiselist == NULL)

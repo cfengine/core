a90a08e9dcaa0788efa02c916c0cc67795b5c543
Eystein Måløy Stenberg <eystein@cfengine.com>
2010-02-23 08:44:52 +0000

    replaced cf_error with cf_cmdout on non-errors

--- src/generic_agent.c
+++ src/generic_agent.c
@@ -712,9 +712,9 @@ void CloseReports(char *agents)
 
 if (SHOWREPORTS)
    {
-   CfOut(cf_error,"","Wrote compilation report %s%creports%cpromise_output_%s.txt",CFWORKDIR,FILE_SEPARATOR,FILE_SEPARATOR,agents);
-   CfOut(cf_error,"","Wrote compilation report %s%creports%cpromise_output_%s.html",CFWORKDIR,FILE_SEPARATOR,FILE_SEPARATOR,agents);
-   CfOut(cf_error,"","Wrote knowledge map %s%cpromise_knowledge.cf",CFWORKDIR,FILE_SEPARATOR,agents);
+   CfOut(cf_cmdout,"","Wrote compilation report %s%creports%cpromise_output_%s.txt",CFWORKDIR,FILE_SEPARATOR,FILE_SEPARATOR,agents);
+   CfOut(cf_cmdout,"","Wrote compilation report %s%creports%cpromise_output_%s.html",CFWORKDIR,FILE_SEPARATOR,FILE_SEPARATOR,agents);
+   CfOut(cf_cmdout,"","Wrote knowledge map %s%cpromise_knowledge.cf",CFWORKDIR,FILE_SEPARATOR,agents);
    }
 
 fprintf(FKNOW,"}\n");

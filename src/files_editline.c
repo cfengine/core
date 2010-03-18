285b2eed077cb4b97925cadb2b606d3b8ce387b1
Mark Burgess <mark@cfengine.com>
2010-03-18 11:54:30 +0000

    End of section should run to end of file if unmatched

--- src/files_editline.c
+++ src/files_editline.c
@@ -501,13 +501,6 @@ if (beg == CF_UNDEFINED_ITEM && a.region.select_start)
    return false;
    }
 
-if (end == CF_UNDEFINED_ITEM && a.region.select_end)
-   {
-   cfPS(cf_verbose,CF_INTERPT,"",pp,a," !! The promised end pattern (%s) was not found when selecting edit region in %s, abort edit",a.region.select_end,pp->this_server);
-   end = NULL; /* End of file is null ptr if nothing else specified */
-   return false;
-   }
-
 if (end == CF_UNDEFINED_ITEM)
    {
    end = NULL; /* End of file is null ptr if nothing else specified */

a9e7a2ef897b15011bcd2581ddda43571b45b9ba
Mark Burgess <mark@cfengine.com>
2010-07-04 20:14:45 +0000

    Bug 172 - seg fault

--- src/verify_storage.c
+++ src/verify_storage.c
@@ -316,7 +316,7 @@ for (rp = list; rp != NULL; rp=rp->next)
 
       found = true;
       
-      if (strcmp(mp->source,a.mount.mount_source) != 0)
+      if (a.mount.mount_source && strcmp(mp->source,a.mount.mount_source) != 0)
          {
          CfOut(cf_inform,"","A different files system (%s:%s) is mounted on %s than what is promised\n",mp->host,mp->source,name);
          return false;

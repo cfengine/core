0f9098a111d3688f018f906458240034e803402a
Mark Burgess <mark@cfengine.com>
2010-07-28 09:45:23 +0000

    Module integration

--- src/enterprise_stubs.c
+++ src/enterprise_stubs.c
@@ -69,6 +69,16 @@ return false;
 
 /*****************************************************************************/
 
+void EnterpriseModuleTrick()
+
+{
+#ifdef HAVE_LIBCFNOVA
+Nova_EnterpriseModuleTrick();
+#endif
+}
+
+/*****************************************************************************/
+
 void EnterpriseContext()
 
 {

94577f9259ec8b2466e86afd7062a17d05bc9fb1
Mark Burgess <mark@cfengine.com>
2010-10-08 14:02:57 +0000

    Reverse change, changed my mind based on some tests

--- src/agent.c
+++ src/agent.c
@@ -38,8 +38,8 @@
 
 enum typesequence
    {
-   kp_classes,
    kp_vars,
+   kp_classes,
    kp_outputs,
    kp_interfaces,
    kp_files,
@@ -57,8 +57,8 @@ enum typesequence
 
 char *TYPESEQUENCE[] =
    {
-   "classes",    /* Maelstrom order 2 */
    "vars",
+   "classes",    /* Maelstrom order 2 */
    "outputs",
    "interfaces",
    "files",

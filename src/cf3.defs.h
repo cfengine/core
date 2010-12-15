6b5d6871320ffae51115e1583ed996fc0c5c1497
Mark Burgess <mark@cfengine.com>
2010-12-15 13:42:28 +0000

    Class regex patch

--- src/cf3.defs.h
+++ src/cf3.defs.h
@@ -476,11 +476,11 @@ enum cfeditorder
 
 #define CF_MODERANGE   "[0-7augorwxst,+-]+"
 #define CF_BSDFLAGRANGE "[+-]*[(arch|archived|nodump|opaque|sappnd|sappend|schg|schange|simmutable|sunlnk|sunlink|uappnd|uappend|uchg|uchange|uimmutable|uunlnk|uunlink)]+"
-#define CF_CLASSRANGE  "[a-zA-Z0-9_!&@@$|.()]+"
-#define CF_IDRANGE     "[a-zA-Z0-9_$()\\[\\].]+"
+#define CF_CLASSRANGE  "[a-zA-Z0-9_!&@@$|.()\\[\\]{}]+"
+#define CF_IDRANGE     "[a-zA-Z0-9_$(){}\\[\\].]+"
 #define CF_USERRANGE   "[a-zA-Z0-9_$.-]+"
-#define CF_IPRANGE     "[a-zA-Z0-9_$().:-]+"
-#define CF_FNCALLRANGE "[a-zA-Z0-9_().$@]+"
+#define CF_IPRANGE     "[a-zA-Z0-9_$(){}.:-]+"
+#define CF_FNCALLRANGE "[a-zA-Z0-9_(){}.$@]+"
 #define CF_NAKEDLRANGE "@[(][a-zA-Z0-9]+[)]"
 #define CF_ANYSTRING   ".*"
 #define CF_PATHRANGE   "\042?(([a-zA-Z]:\\\\.*)|(/.*))"  // can start with e.g. c:\... or "c:\...  |  unix

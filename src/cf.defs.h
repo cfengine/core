f17f005c181cdc720d8074bcfb84365cfd3a6943
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-03-02 09:40:35 +0000

    Make sure we have standard bool type

--- src/cf.defs.h
+++ src/cf.defs.h
@@ -120,6 +120,22 @@ struct utsname
 #define WTERMSIG(s) ((s) & 0)
 #endif
 
+#ifdef HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# ifndef HAVE__BOOL
+#  ifdef __cplusplus
+typedef bool _Bool;
+#  else
+#   define _Bool signed char
+#  endif
+# endif
+# define bool _Bool
+# define false 0
+# define true 1
+# define __bool_true_false_are_defined 1
+#endif
+
 #include <openssl/err.h>
 #include <openssl/pem.h>
 #include <openssl/evp.h>

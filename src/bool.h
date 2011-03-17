31cf5669ab4dc10458848f5a30149a4b4582868b
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-03-17 13:43:15 +0000

    Move bool definition to separate header

---
+++ src/bool.h
@@ -0,0 +1,24 @@
+#ifndef CF_BOOL_H
+#define CF_BOOL_H
+
+#ifdef HAVE_CONFIG_H
+# include "conf.h"
+#endif
+
+#ifdef HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# ifndef HAVE__BOOL
+#  ifdef __cplusplus
+typedef bool _Bool;
+#  else
+#   define _Bool signed char
+#  endif
+# endif
+# define bool _Bool
+# define false 0
+# define true 1
+# define __bool_true_false_are_defined 1
+#endif
+
+#endif

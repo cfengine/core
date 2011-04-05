263bd00e9ef762a0c77f5248f17dd18710c9e85f
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-04-05 09:32:02 +0000

    Move sort.c and full-write.c from pub/ to src/ as those belong there

--- src/full-write.c
+++ src/full-write.c
@@ -1,19 +1,18 @@
-/* 
-
+/*
    Copyright (C) Cfengine AS
 
    This file is part of Cfengine 3 - written and maintained by Cfengine AS.
- 
+
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
    Free Software Foundation; version 3.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
- 
-  You should have received a copy of the GNU General Public License  
+
+  You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
 
@@ -21,12 +20,13 @@
   versions of Cfengine, the applicable Commerical Open Source License
   (COSL) may apply to this file if you as a licensee so wish it. See
   included file COSL.txt.
-
 */
 
+#ifdef HAVE_CONFIG_H
+# include <conf.h>
+#endif
 
-#include "../src/conf.h"
-#include "../src/cf.defs.h"
+#include "cf.defs.h"
 #include <sys/types.h>
 
 /* Forward declaration copied from prototypes.h to avoid compile-time warning */

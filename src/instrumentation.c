cf5784ca7c2df0ee00fd3cfb8847bf24c179e223
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-04-08 12:56:33 +0000

    Protect whole UpdateLastSeen by cft_server_keyseen lock

--- src/instrumentation.c
+++ src/instrumentation.c
@@ -439,8 +439,6 @@ if (SERVER_KEYSEEN == NULL)
    return;
    }
 
-ThreadUnlock(cft_server_keyseen);
-
 
 if (BooleanControl("control_agent",CFA_CONTROLBODY[cfa_intermittency].lval))
    {
@@ -453,6 +451,7 @@ MapName(name);
 
 if (!OpenDB(name,&dbp))
    {
+   ThreadUnlock(cft_server_keyseen);
    return;
    }
 
@@ -461,6 +460,7 @@ if (!OpenDB(name,&dbp))
 
 if (!NewDBCursor(dbp,&dbcp))
    {
+   ThreadUnlock(cft_server_keyseen);
    CfOut(cf_inform,""," !! Unable to scan class db");
    return;
    }
@@ -477,8 +477,6 @@ while(NextDB(dbp,dbcp,&key,&ksize,&stored,&qsize))
       DeleteDB(dbp,key);
       }
 
-   ThreadLock(cft_server_keyseen);
-
    for (rp = SERVER_KEYSEEN; rp !=  NULL; rp=rp->next)
       {
       kp = (struct CfKeyBinding *) rp->item;
@@ -490,15 +488,11 @@ while(NextDB(dbp,dbcp,&key,&ksize,&stored,&qsize))
          }
       }
 
-   ThreadUnlock(cft_server_keyseen);
-
    }
 
 DeleteDBCursor(dbp,dbcp);
 
 /* Now perform updates with the latest data */
-
-ThreadLock(cft_server_keyseen);
 
 for (rp = SERVER_KEYSEEN; rp !=  NULL; rp=rp->next)
    {

4554fc2905baa22b6f973d3d359b0815bcdb6b1c
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-08-04 07:20:34 +0000

    CFEngine spelling

--- src/logging.c
+++ src/logging.c
@@ -519,7 +519,7 @@ if (s)
    va_start(ap, s);
    vsnprintf(buf, CF_BUFSIZE - 1, s, ap);
    va_end(ap);
-   CfOut(cf_error,"","Fatal cfengine error: %s", buf);
+   CfOut(cf_error,"","Fatal CFEngine error: %s", buf);
    }
 
 if (strlen(CFLOCK) > 0)

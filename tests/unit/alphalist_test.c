c38851e62d737d84779524686877f7ec911d0d37
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-10-08 08:48:45 +0000

    alphalist_test.c links with libpromises, do not stub out functions

--- tests/unit/alphalist_test.c
+++ tests/unit/alphalist_test.c
@@ -6,44 +6,6 @@
 #include "cf3.defs.h"
 #include "cf3.extern.h"
 
-/*
- * FIXME: dependencies we want to cut later
- */
-int FuzzySetMatch(char *s1, char *s2)
-{
-return 0;
-}
-
-void DeleteScope(char *name)
-{
-}
-
-void NewScope(char *name)
-{
-}
-
-void ForceScalar(char *lval, char *rval)
-{
-}
-
-bool IsExcluded(const char *exception)
-{
-return false;
-}
-
-void PromiseRef(enum cfreport level, struct Promise *pp)
-{
-}
-
-enum insert_match String2InsertMatch(char *s)
-{
-return 0;
-}
-
-/*
- * End of deps
- */
-
 static void test_create_destroy(void **state)
 {
 struct AlphaList l;

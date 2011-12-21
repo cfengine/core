d5e17ea19794ce46b43e81d03966b752b3382063
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-12-21 12:31:01 +0000

    Disable gcov for file_writer_test due to link-override conflict

---
+++ tests/unit/gcov-stub.c
@@ -0,0 +1,50 @@
+#include "cf3.defs.h"
+#include "cf3.extern.h"
+
+/*
+ * Stubs which allow CFEngine compiled with gcov support to link against unit
+ * test code which has gcov disabled.
+ */
+
+void __gcov_init(void *p)
+{
+}
+
+void __gcov_merge_add(void *p, unsigned n_counters)
+{
+}
+
+int
+__gcov_execv(const char *path, char *const argv[])
+{
+return execv(path, argv);
+}
+
+int
+__gcov_execl(const char *path, char *arg, ...)
+{
+va_list ap, aq;
+unsigned i, length;
+char **args;
+
+va_start(ap, arg);
+va_copy(aq, ap);
+
+length = 2;
+while (va_arg(ap, char *))
+    length++;
+va_end(ap);
+
+args = (char **)alloca(length * sizeof (void *));
+args[0] = arg;
+for (i = 1; i < length; i++)
+    args[i] = va_arg(aq, char *);
+va_end(aq);
+
+return execv(path, args);
+}
+
+pid_t __gcov_fork(void)
+{
+return fork();
+}

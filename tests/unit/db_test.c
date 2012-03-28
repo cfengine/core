7651fda5803d8c5ff7fd3a8a1fa0eca6660112d4
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2012-03-28 12:52:56 +0200

    Fix compilation of db_test with recent changes in DBM API

--- tests/unit/db_test.c
+++ tests/unit/db_test.c
@@ -96,9 +96,24 @@ void FatalError(char *s, ...)
 
 void CfOut(enum cfreport level, const char *errstr, const char *fmt, ...)
 {
+    fprintf(stderr, "CFOUT<%d>: ", level);
+    if (errstr)
+    {
+        fprintf(stderr, " %s: %s ", errstr, strerror(errno));
+    }
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fprintf(stderr, "\n");
 }
 
 char *MapNameCopy(const char *s)
 {
     return xstrdup(s);
+}
+
+int cf_rename(const char *oldpath, const char *newpath)
+{
+    return rename(oldpath, newpath);
 }

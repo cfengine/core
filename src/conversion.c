cc125d4dfe3492747d3162e8a9d4ba24fe08ae4e
Diego Zamboni <diego@zzamboni.org>
2012-04-17 10:48:02 -0500

    Added support for vbox (libvirt VirtualBox driver)
    Added support for the libvirt VirtualBox driver, identified as "vbox".

--- src/conversion.c
+++ src/conversion.c
@@ -174,7 +174,7 @@ char *EscapeRegex(char *s, char *out, int outSz)
 
 enum cfhypervisors Str2Hypervisors(char *s)
 {
-    static char *names[] = { "xen", "kvm", "esx", "test",
+    static char *names[] = { "xen", "kvm", "esx", "vbox", "test",
         "xen_net", "kvm_net", "esx_net", "test_net",
         "zone", "ec2", "eucalyptus", NULL
     };

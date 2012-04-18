d1f25fec5425ef50e2803adb0ede2ccda110f13b
maciejpatucha <maciej.patucha@cfengine.com>
2012-04-18 11:20:04 +0200

    Fixed SegFault when classes promise is incorrectly declared.
    (Issue 1074)

--- src/attributes.c
+++ src/attributes.c
@@ -224,7 +224,7 @@ Attributes GetDatabaseAttributes(Promise *pp)
 
 Attributes GetClassContextAttributes(Promise *pp)
 {
-    Attributes a;
+    Attributes a = { {0} };;
 
     a.transaction = GetTransactionConstraints(pp);
     a.classes = GetClassDefinitionConstraints(pp);

cfengine/core

commit e9b7c299d8d6e4338609e51e6eb8ef3145bf0f83

move package sanity check to where it belongs

src/verify_packages.c
@@ -154,6 +154,13 @@ static int PackageSanityCheck(Attributes a, Promise *pp)
         return false;
     }
 
+    if (a.packages.package_list_command && !IsExecutable(GetArg0(a.packages.package_list_command)))
+    {
+        CfOut(cf_error, "", "The proposed package list command \"%s\" was not executable",
+              a.packages.package_list_command);
+        return false;
+    }
+
     if (a.packages.package_file_repositories)
     {
         Rlist *rp;
@@ -346,13 +353,6 @@ static int VerifyInstalledPackages(PackageManager **all_mgrs, Attributes a, Prom
         CfOut(cf_verbose, "", " ???????????????????????????????????????????????????????????????\n");
         CfOut(cf_verbose, "", "   Reading package list from %s\n", GetArg0(a.packages.package_list_command));
         CfOut(cf_verbose, "", " ???????????????????????????????????????????????????????????????\n");
-
-        if (!IsExecutable(GetArg0(a.packages.package_list_command)))
-        {
-            CfOut(cf_error, "", "The proposed package list command \"%s\" was not executable",
-                  a.packages.package_list_command);
-            return false;
-        }
 
         if ((prp = cf_popen(a.packages.package_list_command, "r")) == NULL)
         {

commit bd1a90d1d04f0b652feddc6d62a157df3a1a3699

Fail the packaging promise if it does not pass sanity check
Fixes regression caused by commit e9b7c299d8d6e4338609e51e6eb8ef3145bf0f83.

src/verify_packages.c
@@ -93,6 +93,7 @@ void VerifyPackagesPromise(Promise *pp)
 
     if (!PackageSanityCheck(a, pp))
     {
+        cfPS(cf_error, CF_FAIL, "", pp, a, " !! Unable to obtain a list of installed packages - aborting");
         return;
     }
 
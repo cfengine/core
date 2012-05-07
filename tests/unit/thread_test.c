bcf19b08d27a697fa1bfe809edbce5fec2811d46
Eystein Måløy Stenberg <eystein@cfengine.com>
2012-05-07 15:55:36 -0700

    test thread create and locking

--- tests/unit/thread_test.c
+++ tests/unit/thread_test.c
@@ -4,6 +4,14 @@
 #include <setjmp.h>
 #include <cmockery.h>
 
+#define NUM_THREADS 5000
+
+static void create_children(pthread_t tids[NUM_THREADS]);
+static void join_children(pthread_t tids[NUM_THREADS]);
+static void increment_shared_var(void);
+
+int SHARED_VAR;
+pthread_mutex_t shared_var_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void test_init_destroy(void **p)
 {
@@ -60,6 +68,80 @@ void test_trylock_static_errorcheck(void **p)
 }
 
 
+void test_create(void **p)
+{
+    SHARED_VAR = 0;
+
+    pthread_t tid;
+
+    int res_create = pthread_create(&tid, NULL, (void *) increment_shared_var, NULL);
+    assert_int_equal(res_create, 0);
+
+    int res_join = pthread_join(tid, NULL);
+    assert_int_equal(res_join, 0);
+    assert_int_equal(SHARED_VAR, 1);
+}
+
+
+
+
+static void increment_shared_var(void)
+{
+#define THREAD_ITERATIONS 1000
+    
+    int res_lock = pthread_mutex_lock(&shared_var_mutex);
+    assert_int_equal(res_lock, 0);
+
+    for(int i = 0; i < THREAD_ITERATIONS; i++)
+    {
+        SHARED_VAR++;
+        SHARED_VAR--;
+    }
+    
+    SHARED_VAR++;
+
+    int res_unlock = pthread_mutex_unlock(&shared_var_mutex);
+    assert_int_equal(res_unlock, 0);
+}
+
+
+void test_lock(void **p)
+{
+    SHARED_VAR = 0;
+    
+    pthread_t tids[NUM_THREADS];
+    
+    create_children(tids);
+    join_children(tids);
+    
+    assert_int_equal(SHARED_VAR, NUM_THREADS);
+}
+
+
+static void create_children(pthread_t tids[NUM_THREADS])
+{
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setstacksize(&attr, 65536);
+
+    for(int i = 0; i < NUM_THREADS; i++)
+    {
+        int res_create = pthread_create(&(tids[i]), &attr, (void *) increment_shared_var, NULL);
+        assert_int_equal(res_create, 0);
+    }
+}
+
+
+static void join_children(pthread_t tids[NUM_THREADS])
+{
+    for(int i = 0; i < NUM_THREADS; i++)
+    {
+        int res_join = pthread_join(tids[i], NULL);
+        assert_int_equal(res_join, 0);
+    }
+}
+
+
 int main()
 {
     const UnitTest tests[] =
@@ -68,6 +150,8 @@ int main()
             unit_test(test_trylock_dynamic),
             unit_test(test_trylock_static),
             unit_test(test_trylock_static_errorcheck),
+            unit_test(test_create),
+            unit_test(test_lock),
         };
 
     return run_tests(tests);

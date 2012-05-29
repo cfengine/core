ac0166e9718ed8a1e3d9282e7dcf1f8e67bf5f69
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-05-29 15:23:06 +0200

    Add some Policy checks and tests
    Let a policy error be an error of the sanity of policy files, e.g.
    that a promise fails to specify  a required parameter. Let a syntax
    error be an error resulting from parser failure, e.g. a promise
    statement is not terminated by a semicolon. By this definition, the
    parser is currently doing basic policy checking in addtion to
    syntax checking.
    
    This commit introduces policy checks as separate from syntax checks.
    Policy checks may be done on a well-formed parse tree, i.e. a
    struct Policy successfully returned from the parser. The motivation
    for this is to be able to test static policy sanity separate from parsing.

---
+++ tests/unit/policy_test.c
@@ -0,0 +1,44 @@
+#include "test.h"
+#include "policy.h"
+#include "parser.h"
+
+static Sequence *LoadAndCheck(const char *filename)
+{
+    char path[1024];
+    sprintf(path, "data/%s", filename);
+
+    Policy *p = PolicyNew();
+    ParserParseFile(p, path);
+
+    Sequence *errs = SequenceCreate(10, PolicyErrorDestroy);
+    PolicyCheck(p, errs);
+
+    return errs;
+}
+
+static void test_vars_multiple_types(void **state)
+{
+    Sequence *errs = LoadAndCheck("vars_multiple_types.cf");
+    assert_int_equal(1, errs->length);
+
+    SequenceDestroy(errs);
+}
+
+static void test_methods_invalid_arity(void **state)
+{
+    Sequence *errs = LoadAndCheck("methods_invalid_arity.cf");
+    assert_int_equal(1, errs->length);
+
+    SequenceDestroy(errs);
+}
+
+int main()
+{
+    const UnitTest tests[] =
+    {
+        unit_test(test_vars_multiple_types),
+        unit_test(test_methods_invalid_arity),
+    };
+
+    return run_tests(tests);
+}

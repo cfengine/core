2bb270d43d3e4862b1c5e07b2c44cc588bf46778
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2012-08-17 14:49:11 +0200

    Remove unused functions: MinGW does not feature getaddrinfo(3)

--- src/sockaddr.c
+++ src/sockaddr.c
@@ -31,62 +31,6 @@
 /* Note these functions are not thread safe                                  */
 /*****************************************************************************/
 
-#ifdef MINGW
-static const char *inet_ntop(int af, const void *src, char *dst, socklen_t cnt)
-{
-    if (af == AF_INET)
-    {
-        struct sockaddr_in in;
-
-        memset(&in, 0, sizeof(in));
-        in.sin_family = AF_INET;
-        memcpy(&in.sin_addr, src, sizeof(struct in_addr));
-        getnameinfo((struct sockaddr *) &in, sizeof(struct sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST);
-        return dst;
-    }
-    else if (af == AF_INET6)
-    {
-        struct sockaddr_in6 in;
-
-        memset(&in, 0, sizeof(in));
-        in.sin6_family = AF_INET6;
-        memcpy(&in.sin6_addr, src, sizeof(struct in_addr6));
-        getnameinfo((struct sockaddr *) &in, sizeof(struct sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST);
-        return dst;
-    }
-    return NULL;
-}
-
-/*****************************************************************************/
-
-static int inet_pton(int af, const char *src, void *dst)
-{
-    struct addrinfo hints, *res, *ressave;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = af;
-
-    if (getaddrinfo(src, NULL, &hints, &res) != 0)
-    {
-        CfOut(cf_error, "getaddrinfo", "!! Could not resolve host \"%s\"", src);
-        return -1;
-    }
-
-    ressave = res;
-
-    while (res)
-    {
-        memcpy(dst, res->ai_addr, res->ai_addrlen);
-        res = res->ai_next;
-    }
-
-    freeaddrinfo(ressave);
-    return 0;
-}
-#endif /* MINGW */
-
-/*****************************************************************************/
-
 char *sockaddr_ntop(struct sockaddr *sa)
 {
 #if defined(HAVE_GETADDRINFO)

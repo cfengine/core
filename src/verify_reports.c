1b872b51fa24e98d595ccb408be5bf4e9d73a801
root <root@test3.irc.sara.nl.irc.sara.nl>
2012-09-03 09:57:11 +0200

    Forgot to include "vars.h" and fixed a format string warning

--- src/verify_reports.c
+++ src/verify_reports.c
@@ -28,6 +28,7 @@
 #include "dbm_api.h"
 #include "files_names.h"
 #include "item_lib.h"
+#include "vars.h"
 #include "sort.h"
 
 static void ShowState(char *type);
@@ -60,7 +61,7 @@ void VerifyReportPromise(Promise *pp)
         }
         else
         {
-            snprintf(unique_name, CF_BUFSIZE, "last-result", a.report.result);
+            snprintf(unique_name, CF_BUFSIZE, "last-result");
         }
 
         NewScalar(pp->bundle, unique_name, pp->promiser, cf_str);

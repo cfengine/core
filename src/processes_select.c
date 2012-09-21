decd2b71635733057c69af213994ddf042116bc0
Carlos Manuel Duclos Vergara (Senior QA Engineer) <carlos.duclos@cfengine.com>
2012-09-21 13:53:27 +0200

    Removing commented out code, this time without removing the code inside the ifs.

--- src/processes_select.c
+++ src/processes_select.c
@@ -137,10 +137,7 @@ int SelectProcess(char *procentry, char **names, int *start, int *end, Attribute
         PrependAlphaList(&proc_attr, "tty");
     }
 
-    if ((result = EvalProcessResult(a.process_select.process_result, &proc_attr)))
-    {
-    //NewClassesFromString(fp->defines, pp->namespace);
-    }
+    result = EvalProcessResult(a.process_select.process_result, &proc_attr);
    
     DeleteAlphaList(&proc_attr);
 

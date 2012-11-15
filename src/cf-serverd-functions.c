67b51208766e9585416f0dbb44716307b7378b97
P. Christeas <xrg@linux.gr>
2012-11-15 20:55:56 +0200

    server: fix close of "policy_server.dat" after poking it
    In KeepHardClasses() we attempt to fopen("policy_server.dat") just
    to prove it's readable.
    
    We have to close it, too. We are not in C++, guys..
    (cherry picked from commit a4a4442277702fb0b4b05ea3197bfe2b9814752a)

--- src/cf-serverd-functions.c
+++ src/cf-serverd-functions.c
@@ -85,6 +85,7 @@ static void KeepHardClasses()
 
         if (fp != NULL)
         {
+            fclose(fp);
             snprintf(name, sizeof(name), "%s/state/am_policy_hub", CFWORKDIR);
             MapName(name);
 

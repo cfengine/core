439a055950bc0cc9a4fcbfa7cc3714920714cbd0
Mark Burgess <mark@cfengine.com>
2012-12-06 11:25:21 +0100

    Time/hard classes not updated properly in new exec loop (cherry picked from commit b34bbab04819da18c27bd90636ab69d8d0ae9376)

--- src/cf-execd.c
+++ src/cf-execd.c
@@ -737,6 +737,8 @@ static bool ScheduleRun(Policy **policy, ExecConfig *exec_config, const ReportCo
         InitAlphaList(&VHEAP);
         DeleteAlphaList(&VADDCLASSES);
         InitAlphaList(&VADDCLASSES);
+        DeleteAlphaList(&VHARDHEAP);
+        InitAlphaList(&VHARDHEAP);
 
         DeleteItemList(IPADDRESSES);
         IPADDRESSES = NULL;
@@ -767,6 +769,7 @@ static bool ScheduleRun(Policy **policy, ExecConfig *exec_config, const ReportCo
         }
     }
 
+    CfOut(cf_verbose, "", "Nothing to do at %s\n", cf_ctime(&CFSTARTTIME));
     return false;
 }
 

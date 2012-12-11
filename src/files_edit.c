a1f83ec750db361cc63c59613d64556020745e3a
Shauna Thomas <shauna.thomas@cfengine.com>
2012-12-11 15:13:28 +0100

    Improved error message when libxml2 support is not compiled (Issue 1799) (cherry picked from commit 70b8a64f28ed37f0189d548312a0a440aea30bde)

--- src/files_edit.c
+++ src/files_edit.c
@@ -65,7 +65,7 @@ EditContext *NewEditContext(char *filename, Attributes a, Promise *pp)
             return NULL;
         }
 #else
-        cfPS(cf_verbose, CF_INTERPT, "", pp, a, " !! Cannot edit xml files without LIBXML2\n");
+        cfPS(cf_error, CF_FAIL, "", pp, a, " !! Cannot edit XML files without LIBXML2\n");
         free(ec);
         return NULL;
 #endif
@@ -131,7 +131,7 @@ void FinishEditContext(EditContext *ec, Attributes a, Promise *pp, const ReportC
             }
             xmlFreeDoc(ec->xmldoc);
 #else
-            cfPS(cf_verbose, CF_INTERPT, "", pp, a, " !! Cannot edit xml files without LIBXML2\n");
+            cfPS(cf_error, CF_FAIL, "", pp, a, " !! Cannot edit XML files without LIBXML2\n");
 #endif
         }
     }

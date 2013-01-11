14ac00a6956ae521c8add5ba3bb022270f2168a9
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2013-01-11 12:04:04 +0100

    Revert "Remove deprecated functions: ToUpperStr, ToLowerStr"
    The functions are still in use in Nova 3.0.x.
    
    This reverts commit ef1cc7fe6fc54496b00f17a5d95b69039fb8d5ac.

--- src/string_lib.c
+++ src/string_lib.c
@@ -79,12 +79,47 @@ void ToUpperStrInplace(char *str)
 
 /*********************************************************************/
 
+char *ToUpperStr(const char *str)
+{
+    static char buffer[CF_BUFSIZE];
+
+    if (strlen(str) >= CF_BUFSIZE)
+    {
+        FatalError("String too long in ToUpperStr: %s", str);
+    }
+
+    strlcpy(buffer, str, CF_BUFSIZE);
+    ToUpperStrInplace(buffer);
+
+    return buffer;
+}
+
+/*********************************************************************/
+
 void ToLowerStrInplace(char *str)
 {
     for (; *str != '\0'; str++)
     {
         *str = ToLower(*str);
     }
+}
+
+/*********************************************************************/
+
+char *ToLowerStr(const char *str)
+{
+    static char buffer[CF_BUFSIZE];
+
+    if (strlen(str) >= CF_BUFSIZE - 1)
+    {
+        FatalError("String too long in ToLowerStr: %s", str);
+    }
+
+    strlcpy(buffer, str, CF_BUFSIZE);
+
+    ToLowerStrInplace(buffer);
+
+    return buffer;
 }
 
 /*********************************************************************/

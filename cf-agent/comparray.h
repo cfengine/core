aec0ddb809fe896326bae765434f6f6d627a97a2
Mikhail Gusarov <dottedmag@dottedmag.net>
2013-01-15 15:19:10 +0100

    Move comparray from libpromises to cf-agent
    (Related: #1973)

--- cf-agent/comparray.h
+++ cf-agent/comparray.h
@@ -3,6 +3,13 @@
 
 #include "cf3.defs.h"
 
+struct CompressedArray_
+{
+    int key;
+    char *value;
+    CompressedArray *next;
+};
+
 int FixCompressedArrayValue(int i, char *value, CompressedArray **start);
 void DeleteCompressedArray(CompressedArray *start);
 int CompressedArrayElementExists(CompressedArray *start, int key);

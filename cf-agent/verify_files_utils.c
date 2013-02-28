fd14a381123d2de9e7f40e75e980d5cc07e0acde
Mikhail Gusarov <dottedmag@dottedmag.net>
2013-02-28 09:41:57 +0100

    Update for OS X build after EvalContext introduction

--- cf-agent/verify_files_utils.c
+++ cf-agent/verify_files_utils.c
@@ -93,7 +93,7 @@ static void VerifySetUidGid(EvalContext *ctx, char *file, struct stat *dstat, mo
 static int VerifyOwner(EvalContext *ctx, char *file, Promise *pp, Attributes attr, struct stat *sb);
 #endif
 #ifdef __APPLE__
-static int VerifyFinderType(char *file, struct stat *statbuf, Attributes a, Promise *pp);
+static int VerifyFinderType(EvalContext *ctx, char *file, struct stat *statbuf, Attributes a, Promise *pp);
 #endif
 static void VerifyFileChanges(EvalContext *ctx, char *file, struct stat *sb, Attributes attr, Promise *pp);
 static void VerifyFileIntegrity(EvalContext *ctx, char *file, Attributes attr, Promise *pp);
@@ -1912,7 +1912,7 @@ void VerifyFileAttributes(EvalContext *ctx, char *file, struct stat *dstat, Attr
     VerifySetUidGid(ctx, file, dstat, newperm, pp, attr);
 
 # ifdef __APPLE__
-    if (VerifyFinderType(file, dstat, attr, pp))
+    if (VerifyFinderType(ctx, file, dstat, attr, pp))
     {
         /* nop */
     }
@@ -2811,7 +2811,7 @@ static void VerifySetUidGid(EvalContext *ctx, char *file, struct stat *dstat, mo
 
 #ifdef __APPLE__
 
-static int VerifyFinderType(char *file, struct stat *statbuf, Attributes a, Promise *pp)
+static int VerifyFinderType(EvalContext *ctx, char *file, struct stat *statbuf, Attributes a, Promise *pp)
 {                               /* Code modeled after hfstar's extract.c */
     typedef struct
     {

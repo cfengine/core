8933adff824dbaabeef54cca94f101083c0be112
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-03-12 17:52:50 +0100

    Add unit tests for extracting scalar, list variables

--- tests/unit/expand_test.c
+++ tests/unit/expand_test.c
@@ -2,6 +2,7 @@
 
 #include "expand.h"
 #include "rlist.h"
+#include "scope.h"
 
 void test_map_iterators_from_rval_empty(void **state)
 {
@@ -21,13 +22,62 @@ void test_map_iterators_from_rval_literal(void **state)
     assert_int_equal(0, RlistLen(lists));
 }
 
-void test_map_iterators_from_rval_naked_var_nonresolvable(void **state)
+void test_map_iterators_from_rval_naked_scalar_var_nonresolvable(void **state)
 {
     Rlist *scalars = NULL, *lists = NULL;
     MapIteratorsFromRval("scope", &scalars, &lists, (Rval) { "${snookie}", RVAL_TYPE_SCALAR });
 
     assert_int_equal(0, RlistLen(scalars));
     assert_int_equal(0, RlistLen(lists));
+}
+
+void test_map_iterators_from_rval_naked_scalar_var(void **state)
+{
+    ScopeDeleteAll();
+    ScopeNew("scope");
+    ScopeSetCurrent("scope");
+    ScopeAddVariableHash("scope", "snookie", (Rval) { "jersey", RVAL_TYPE_SCALAR }, DATA_TYPE_STRING, NULL, 0);
+
+    Rlist *scalars = NULL, *lists = NULL;
+    MapIteratorsFromRval("scope", &scalars, &lists, (Rval) { "${snookie}", RVAL_TYPE_SCALAR });
+
+    assert_int_equal(1, RlistLen(scalars));
+    assert_string_equal("snookie", scalars->item);
+    assert_int_equal(0, RlistLen(lists));
+}
+
+void test_map_iterators_from_rval_scalar_var(void **state)
+{
+    ScopeDeleteAll();
+    ScopeNew("scope");
+    ScopeSetCurrent("scope");
+    ScopeAddVariableHash("scope", "snookie", (Rval) { "jersey", RVAL_TYPE_SCALAR }, DATA_TYPE_STRING, NULL, 0);
+
+    Rlist *scalars = NULL, *lists = NULL;
+    MapIteratorsFromRval("scope", &scalars, &lists, (Rval) { "abc${snookie} def", RVAL_TYPE_SCALAR });
+
+    assert_int_equal(1, RlistLen(scalars));
+    assert_string_equal("snookie", scalars->item);
+    assert_int_equal(0, RlistLen(lists));
+}
+
+void test_map_iterators_from_rval_naked_list_var(void **state)
+{
+    ScopeDeleteAll();
+    ScopeNew("scope");
+    ScopeSetCurrent("scope");
+
+    Rlist *list = NULL;
+    RlistAppend(&list, "jersey", RVAL_TYPE_SCALAR);
+
+    ScopeAddVariableHash("scope", "jwow", (Rval) { list, RVAL_TYPE_LIST }, DATA_TYPE_STRING_LIST, NULL, 0);
+
+    Rlist *scalars = NULL, *lists = NULL;
+    MapIteratorsFromRval("scope", &scalars, &lists, (Rval) { "${jwow}", RVAL_TYPE_SCALAR });
+
+    assert_int_equal(0, RlistLen(scalars));
+    assert_int_equal(1, RlistLen(lists));
+    assert_string_equal("jwow", lists->item);
 }
 
 int main()
@@ -37,7 +87,10 @@ int main()
     {
         unit_test(test_map_iterators_from_rval_empty),
         unit_test(test_map_iterators_from_rval_literal),
-        unit_test(test_map_iterators_from_rval_naked_var_nonresolvable)
+        unit_test(test_map_iterators_from_rval_naked_scalar_var_nonresolvable),
+        unit_test(test_map_iterators_from_rval_naked_scalar_var),
+        unit_test(test_map_iterators_from_rval_scalar_var),
+        unit_test(test_map_iterators_from_rval_naked_list_var),
     };
 
     return run_tests(tests);

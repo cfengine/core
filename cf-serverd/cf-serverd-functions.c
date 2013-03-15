4ebc6bdeb57b1bdf5b2e6ada65ab3d4814a7c352
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-03-15 18:02:22 +0100

    Do not clear nonexisting stack frame on serverd policy reload

--- cf-serverd/cf-serverd-functions.c
+++ cf-serverd/cf-serverd-functions.c
@@ -586,7 +586,6 @@ void CheckFileChanges(EvalContext *ctx, Policy **policy, GenericAgentConfig *con
             /* Free & reload -- lock this to avoid access errors during reload */
             
             EvalContextHeapClear(ctx);
-            EvalContextStackFrameClear(ctx);
 
             DeleteItemList(IPADDRESSES);
             IPADDRESSES = NULL;

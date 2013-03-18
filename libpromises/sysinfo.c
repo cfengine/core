ce5208404e208058aac881147ef05ce2d0c3d673
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-03-18 15:40:22 +0100

    Remove useless call to ScopeDeleteVariable

--- libpromises/sysinfo.c
+++ libpromises/sysinfo.c
@@ -708,7 +708,6 @@ void Get3Environment(EvalContext *ctx, AgentType agent_type)
            
             CfDebug(" -> Setting new monitoring list %s => %s", name, value);
             list = RlistParseShown(value);
-            ScopeDeleteVariable("mon", name);
             ScopeNewList("mon", name, list, DATA_TYPE_STRING_LIST);
 
             RlistDestroy(list);

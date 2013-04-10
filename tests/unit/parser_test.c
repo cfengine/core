e214eb6f213b4026c271be5bbcdaad1f52b4369f
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-04-10 13:22:03 +0200

    Add parser unit test for invalid ifvarclass expressions

--- tests/unit/parser_test.c
+++ tests/unit/parser_test.c
@@ -34,6 +34,12 @@ void test_constraint_comment_nonscalar(void **state)
     assert_false(p);
 }
 
+void test_constraint_ifvarclass_invalid(void **state)
+{
+    Policy *p = LoadPolicy("constraint_ifvarclass_invalid.cf");
+    assert_false(p);
+}
+
 void test_bundle_args_invalid_type(void **state)
 {
     assert_false(LoadPolicy("bundle_args_invalid_type.cf"));
@@ -55,6 +61,8 @@ int main()
         unit_test(test_body_invalid_type),
         unit_test(test_bundle_args_invalid_type),
         unit_test(test_bundle_args_forgot_cp),
+
+        unit_test(test_constraint_ifvarclass_invalid)
     };
 
     return run_tests(tests);

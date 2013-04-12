510d5f8af17b562982da9ea879c47bda3de17a0c
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-04-12 19:38:08 +0200

    Added more unit tests for GenericAgentResolveInputPath

--- tests/unit/generic_agent_test.c
+++ tests/unit/generic_agent_test.c
@@ -24,24 +24,31 @@ void test_resolve_absolute_input_path(void **state)
     assert_string_equal("/abs/aux.cf", GenericAgentResolveInputPath("/abs/aux.cf", NULL));
 }
 
-void test_resolve_input_path(void **state)
+void test_resolve_non_anchored_input_path(void **state)
 {
     MINUSF = false;
 
     assert_string_equal("/workdir/inputs/aux.cf", GenericAgentResolveInputPath("aux.cf", NULL));
-    assert_string_equal("./aux.cf", GenericAgentResolveInputPath("./aux.cf", NULL));
-}
 
-void test_resolve_input_path_specified(void **state)
-{
     MINUSF = true;
 
     assert_string_equal("/workdir/inputs/promises.cf", GenericAgentResolveInputPath("promises.cf", "promises.cf"));
     assert_string_equal("/workdir/inputs/aux.cf", GenericAgentResolveInputPath("aux.cf", "promises.cf"));
+    assert_string_equal("./rel/aux.cf", GenericAgentResolveInputPath("aux.cf", "./rel/promises.cf"));
+
+    assert_string_equal("/workdir/inputs/a/b.cf", GenericAgentResolveInputPath("a/b.cf", "a/b.cf"));
+    assert_string_equal("/workdir/inputs/a/a/b.cf", GenericAgentResolveInputPath("a/b.cf", "/workdir/inputs/a/b.cf"));
+}
+
+void test_resolve_relative_input_path(void **state)
+{
+    MINUSF = false;
     assert_string_equal("./aux.cf", GenericAgentResolveInputPath("./aux.cf", "promises.cf"));
 
+    MINUSF = true;
+
+    assert_string_equal("./aux.cf", GenericAgentResolveInputPath("./aux.cf", "promises.cf"));
     assert_string_equal("./rel/promises.cf", GenericAgentResolveInputPath("./rel/promises.cf", "./rel/promises.cf"));
-    assert_string_equal("./rel/aux.cf", GenericAgentResolveInputPath("aux.cf", "./rel/promises.cf"));
     assert_string_equal("./rel/./rel/aux.cf", GenericAgentResolveInputPath("./rel/aux.cf", "./rel/promises.cf"));
 }
 
@@ -54,8 +61,8 @@ int main()
     {
         unit_test(test_load_masterfiles),
         unit_test(test_resolve_absolute_input_path),
-        unit_test(test_resolve_input_path),
-        unit_test(test_resolve_input_path_specified)
+        unit_test(test_resolve_non_anchored_input_path),
+        unit_test(test_resolve_relative_input_path),
     };
 
     return run_tests(tests);

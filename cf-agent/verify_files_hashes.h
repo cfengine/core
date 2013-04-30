1c3a3a4559abbc14c7bfd56a75d87bff1165b8ca
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2013-04-30 09:04:59 +0200

    Whitespace and missing copyright blurbs

--- cf-agent/verify_files_hashes.h
+++ cf-agent/verify_files_hashes.h
@@ -20,7 +20,6 @@
   versions of Cfengine, the applicable Commerical Open Source License
   (COSL) may apply to this file if you as a licensee so wish it. See
   included file COSL.txt.
-
 */
 
 #include "cf3.defs.h"

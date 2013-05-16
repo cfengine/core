6a999343d40b4a4893a9e30ec2d8d9485b023fff
maciejmrowiec <maciej.mrowiec@cfengine.com>
2013-05-16 12:18:17 +0200

    fix: extra semicolon after if statement

--- cf-agent/cf-agent.c
+++ cf-agent/cf-agent.c
@@ -361,7 +361,7 @@ static GenericAgentConfig *CheckOpts(EvalContext *ctx, int argc, char **argv)
                     }
 
                     int err = AutomaticBootstrap(config);
-                    if (err < 0);
+                    if (err < 0)
                     {
                         Log(LOG_LEVEL_ERR, "Automatic bootstrap failed, error code '%d'", err);
                         exit(EXIT_FAILURE);

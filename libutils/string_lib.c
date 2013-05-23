75423d63c11741f3df65a34db0be2abfb52e59be
jeffali <hichame.jeffali@cfengine.com>
2013-05-23 07:09:49 -0400

    The two strings at absolute address 0 are not equal...

--- libutils/string_lib.c
+++ libutils/string_lib.c
@@ -125,14 +125,14 @@ int StringSafeCompare(const char *a, const char *b)
 
 bool StringSafeEqual(const char *a, const char *b)
 {
-    if (a == b)
-    {
-        return true;
-    }
-
     if ((a == NULL) || (b == NULL))
     {
         return false;
+    }
+
+    if (a == b)
+    {
+        return true;
     }
 
     return strcmp(a, b) == 0;

1e71f695d596f2a73a2bdad714fc918f7772c06b
Kristian Amlie <kristian.amlie@cfengine.com>
2013-05-27 15:24:06 +0200

    Backport fixes to race conditions and file permissions in file editing.
    Previously the original was removed and then the edited file was
    renamed, leaving a small timeframe where the file did not exist, which
    can lead to bad situations. Fixed by leaving the file in place until
    rename is called, which is atomic on all POSIX platforms.
    
    Also make sure all permissions, including ACLs and SELinux security
    contexts, are preserved when making a copy of the original file.
    
    Fixes issue 2545.
    
    Conflicts:
    	cf-agent/files_operators.c
    	cf-agent/files_operators.h
    	src/acl.h
    
    (cherry picked from commit a3f1a964ad5e079f8abc419bb9e0489aa8c62f7a)
    (cherry picked from commit 908fb187191b41df44bdfb7d092a429cded06436)
    (cherry picked from commit 76d895609ea7dee55502139acf52b1b141ca9b10)

--- src/acl_posix.c
+++ src/acl_posix.c
@@ -1094,6 +1094,68 @@ static int ParseModePosixLinux(char *mode, acl_permset_t perms)
     return true;
 }
 
+int CopyACLs(const char *src, const char *dst)
+{
+    acl_t acls;
+    struct stat statbuf;
+    int ret;
+
+    acls = acl_get_file(src, ACL_TYPE_ACCESS);
+    if (!acls)
+    {
+        if (errno == ENOTSUP)
+        {
+            return true;
+        }
+        else
+        {
+            Log(LOG_LEVEL_INFO, "Can't copy ACLs from '%s'. (acl_get_file: %s)", src, GetErrorStr());
+            return false;
+        }
+    }
+    ret = acl_set_file(dst, ACL_TYPE_ACCESS, acls);
+    acl_free(acls);
+    if (ret != 0)
+    {
+        if (errno == ENOTSUP)
+        {
+            return true;
+        }
+        else
+        {
+            Log(LOG_LEVEL_INFO, "Can't copy ACLs to '%s'. (acl_set_file: %s)", dst, GetErrorStr());
+            return false;
+        }
+    }
+
+    if (stat(src, &statbuf) != 0)
+    {
+        Log(LOG_LEVEL_INFO, "Can't copy ACLs from '%s'. (stat: %s)", src, GetErrorStr());
+        return false;
+    }
+    if (!S_ISDIR(statbuf.st_mode))
+    {
+        return true;
+    }
+
+    // For directory, copy default ACL too.
+    acls = acl_get_file(src, ACL_TYPE_DEFAULT);
+    if (!acls)
+    {
+        Log(LOG_LEVEL_INFO, "Can't copy ACLs from '%s'. (acl_get_file: %s)", src, GetErrorStr());
+        return false;
+    }
+    ret = acl_set_file(dst, ACL_TYPE_DEFAULT, acls);
+    acl_free(acls);
+    if (ret != 0)
+    {
+        Log(LOG_LEVEL_INFO, "Can't copy ACLs to '%s'. (acl_set_file: %s)", dst, GetErrorStr());
+        return false;
+    }
+
+    return true;
+}
+
 #else /* HAVE_LIBACL */
 
 int CheckPosixLinuxACL(char *file_path, Acl acl, Attributes a, Promise *pp)
@@ -1101,6 +1163,11 @@ int CheckPosixLinuxACL(char *file_path, Acl acl, Attributes a, Promise *pp)
     cfPS(cf_error, CF_FAIL, "", pp, a,
          "!! Posix ACLs are not supported on this Linux system - install the Posix acl library");
     PromiseRef(cf_error, pp);
+    return true;
+}
+
+int CopyACLs(const char *src, const char *dst)
+{
     return true;
 }
 

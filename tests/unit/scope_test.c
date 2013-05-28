4defc519a1d22d7c4f3461b35bbaf1b9d9e3f59c
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-05-28 14:34:04 -0700

    Ensure scope 'this' is properly restored, add unit test.
    Close #2632

--- tests/unit/scope_test.c
+++ tests/unit/scope_test.c
@@ -1,5 +1,7 @@
 #include "test.h"
 #include "scope.h"
+#include "env_context.h"
+#include "rlist.h"
 
 static void test_name_join(void)
 {
@@ -35,12 +37,52 @@ static void test_name_split(void)
     }
 }
 
+static void test_push_pop_this(void)
+{
+    VarRef lval = VarRefParse("this.lval");
+    Rval rval;
+
+    EvalContext *ctx = EvalContextNew();
+
+    ScopeNewSpecial(ctx, "this", "lval", "rval1", DATA_TYPE_STRING);
+    assert_true(EvalContextVariableGet(ctx, lval, &rval, NULL));
+    assert_string_equal("rval1", RvalScalarValue(rval));
+    {
+        ScopePushThis();
+
+        assert_false(EvalContextVariableGet(ctx, lval, &rval, NULL));
+
+        ScopeNewSpecial(ctx, "this", "lval", "rval2", DATA_TYPE_STRING);
+        assert_true(EvalContextVariableGet(ctx, lval, &rval, NULL));
+        assert_string_equal("rval2", RvalScalarValue(rval));
+        {
+            ScopePushThis();
+
+            assert_false(EvalContextVariableGet(ctx, lval, &rval, NULL));
+
+            ScopeNewSpecial(ctx, "this", "lval", "rval3", DATA_TYPE_STRING);
+            assert_true(EvalContextVariableGet(ctx, lval, &rval, NULL));
+            assert_string_equal("rval3", RvalScalarValue(rval));
+            {
+
+            }
+
+            ScopePopThis();
+        }
+
+        ScopePopThis();
+    }
+    assert_true(EvalContextVariableGet(ctx, lval, &rval, NULL));
+    assert_string_equal("rval1", RvalScalarValue(rval));
+}
+
 int main()
 {
     const UnitTest tests[] =
 {
         unit_test(test_name_split),
         unit_test(test_name_join),
+        unit_test(test_push_pop_this),
     };
 
     PRINT_TEST_BANNER();

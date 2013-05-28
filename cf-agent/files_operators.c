76d895609ea7dee55502139acf52b1b141ca9b10
Kristian Amlie <kristian.amlie@cfengine.com>
2013-05-28 08:59:35 +0200

    Remove redundant SELinux code.
    This is covered in the CopyFilePermissionsDisk() function, and should
    have been part of commit a3f1a964ad5e079f8a.
    
    Addition to issue 2545.

--- cf-agent/files_operators.c
+++ cf-agent/files_operators.c
@@ -144,19 +144,6 @@ int SaveAsFile(SaveCallbackFn callback, void *param, const char *file, Attribute
     char stamp[CF_BUFSIZE];
     time_t stamp_now;
 
-#ifdef WITH_SELINUX
-    int selinux_enabled = 0;
-    security_context_t scontext = NULL;
-
-    selinux_enabled = (is_selinux_enabled() > 0);
-
-    if (selinux_enabled)
-    {
-        /* get current security context */
-        getfilecon(file, &scontext);
-    }
-#endif
-
     stamp_now = time((time_t *) NULL);
 
     if (stat(file, &statbuf) == -1)

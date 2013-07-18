5e987d73748d31c0b4cfbd7271a50c0418ee2f6f
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-07-18 14:46:29 -0400

    Fix compilation with libvirt, ExpandDeRefPromise needs namespace

--- cf-agent/verify_environments.c
+++ cf-agent/verify_environments.c
@@ -136,7 +136,7 @@ void VerifyEnvironmentsPromise(EvalContext *ctx, Promise *pp)
         PromiseBanner(pp);
         ScopeNewSpecial(ctx, "this", "promiser", pp->promiser, DATA_TYPE_STRING);
 
-        pexp = ExpandDeRefPromise(ctx, "this", pp);
+        pexp = ExpandDeRefPromise(ctx, NULL, "this", pp);
         VerifyEnvironments(ctx, a, pp);
         PromiseDestroy(pexp);
     }

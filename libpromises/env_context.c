af1f5f46d7967222dbcb3ec26106246206f45dc3
Sigurd Teigen <sigurd.teigen@cfengine.com>
2013-07-25 14:50:45 -0400

    Store 'sys' variables in EvalContext global VariableTable

--- libpromises/env_context.c
+++ libpromises/env_context.c
@@ -1285,6 +1285,7 @@ bool EvalContextVariablePutSpecial(EvalContext *ctx, SpecialScope scope, const c
 {
     switch (scope)
     {
+    case SPECIAL_SCOPE_SYS:
     case SPECIAL_SCOPE_CONST:
     case SPECIAL_SCOPE_EDIT:
         {
@@ -1303,6 +1304,8 @@ bool EvalContextVariableRemoveSpecial(const EvalContext *ctx, SpecialScope scope
 {
     switch (scope)
     {
+    case SPECIAL_SCOPE_SYS:
+    case SPECIAL_SCOPE_CONST:
     case SPECIAL_SCOPE_EDIT:
         {
             VarRef ref = (VarRef) { NULL, SpecialScopeToString(scope), lval };
@@ -1319,6 +1322,7 @@ static VariableTable *GetVariableTableForVarRef(const EvalContext *ctx, const Va
 {
     switch (SpecialScopeFromString(ref->scope))
     {
+    case SPECIAL_SCOPE_SYS:
     case SPECIAL_SCOPE_CONST:
         return ctx->global_variables;
 
@@ -1431,7 +1435,7 @@ bool EvalContextVariablePut(EvalContext *ctx, const VarRef *ref, Rval rval, Data
         VariableTable *table = GetVariableTableForVarRef(ctx, ref);
         return VariableTablePut(table, ref, &rval, type);
     }
-    else if (strcmp("const", ref->scope) == 0)
+    else if (strcmp("const", ref->scope) == 0 || strcmp("sys", ref->scope) == 0)
     {
         assert(!ref->ns);
         VariableTable *table = GetVariableTableForVarRef(ctx, ref);
@@ -1500,7 +1504,7 @@ bool EvalContextVariableGet(const EvalContext *ctx, const VarRef *ref, Rval *rva
     Scope *get_scope = NULL;
     if (VarRefIsQualified(ref))
     {
-        if (strcmp(ref->scope, "edit") == 0 || strcmp(ref->scope, "const") == 0)
+        if (strcmp(ref->scope, "edit") == 0 || strcmp(ref->scope, "const") == 0 || strcmp(ref->scope, "sys") == 0)
         {
             VariableTable *table = GetVariableTableForVarRef(ctx, ref);
             Variable *var = VariableTableGet(table, ref);

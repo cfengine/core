dbbbf0a66692b600bf217b067ccb36121cbbfdf4
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2013-08-08 22:59:28 +0200

    Verify received public key is correctly signed. Signing algorithm can now be configured with a string.

--- cf-serverd/server_tls.c
+++ cf-serverd/server_tls.c
@@ -89,14 +89,30 @@ bool ServerTLSInitialize()
         EVP_PKEY_set1_RSA(pkey, PRIVKEY);
         X509_NAME *name = X509_get_subject_name(x509);
         X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC,
-                                   (const char *) "ouripaddress", /* TODO */
+                                   (const char *) "",
                                    -1, -1, 0);
         X509_set_issuer_name(x509, name);
         X509_set_pubkey(x509, pkey);
-        X509_sign(x509, pkey, EVP_sha384());
-        EVP_PKEY_free(pkey);
 
+        const EVP_MD *md = EVP_get_digestbyname("sha384");
+        if (md == NULL)
+        {
+            Log(LOG_LEVEL_ERR, "Uknown digest algorithm %s",
+                "sha384");
+            return false;
+        }
+        ret = X509_sign(x509, pkey, md);
+
+        EVP_PKEY_free(pkey);
         SSLSERVERCERT = x509;
+
+        if (ret <= 0)
+        {
+            Log(LOG_LEVEL_ERR,
+                "Couldn't sign the public key for the TLS handshake: %s",
+                ERR_reason_error_string(ERR_get_error()));
+            return false;
+        }
     }
     /* Log(LOG_LEVEL_ERR, "generate cert from priv key: %s", */
     /*     ERR_reason_error_string(ERR_get_error())); */

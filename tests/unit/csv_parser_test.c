751c5c60d2bc6493994f2fbbb094a43fb7ebc96d
jeffali <hichame.jeffali@cfengine.com>
2013-08-18 14:18:08 -0400

    Code to parse CSV-formatted strings (redmine feature #3272)

---
+++ tests/unit/csv_parser_test.c
@@ -0,0 +1,83 @@
+#include <test.h>
+#include <sequence.h>
+#include <alloc.h>
+#include <csv_parser.h>
+
+static void test_new_csv_reader_basic()
+{
+    Seq *list = NULL;
+    char *lines[4] = {
+        "aaaa,bbb,ccccc",
+        "\"aaaa\",\"bbb\",\"ccccc\"",
+        "\"aaaa\",bbb,\"ccccc\"",
+        "aaaa,\"bbb\",ccccc"
+    };
+
+
+    for (int i=0; i<4; i++)
+    {
+        list = SeqParseCsvString(lines[i]);
+        assert_int_equal(list->length, 3);
+        assert_string_equal(list->data[2], "ccccc");
+        if (list != NULL)
+        {
+            SeqDestroy(list);
+        } else {
+            assert_true(false);
+        }
+    }
+}
+
+static void test_new_csv_reader()
+{
+    Seq *list = NULL;
+    char line[]="  Type    ,\"walo1\",  \"walo2\"   ,  \"wal,o \"\" 3\",  \"  ab,cd  \", walo solo ,, \"walo\" ";
+
+
+    list = SeqParseCsvString(line);
+
+    assert_int_equal(list->length, 8);
+    assert_string_equal(list->data[3], "wal,o \" 3");
+    if (list != NULL)
+    {
+        SeqDestroy(list);
+    }
+    else
+    {
+        assert_true(false);
+    }
+}
+
+static void test_new_csv_reader_lfln()
+{
+    Seq *list = NULL;
+    char line[]="  Type    ,\"walo1\",  \"walo2\"   ,  \"wa\r\nl,o\n \"\"\r 3\",  \"  ab,cd  \", walo solo ,, \"walo\" ";
+
+
+    list = SeqParseCsvString(line);
+
+    assert_int_equal(list->length, 8);
+    assert_string_equal(list->data[3], "wa\r\nl,o\n \"\r 3");
+    if (list != NULL)
+    {
+        SeqDestroy(list);
+    }
+    else
+    {
+        assert_true(false);
+    }
+}
+
+int main()
+{
+    PRINT_TEST_BANNER();
+    const UnitTest tests[] =
+    {
+        unit_test(test_new_csv_reader_basic),
+        unit_test(test_new_csv_reader),
+        unit_test(test_new_csv_reader_lfln),
+    };
+
+    return run_tests(tests);
+}
+

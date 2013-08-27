935547c567e646aded203054e1ab07e44ad55178
Loic Pefferkorn <loic-git@loicp.eu>
2013-08-27 23:31:13 +0200

    Cosmetic changes

--- libpromises/classes.c
+++ libpromises/classes.c
@@ -73,9 +73,9 @@ const char *VPSOPTS[PLATFORM_CONTEXT_MAX] =
 {
     "",
     "-ef",                      /* hpux */
-    "-N -eo user,pid,ppid,pgid,pcpu,pmem,vsz,ni,stat,st=STIME,time,args",  /* aix */
-    "-eo user,pid,ppid,pgid,pcpu,pmem,vsz,ni,rss,nlwp,stime,time,args",        /* linux */
-    "-eo user,pid,ppid,pgid,pcpu,pmem,vsz,pri,rss,nlwp,stime,time,args",        /* solaris */
+    "-N -eo user,pid,ppid,pgid,pcpu,pmem,vsz,ni,stat,st=STIME,time,args", /* aix */
+    "-eo user,pid,ppid,pgid,pcpu,pmem,vsz,ni,rss,nlwp,stime,time,args",   /* linux */
+    "-eo user,pid,ppid,pgid,pcpu,pmem,vsz,pri,rss,nlwp,stime,time,args",  /* solaris */
     "auxw",                     /* freebsd */
     "auxw",                     /* netbsd */
     "-elyf",                    /* cray */

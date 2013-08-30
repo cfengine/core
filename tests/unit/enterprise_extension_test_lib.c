0f84cb0a0f1f8f296d02bad33e13574468489b94
Kristian Amlie <kristian.amlie@cfengine.com>
2013-08-30 09:52:07 +0200

    Add enterprise extension test for version mismatch.

--- tests/unit/enterprise_extension_test_lib.c
+++ tests/unit/enterprise_extension_test_lib.c
@@ -3,6 +3,7 @@
 // Pretend we are an extension.
 #define BUILDING_CORE_EXTENSION
 #include <enterprise_extension.h>
+#include <config.h>
 
 ENTERPRISE_FUNC_2ARG_DECLARE(int64_t, extension_function, int32_t, short_int, int64_t, long_int);
 ENTERPRISE_FUNC_3ARG_DECLARE(int64_t, extension_function_broken, int32_t, short_int, char, unwanted_par, int64_t, long_int);
@@ -16,4 +17,17 @@ ENTERPRISE_FUNC_2ARG_DEFINE(int64_t, extension_function, int32_t, short_int, int
 ENTERPRISE_FUNC_3ARG_DEFINE(int64_t, extension_function_broken, int32_t, short_int, char, unwanted_par, int64_t, long_int)
 {
     return short_int * long_int;
+}
+
+const char *GetExtensionLibraryVersion()
+{
+    const char *version = getenv("CFENGINE_TEST_RETURN_VERSION");
+    if (version)
+    {
+        return version;
+    }
+    else
+    {
+        return VERSION;
+    }
 }

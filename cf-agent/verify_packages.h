9cfc0a956a4c4028ba82faff273c80f61b836e68
Kristian Amlie <kristian.amlie@cfengine.com>
2013-10-02 11:35:27 +0200

    Fix wrong function signature.

--- cf-agent/verify_packages.h
+++ cf-agent/verify_packages.h
@@ -27,9 +27,20 @@
 
 #include <cf3.defs.h>
 
+#include <vercmp.h>
+
 PromiseResult VerifyPackagesPromise(EvalContext *ctx, Promise *pp);
 void ExecuteScheduledPackages(EvalContext *ctx);
 void CleanScheduledPackages(void);
 int PrependPackageItem(EvalContext *ctx, PackageItem ** list, const char *name, const char *version, const char *arch, Promise *pp);
+
+// For testing.
+VersionCmpResult ComparePackages(EvalContext *ctx,
+                                 const char *n, const char *v, const char *arch,
+                                 PackageItem *pi, Attributes a,
+                                 Promise *pp,
+                                 const char *mode,
+                                 PromiseResult *result);
+
 
 #endif

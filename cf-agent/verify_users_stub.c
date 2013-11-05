ec69c55e8af092c106c9cee5713ca41065eb3ed9
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2013-11-05 10:47:40 +0100

    Compile users promise conditionally
    OS X does not use useradd/userdel/usermod commands to manage
    user accounts, so do a stub implementation for now.

---
+++ cf-agent/verify_users_stub.c
@@ -0,0 +1,35 @@
+/*
+   Copyright (C) CFEngine AS
+
+   This file is part of CFEngine 3 - written and maintained by CFEngine AS.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+
+  To the extent this program is licensed as part of the Enterprise
+  versions of CFEngine, the applicable Commerical Open Source License
+  (COSL) may apply to this file if you as a licensee so wish it. See
+  included file COSL.txt.
+*/
+
+#include <verify_users.h>
+
+void VerifyOneUsersPromise (ARG_UNUSED const char *puser, ARG_UNUSED User u,
+                            ARG_UNUSED PromiseResult *result,
+                            ARG_UNUSED enum cfopaction action,
+                            ARG_UNUSED EvalContext *ctx,
+                            ARG_UNUSED const Attributes *a,
+                            ARG_UNUSED Promise *pp)
+{
+    Log(LOG_LEVEL_ERR, "Users promise type is not supported on this OS");
+}

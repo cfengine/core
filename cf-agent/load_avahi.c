294838931a28869b0a5400270484eaff35ef1ae4
Edward Welbourne <edward.welbourne@cfengine.com>
2013-11-21 13:31:21 +0100

    Use spaces in place of tabs in indentation.
    Noticed while reviewing David Volgyes's fixes.

--- cf-agent/load_avahi.c
+++ cf-agent/load_avahi.c
@@ -31,8 +31,8 @@ static const char *paths[] = {
     "/usr/lib/x86_64-linux-gnu/libavahi-client.so.3",
     "/usr/lib/libavahi-client.so.3",
     "/usr/lib64/libavahi-client.so.3",
-	/* 32 bits variants */
-	"/usr/lib/i386-linux-gnu/libavahi-client.so.3"
+    /* 32-bit variants */
+    "/usr/lib/i386-linux-gnu/libavahi-client.so.3"
 };
 
 static const char *getavahipath();

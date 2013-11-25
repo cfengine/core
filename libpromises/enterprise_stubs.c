8c767ddfe361bdd2882ed6aa171ef65c37e5d0d2
David Volgyes <david.volgyes@gmail.com>
2013-11-25 11:19:17 +0100

    [fix] string formating: another escape character fix.

--- libpromises/enterprise_stubs.c
+++ libpromises/enterprise_stubs.c
@@ -99,7 +99,7 @@ ENTERPRISE_VOID_FUNC_2ARG_DEFINE_STUB(void, LogTotalCompliance, const char *, ve
     char string[CF_BUFSIZE] = { 0 };
 
     snprintf(string, CF_BUFSIZE,
-             "Outcome of version %s (" CF_AGENTC "-%d): Promises observed to be kept %.0f%%, Promises repaired %.0f%%, Promises not repaired %.0f\%%",
+             "Outcome of version %s (" CF_AGENTC "-%d): Promises observed to be kept %.0f%%, Promises repaired %.0f%%, Promises not repaired %.0f%%",
              version, background_tasks,
              (double) PR_KEPT / total,
              (double) PR_REPAIRED / total,

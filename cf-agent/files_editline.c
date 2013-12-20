f8c7dca55ed8d99212564ea130666de4bd5e0931
Mark Burgess <mark@cfengine.com>
2013-12-20 14:30:17 +0100

    Patch for bug 2315 and 1525 - non convergent editline with preserve block

--- cf-agent/files_editline.c
+++ cf-agent/files_editline.c
@@ -574,6 +574,7 @@ static PromiseResult VerifyLineInsertions(EvalContext *ctx, const Promise *pp, E
     /* Are we working in a restricted region? */
 
     PromiseResult result = PROMISE_RESULT_NOOP;
+
     if (!a.haveregion)
     {
         begin_ptr = *start;
@@ -736,6 +737,11 @@ static int InsertMultipleLinesToRegion(EvalContext *ctx, Item **start, Item *beg
     {
         for (ip = *start; ip != NULL; ip = ip->next)
         {
+            if (MatchRegion(ctx, pp->promiser, ip, end_ptr, true))
+            {
+                return false;
+            }
+
             if (ip->next != NULL && ip->next == end_ptr)
             {
                 return InsertMultipleLinesAtLocation(ctx, start, begin_ptr, end_ptr, ip, prev, a, pp, edcontext, result);

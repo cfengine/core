e136acbf308a810b436b1032e559bf820ac01f68
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2013-12-20 14:03:01 +0100

    Note lifetime and usage of globals in cf3globals.c

--- libpromises/cf3globals.c
+++ libpromises/cf3globals.c
@@ -72,32 +72,106 @@ char VSHIFT[12] = { 0 };
 
 char CFWORKDIR[CF_BUFSIZE] = { 0 };
 
+/*
+  Default value for copytype attribute. Loaded by cf-agent from body control
+*/
 char *DEFAULT_COPYTYPE = NULL;
 
+/*
+  Keys for the agent. Loaded by GAInitialize (and hence every time policy is
+  reloaded).
+
+  Used in network protocol and leaked to language.
+*/
 RSA *PRIVKEY = NULL, *PUBKEY = NULL;
 
+/*
+  First IP address discovered by DetectEnvironment (hence reloaded every policy
+  change).
+
+  Used somewhere in cf-execd, superficially in old-style protocol handshake and
+  sporadically in other situations.
+*/
 char VIPADDRESS[CF_MAX_IP_LEN] = { 0 };
 
-/*******************************************************************/
-/*                                                                 */
-/* Checksums                                                       */
-/*                                                                 */
-/*******************************************************************/
+/*
+  Edition-time constant (MD5 for community, something else for Enterprise)
 
+  Used as a default hash everywhere (not only in network protocol)
+*/
 HashMethod CF_DEFAULT_DIGEST;
 int CF_DEFAULT_DIGEST_LEN;
 
+/*
+  Holds the "now" time captured at the moment of policy load (and in response to
+  cf-runagent command to cf-serverd?!).
+
+  Utilized everywhere "now" start time is needed
+*/
 time_t CFSTARTTIME;
 
+/*
+  Set in cf-serverd (from control body)/GenericAgentInitialize (defaults)
+
+  Used in network code
+*/
 int CFENGINE_PORT;
 
+/*
+  Set in cf-agent/cf-runagent (from control body).
+
+  Used as a timeout for socket operations in network code.
+*/
 time_t CONNTIMEOUT = 30;        /* seconds */
+
+/*
+  Internal detail of timeout operations. Due to historical reasons
+  is defined here, not in libpromises/timeout.c
+ */
 pid_t ALARM_PID = -1;
+
+/*
+  Set in cf-agent (from control body).
+
+  Used as a default value for maxfilesize attribute in policy
+*/
 int EDITFILESIZE = 10000;
+
+/*
+  Set in cf-agent (from control body) and GenericAgentInitialize.
+
+  Used as a default value for ifelapsed attribute in policy.
+*/
 int VIFELAPSED = 1;
+
+/*
+  Set in cf-agent (from control body) and GenericAgentInitialize.
+
+  Used as a default value for expireafter attribute in policy.
+*/
 int VEXPIREAFTER = 120;
+
+/*
+  Set in cf-agent/cf-serverd (from control body).
+
+  Utilized in server/client code to bind sockets.
+*/
 char BINDINTERFACE[CF_BUFSIZE] = { 0 };
 
+/*
+  Set in cf-*.c:CheckOpts and GenericAgentConfigParseArguments.
+
+  Utilized in generic_agent.c for
+    - cf_promises_validated filename
+    - GenericAgentCheckPolicy
+    - GenericAgentLoadPolicy (ReadPolicyValidatedFile)
+*/
 bool MINUSF = false;
 
+/* Set in libenv/sysinfo.c::DetectEnvironment (called every time environment
+   reload is performed).
+
+   Utilized all over the place, usually to look up OS-specific command/option to
+   call external utility
+*/
 PlatformContext VSYSTEMHARDCLASS;

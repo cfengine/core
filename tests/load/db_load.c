b2a5dd5fc9364dd74147e7b2e35f424e2ae9577b
Edward Welbourne <edward.welbourne@cfengine.com>
2014-01-08 14:47:53 +0100

    Tidy-up in db_load.c
    Some overly-complex code to do simple things, some stray code
    commented out that might as well be removed.  Moved CoinFlip one
    function earlier to save the need for pre-declaration.

--- tests/load/db_load.c
+++ tests/load/db_load.c
@@ -24,7 +24,6 @@
 
 char CFWORKDIR[CF_BUFSIZE];
 
-static bool CoinFlip(void);
 static void WriteReadWriteData(CF_DB *db);
 static bool ReadWriteDataIsValid(char *data);
 static void DBWriteTestData(CF_DB *db);
@@ -50,7 +49,6 @@ static void *contend(ARG_UNUSED void *param)
     return (void *)STATUS_SUCCESS;
 }
 
-
 static void TestReadWriteData(CF_DB *db)
 {
     WriteReadWriteData(db);
@@ -63,9 +61,6 @@ static void TestReadWriteData(CF_DB *db)
     }
 
     static const int key = READWRITEKEY;
-
-    //char key[64];
-    //snprintf(key, sizeof(key), "%050d", READWRITEKEY);
 
     char readData[sizeof(READWRITEDATA1)];
 
@@ -80,23 +75,14 @@ static void TestReadWriteData(CF_DB *db)
     }
 }
 
+static bool CoinFlip(void)
+{
+    return rand() % 2 == 0;
+}
+
 static void WriteReadWriteData(CF_DB *db)
 {
-    bool flip = CoinFlip();
-
-    char *data;
-    if(flip)
-    {
-        data = READWRITEDATA1;
-    }
-    else
-    {
-        data = READWRITEDATA2;
-    }
-
-    //char key[64];
-    //snprintf(key, sizeof(key), "%050d", READWRITEKEY);
-
+    const char *const data = CoinFlip() ? READWRITEDATA1 : READWRITEDATA2;
     static const int key = READWRITEKEY;
 
     if(!WriteComplexKeyDB(db, (const char *)&key, sizeof(key), data, sizeof(READWRITEDATA1)))
@@ -106,20 +92,10 @@ static void WriteReadWriteData(CF_DB *db)
     }
 }
 
-static bool CoinFlip(void)
-{
-    bool flip = (rand() % 2 == 0) ? true : false;
-    return flip;
-}
-
 static bool ReadWriteDataIsValid(char *data)
 {
-    if(strcmp(data, READWRITEDATA1) == 0 || strcmp(data, READWRITEDATA2) == 0)
-    {
-        return true;
-    }
-
-    return false;
+    return (strcmp(data, READWRITEDATA1) == 0 ||
+            strcmp(data, READWRITEDATA2) == 0);
 }
 
 static void TestCursorIteration(CF_DB *db)
@@ -142,9 +118,6 @@ static void TestCursorIteration(CF_DB *db)
     {
         int key_num = *(int *)key;
         int value_num = *(int *)value;
-
-        //int key_num = atoi(key);
-        //int value_num = atoi(value);
 
         if(key_num >= 0 && key_num < RECORD_COUNT_JUNK)
         {
@@ -257,25 +230,10 @@ int main(int argc, char **argv)
 
 static void DBWriteTestData(CF_DB *db)
 {
-    //char key[64];
-    //char value[128];
-
     for(int i = 0; i < RECORD_COUNT_JUNK; i++)
     {
         bool flip = CoinFlip();
-        int value_num;
-
-        if(flip)
-        {
-            value_num = i + VALUE_OFFSET1;
-        }
-        else
-        {
-            value_num = i + VALUE_OFFSET2;
-        }
-
-        //snprintf(key, sizeof(key), "%050d", i);
-        //snprintf(value, sizeof(value), "%0100d", value_num);
+        int value_num = i + (flip ? VALUE_OFFSET1 : VALUE_OFFSET2);
 
         if (!WriteComplexKeyDB(db, (const char *)&i, sizeof(i), &value_num, sizeof(value_num)))
         {

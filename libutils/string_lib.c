46119277f5a11420305de5026129f0324fc395f5
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2014-01-17 18:20:49 +0100

    Fixed documentation, added TODO.

--- libutils/string_lib.c
+++ libutils/string_lib.c
@@ -624,6 +624,8 @@ void ReplaceChar(char *in, char *out, int outSz, char from, char to)
     }
 }
 
+/* TODO replace with StringReplace. This one is pretty slow, calls strncmp
+ * O(n) times even if string matches nowhere. */
 int ReplaceStr(char *in, char *out, int outSz, char *from, char *to)
 /* Replaces all occurences of strings 'from' to 'to' in preallocated
  * string 'out'. Returns true on success, false otherwise. */

47c6e4d7fe0e727a26afc437c80cb543cc908ee9
Sigurd Teigen <sigurd.teigen@cfengine.com>
2014-02-07 15:33:40 -0500

    Move promise_logging into eval_context

--- libpromises/eval_context.c
+++ libpromises/eval_context.c
@@ -39,11 +39,12 @@
 #include <vars.h>
 #include <syslog_client.h>
 #include <audit.h>
-#include <promise_logging.h>
 #include <rlist.h>
 #include <buffer.h>
 #include <promises.h>
 #include <fncall.h>
+#include <ring_buffer.h>
+#include <logging_priv.h>
 
 static bool BundleAborted(const EvalContext *ctx);
 static void SetBundleAborted(EvalContext *ctx);
@@ -84,6 +85,223 @@ struct EvalContext_
     // Full path to directory that the binary was launched from.
     char *launch_directory;
 };
+
+typedef struct
+{
+    const EvalContext *eval_context;
+
+    bool in_promise;
+
+    char *stack_path;
+
+    RingBuffer *messages;
+} PromiseLoggingContext;
+
+static LogLevel AdjustLogLevel(LogLevel base, LogLevel adjust)
+{
+    if (adjust == -1)
+    {
+        return base;
+    }
+    else
+    {
+        return MAX(base, adjust);
+    }
+}
+
+static LogLevel StringToLogLevel(const char *value)
+{
+    if (value)
+    {
+        if (!strcmp(value, "verbose"))
+        {
+            return LOG_LEVEL_VERBOSE;
+        }
+        if (!strcmp(value, "inform"))
+        {
+            return LOG_LEVEL_INFO;
+        }
+        if (!strcmp(value, "error"))
+        {
+            return LOG_LEVEL_NOTICE; /* Error level includes warnings and notices */
+        }
+    }
+    return -1;
+}
+
+static LogLevel GetLevelForPromise(const Promise *pp, const char *attr_name)
+{
+    return StringToLogLevel(PromiseGetConstraintAsRval(pp, attr_name, RVAL_TYPE_SCALAR));
+}
+
+static LogLevel CalculateLogLevel(const Promise *pp)
+{
+    LogLevel log_level = LogGetGlobalLevel();
+
+    if (pp)
+    {
+        log_level = AdjustLogLevel(log_level, GetLevelForPromise(pp, "log_level"));
+    }
+
+    /* Disable system log for dry-runs */
+    if (DONTDO)
+    {
+        log_level = LOG_LEVEL_NOTHING;
+    }
+
+    return log_level;
+}
+
+static LogLevel CalculateReportLevel(const Promise *pp)
+{
+    LogLevel report_level = LogGetGlobalLevel();
+
+    if (pp)
+    {
+        report_level = AdjustLogLevel(report_level, GetLevelForPromise(pp, "report_level"));
+    }
+
+    return report_level;
+}
+
+static char *LogHook(LoggingPrivContext *pctx, const char *message)
+{
+    PromiseLoggingContext *plctx = pctx->param;
+
+    if (plctx->in_promise)
+    {
+        RingBufferAppend(plctx->messages, xstrdup(message));
+        return StringConcatenate(3, plctx->stack_path, ": ", message);
+    }
+    else
+    {
+        return xstrdup(message);
+    }
+}
+
+static void PromiseLoggingInit(const EvalContext *eval_context, size_t max_messages)
+{
+    LoggingPrivContext *pctx = LoggingPrivGetContext();
+
+    if (pctx != NULL)
+    {
+        ProgrammingError("Promise logging: Still bound to another EvalContext");
+    }
+
+    PromiseLoggingContext *plctx = xcalloc(1, sizeof(PromiseLoggingContext));
+    plctx->eval_context = eval_context;
+    plctx->messages = RingBufferNew(max_messages, NULL, free);
+
+    pctx = xcalloc(1, sizeof(LoggingPrivContext));
+    pctx->param = plctx;
+    pctx->log_hook = &LogHook;
+
+    LoggingPrivSetContext(pctx);
+}
+
+static void PromiseLoggingPromiseEnter(const EvalContext *eval_context, const Promise *pp)
+{
+    LoggingPrivContext *pctx = LoggingPrivGetContext();
+    if (!pctx)
+    {
+        ProgrammingError("Promise logging: Unable to enter promise, not bound to EvalContext");
+    }
+
+    PromiseLoggingContext *plctx = pctx->param;
+    if (plctx->eval_context != eval_context)
+    {
+        ProgrammingError("Promise logging: Unable to enter promise, bound to EvalContext different from passed one");
+    }
+
+    assert(!plctx->in_promise);
+    plctx->in_promise = true;
+
+    plctx->stack_path = EvalContextStackPath(eval_context);
+    RingBufferClear(plctx->messages);
+    LoggingPrivSetLevels(CalculateLogLevel(pp), CalculateReportLevel(pp));
+}
+
+static void PromiseLoggingPromiseFinish(const EvalContext *eval_context, const Promise *pp)
+{
+    LoggingPrivContext *pctx = LoggingPrivGetContext();
+
+    if (pctx == NULL)
+    {
+        ProgrammingError("Promise logging: Unable to finish promise, not bound to EvalContext");
+    }
+
+    PromiseLoggingContext *plctx = pctx->param;
+
+    if (plctx->eval_context != eval_context)
+    {
+        ProgrammingError("Promise logging: Unable to finish promise, bound to EvalContext different from passed one");
+    }
+
+    if (EvalContextStackCurrentPromise(eval_context) != pp)
+    {
+        /*
+         * FIXME: There are still cases where promise passed here is not on top of stack
+         */
+        /* ProgrammingError("Logging: Attempt to finish promise not on top of stack"); */
+    }
+
+    assert(plctx->in_promise);
+    plctx->in_promise = false;
+
+    free(plctx->stack_path);
+    plctx->stack_path = NULL;
+
+    LoggingPrivSetLevels(LogGetGlobalLevel(), LogGetGlobalLevel());
+}
+
+static const RingBuffer *PromiseLoggingMessages(const EvalContext *ctx)
+{
+    LoggingPrivContext *pctx = LoggingPrivGetContext();
+
+    if (pctx == NULL)
+    {
+        ProgrammingError("Promise logging: Unable to finish promise, not bound to EvalContext");
+    }
+
+    PromiseLoggingContext *plctx = pctx->param;
+
+    if (plctx->eval_context != ctx)
+    {
+        ProgrammingError("Promise logging: Unable to finish promise, bound to EvalContext different from passed one");
+    }
+
+    return plctx->messages;
+}
+
+static void PromiseLoggingFinish(const EvalContext *eval_context)
+{
+    LoggingPrivContext *pctx = LoggingPrivGetContext();
+
+    if (pctx == NULL)
+    {
+        ProgrammingError("Promise logging: Unable to finish, PromiseLoggingInit was not called before");
+    }
+
+    PromiseLoggingContext *plctx = pctx->param;
+
+    if (plctx->eval_context != eval_context)
+    {
+        ProgrammingError("Promise logging: Unable to finish, passed EvalContext does not correspond to current one");
+    }
+
+    if (plctx->in_promise)
+    {
+        ProgrammingError("Promise logging: Unable to finish, promise is still active");
+    }
+
+    RingBufferDestroy(plctx->messages);
+    LoggingPrivSetContext(NULL);
+
+    free(plctx);
+    free(pctx);
+}
+
+
 
 static StackFrame *LastStackFrame(const EvalContext *ctx, size_t offset)
 {

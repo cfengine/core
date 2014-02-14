f3141fde2d5fb33116863cc0042bb2647aa089e6
David Volgyes <david.volgyes@ieee.org>
2014-02-14 10:43:41 +0100

    String lib performance fix: using memcpy when the length is known.
    Using strncpy(target, string, strlen(string)) equals with memcpy(target, string, strlen(string).
    Both of them remove the \0 from the end. But for longer string memcpy is definitely more efficient,
    because it does not have to search for string termination.
    
    Usually the performance difference is small, and this kind of code rarely used elsewhere,
    but a string handling lib should be efficient.

--- libutils/string_lib.c
+++ libutils/string_lib.c
@@ -295,7 +295,7 @@ char *StringSubstring(const char *source, size_t source_len, int start, int len)
 
     char *result = xcalloc(end - start + 2, sizeof(char));
 
-    strncpy(result, source + start, end - start + 1);
+    memcpy(result, source + start, end - start + 1);
     return result;
 }
 
@@ -826,7 +826,7 @@ char **String2StringArray(const char *str, char separator)
         }
 
         arr[i] = xcalloc(len + 1, sizeof(char));
-        strncpy(arr[i], sp, len);
+        memcpy(arr[i], sp, len);
 
         sp = esp;
         i++;
@@ -1050,13 +1050,13 @@ bool StringNotMatchingSetCapped(const char *isp, int limit,
 
     if (l < limit-1)
     {
-        strncpy(obuf, isp, l);
+        memcpy(obuf, isp, l);
         obuf[l]='\0';
         return false;
     }
     else
     {
-        strncpy(obuf, isp, limit-1);
+        memcpy(obuf, isp, limit-1);
         obuf[limit-1]='\0';
         return true;
     }

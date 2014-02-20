8b2dfc521ed343b3caaceda1a04722612086fbfc
Edward Welbourne <edward.welbourne@cfengine.com>
2014-02-20 19:22:27 +0100

    Fix bogus "return" in void Prefix##SetClear().
    The type-specific variant of SetClear was returning SetClear, except
    that they're both void.  Many compilers cope with that, but apparently
    a user had problems building on Solaris because of it.

--- libutils/set.h
+++ libutils/set.h
@@ -93,7 +93,7 @@ void *SetIteratorNext(SetIterator *i);
                                                                         \
     void Prefix##SetClear(Prefix##Set *set)                             \
     {                                                                   \
-        return SetClear(set->impl);                                     \
+        SetClear(set->impl);                                            \
     }                                                                   \
                                                                         \
     size_t Prefix##SetSize(const Prefix##Set *set)                      \

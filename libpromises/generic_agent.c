9451657b93ed300e2578efef0743ca9fb5ca5421
Kristian Amlie <kristian.amlie@cfengine.com>
2014-04-04 12:59:46 +0200

    Fix infinite recursion when checking cf_promises_validated.
    GenericAgentIsPolicyReloadNeeded() calls
    GenericAgentTagReleaseDirectory(), so we cannot also call the former
    from within there. This only happens if the timestamp within
    cf_promises_validated is newer than the current time, which shouldn't
    be often, but is not impossible either.
    
    Within generic_agent.c, GenericAgentTagReleaseDirectory() is only
    called after GenericAgentIsPolicyReloadNeeded() has already been
    checked, so there is no point in calling it a second time. However, it
    changes the behavior of "cf-promises -T", which will now
    unconditionally write cf_promises_validated, whereas before it would
    only write if the timestamp was older than any of the policy files.

--- libpromises/generic_agent.c
+++ libpromises/generic_agent.c
@@ -411,22 +411,18 @@ bool GenericAgentTagReleaseDirectory(const GenericAgentConfig *config, const cha
     {
         Log(LOG_LEVEL_DEBUG, "Tagging directory %s for validation", dirname);
 
-        if (GenericAgentIsPolicyReloadNeeded(config))
+        GetPromisesValidatedFile(filename, sizeof(filename), config, dirname);
+
+        bool wrote_validated = WritePolicyValidatedFile(config, filename);
+
+        if (!wrote_validated)
         {
-            Log(LOG_LEVEL_DEBUG, "The promises_validated of %s needs to be updated", dirname);
-            GetPromisesValidatedFile(filename, sizeof(filename), config, dirname);
-
-            bool wrote_validated = WritePolicyValidatedFile(config, filename);
-
-            if (!wrote_validated)
-            {
-                Log(LOG_LEVEL_VERBOSE, "The promises_validated file %s was NOT updated", filename);
-                return false;
-            }
-
-            Log(LOG_LEVEL_DEBUG, "The promises_validated file %s was updated", filename);
-            return true;
+            Log(LOG_LEVEL_VERBOSE, "The promises_validated file %s was NOT updated", filename);
+            return false;
         }
+
+        Log(LOG_LEVEL_DEBUG, "The promises_validated file %s was updated", filename);
+        return true;
     }
 
     return true;

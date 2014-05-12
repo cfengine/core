4c07869d74b7ca5126b06e9b8639c8dcd54f5df0
Kristian Amlie <kristian.amlie@cfengine.com>
2014-05-12 09:10:42 +0200

    Fix build: Remove redundant function declaration.

--- cf-serverd/cf-serverd-enterprise-stubs.h
+++ cf-serverd/cf-serverd-enterprise-stubs.h
@@ -52,6 +52,5 @@ ENTERPRISE_VOID_FUNC_0ARG_DECLARE(void, CollectCallStop);
 ENTERPRISE_FUNC_0ARG_DECLARE(bool, CollectCallHasPending);
 ENTERPRISE_FUNC_1ARG_DECLARE(int, CollectCallGetPending, ARG_UNUSED int *, queue_length);
 ENTERPRISE_VOID_FUNC_0ARG_DECLARE(void, CollectCallMarkProcessed);
-ENTERPRISE_VOID_FUNC_1ARG_DECLARE(void, FprintAvahiCfengineTag, FILE *, fp);
 
 #endif

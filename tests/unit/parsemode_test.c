054a7febb803462027a0f9ee94e86390f80054ea
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-05-27 15:26:48 +0200

    Beginnings of a unit test for ParseModeString.
    (cherry picked from commit a98f4f19eccf8b47a047b4a7b9008920d8ff1981)

---
+++ tests/unit/parsemode_test.c
@@ -0,0 +1,68 @@
+#include <test.h>
+
+#include <cf3.defs.h>
+
+typedef struct {
+   char *string;
+   mode_t plus;
+   mode_t minus;
+} mode_definition;
+
+mode_definition modes[] = {
+   { "666", S_IRUSR|S_IRGRP|S_IROTH|S_IWUSR|S_IWGRP|S_IWOTH, S_IXUSR|S_IXGRP|S_IXOTH|S_ISUID|S_ISGID|S_ISVTX },
+   { "g+w", S_IWGRP, 0 },
+   { "u+r,u+w,g-w,o-rw", S_IRUSR|S_IWUSR, S_IWGRP|S_IWOTH|S_IROTH },
+   { NULL, 0, 0 } // last case, still tested
+};
+
+void test_mode(void)
+{
+    int ret = false;
+    mode_t plus = 0;
+    mode_t minus = 0;
+
+    int mode = 0;
+    do {
+	ret = ParseModeString(modes[mode].string, &plus, &minus);
+	assert_true(ret);
+	assert_int_equal(modes[mode].plus, plus);
+	assert_int_equal(modes[mode].minus, minus);
+    } while (modes[mode++].string);
+}
+
+typedef struct {
+    char *string;
+    bool valid;
+} validation_mode;
+
+validation_mode validation_modes[] = {
+    { "", false },
+    { "abc", false },
+    { "222222", false },
+    { "22222", true },
+    { NULL, true } // last case, still tested
+};
+
+void test_validation(void)
+{
+    int ret = false;
+    mode_t minus = 0;
+    mode_t plus = 0;
+
+    int mode = 0;
+    do {
+       ret = ParseModeString( validation_modes[mode].string, &plus, &minus);
+       assert_int_equal(validation_modes[mode].valid, ret);
+    } while (validation_modes[mode++].string);
+}
+
+int main()
+{
+    PRINT_TEST_BANNER();
+    const UnitTest tests[] =
+    {
+        unit_test(test_validation),
+        unit_test(test_mode)
+    };
+    return run_tests(tests);
+}

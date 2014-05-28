cca21b40d1a66b379219ece36b830454001ddcf6
Mark Burgess <mark@cfengine.com>
2014-05-28 10:40:01 +0200

    Control parsing for bgp

--- libpromises/mod_common.c
+++ libpromises/mod_common.c
@@ -397,24 +397,22 @@ const ConstraintSyntax FILE_CONTROL_CONSTRAINTS[] =
 
 const ConstraintSyntax ROUTING_CONTROLBODY[] =
 {
+    ConstraintSyntaxNewString("log_file", CF_ANYSTRING, "Log file for route service messages, if supported on platform", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewOption("ospf_log_adjacency_changes", "normal,detail,none", "OSPF logging of neighbour changes", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewInt("ospf_log_timestamp_precision", "0,6", "Maximum precision in microseconds [0-6]", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewString("ospf_router_id", CF_IPRANGE, "The router's identity address aka loopback address", SYNTAX_STATUS_NORMAL),
-    ConstraintSyntaxNewString("ospf_log_file", CF_ANYSTRING, "Log file for OSPF messages, if supported on platform", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewOptionList("ospf_redistribute", "kernel,connected,static,bgp", "Which source of configuration is considered authoritative?", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewOption("ospf_external_metric_type", "1,2", "How to calculate metrics for external routes", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewInt("ospf_redistribute_kernel_metric", CF_VALRANGE, "Metric for redistributed kernel route", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewInt("ospf_redistribute_connected_metric", CF_VALRANGE, "Metric for redistributed direct connetions", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewInt("ospf_redistribute_static_metric", CF_VALRANGE, "Metric for redistributed static route", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewInt("ospf_redistribute_bgp_metric", CF_VALRANGE, "Metric for redistributed BGP route", SYNTAX_STATUS_NORMAL),
-    ConstraintSyntaxNewInt("bgp_this_as", CF_VALRANGE, "This router's BGP autonomous system number", SYNTAX_STATUS_NORMAL),
+    ConstraintSyntaxNewInt("bgp_local_as", CF_VALRANGE, "This router's BGP autonomous system number", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewString("bgp_router_id", CF_IPRANGE, "The router's identity address aka loopback address", SYNTAX_STATUS_NORMAL),
-    ConstraintSyntaxNewString("bgp_log_file", CF_ANYSTRING, "Log file for BGP messages, if supported on platform", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewOption("bgp_log_adjacency_changes", "normal,detail,none", "Activate logging for bgp neighbour changes", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewOptionList("bgp_redistribute", "kernel,connected,static,ospf", "Which source of configuration is considered authoritative?", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewStringList("bgp_bestpath", "", "Path selection policy for BGP", SYNTAX_STATUS_NORMAL),
     ConstraintSyntaxNewStringList("bgp_networks", CF_IPRANGE, "Declare local networks to advertise for this router", SYNTAX_STATUS_NORMAL),
-
     ConstraintSyntaxNewNull()
 };
 

043246d519d9e306629174735bcf426e4e23dc8f
Mark Burgess <mark@cfengine.com>
2014-06-04 19:21:28 +0200

    Move the LLDP discovery to make available in inventory

--- libenv/sysinfo.c
+++ libenv/sysinfo.c
@@ -2009,9 +2009,8 @@ static int Linux_Misc_Version(EvalContext *ctx)
                 CanonifyNameInPlace(version);
             }
         }
-    }
-
     fclose(fp);
+    }
 
     if (*os && *version)
     {

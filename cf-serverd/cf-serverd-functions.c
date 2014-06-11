a59d571c0decfd156e73036e6b296743ea54ba16
Edward Welbourne <edward.welbourne@cfengine.com>
2014-06-11 16:04:48 +0200

    Leak-fix (FD): close socket when aborting on failure to lock.
    That's in cf-serverd's StartServer().  Also avoid using an invalid
    socket; if OpenReceiverChannel() fails to bind, it closes the socket
    it just tried to use, so we should reset the socket variable to -1 so
    we don't try to re-use the socket we just closed.  Split a long line.

--- cf-serverd/cf-serverd-functions.c
+++ cf-serverd/cf-serverd-functions.c
@@ -518,8 +518,10 @@ static int OpenReceiverChannel(void)
         else
         {
             Log(LOG_LEVEL_INFO,
-                "Could not bind server address. (bind: %s)", GetErrorStr());
+                "Could not bind server address. (bind: %s)",
+                GetErrorStr());
             cf_closesocket(sd);
+            sd = -1;
         }
     }
 
@@ -585,6 +587,10 @@ void StartServer(EvalContext *ctx, Policy **policy, GenericAgentConfig *config)
     if (thislock.lock == NULL)
     {
         PolicyDestroy(server_cfengine_policy);
+        if (sd >= 0)
+        {
+            cf_closesocket(sd);
+        }
         return;
     }
 

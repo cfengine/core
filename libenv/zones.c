9661a5093c1f123baed7c41ccd968bd3375d07b6
Matthew Cattell <matthew.cattell@cfengine.com>
2014-06-30 16:01:04 +0200

    solaris: extend ps command column width and prepend zone name

--- libenv/zones.c
+++ libenv/zones.c
@@ -23,6 +23,7 @@
 */
 
 #include <zones.h>
+#include <string_lib.h>
 
 #ifdef HAVE_ZONE_H
 # include <zone.h>
@@ -52,7 +53,7 @@ bool ForeignZone(char *s)
 {
 // We want to keep the banner
 
-    if (strstr(s, "%CPU"))
+    if (strstr(s, "PID"))
     {
         return false;
     }
@@ -66,9 +67,8 @@ bool ForeignZone(char *s)
 
     if (strcmp(zone, "global") == 0)
     {
-        if (strcmp(s + strlen(s) - 6, "global") == 0)
+        if (StringStartsWith(s, "global"))
         {
-            *(s + strlen(s) - 6) = '\0';
 
             for (sp = s + strlen(s) - 1; isspace(*sp); sp--)
             {
@@ -85,5 +85,18 @@ bool ForeignZone(char *s)
 # endif
     return false;
 }
+int  CurrentZoneName(char *s)
+{
+    int ok = -1;
+# ifdef HAVE_GETZONEID
+    zoneid_t zid;
+    if ((zid = getzoneid()) < 0)
+    {
+        return ok;
+    }
 
+    ok = getzonenamebyid(zid, s, ZONENAME_MAX);
+# endif
+    return ok;
+}
 #endif // !__MINGW32__

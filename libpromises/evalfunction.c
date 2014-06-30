4a8594466978353d18da5488157834c358225c10
Edward Welbourne <edward.welbourne@cfengine.com>
2014-06-30 19:24:17 +0200

    Leak-fixes and clean-up in FnCallPackagesMatching().
    Move computation of regex outside the loop - no need to recompute the
    same regex for every line of the CSV file ! - and pre-compile it so
    that we can search using it more efficiently.  Localise all the
    variables related to that at the same time.
    
    Split up various long lines, notably when Log()ging failure to open
    the inventory.
    
    Move creation of some objects to where they're needed, to save the
    need to tidy them away on failure in the interval.
    
    Don't leak the allocated line when it doesn't match our expectations.
    
    Restructure the for(;;) loop as a while with assignment in its
    condition so as to move the failure-handling out of the loop and avoid
    duplication of clean-up code.  Reviewed the error message on that
    failure: it claimed fread() was involved, which is a lie, so re-worked
    it.
    
    (cherry picked from commit 3fc8d68041c9cae1cebb2e5438a3a040ded3e5d4)

--- libpromises/evalfunction.c
+++ libpromises/evalfunction.c
@@ -1313,16 +1313,25 @@ static FnCallResult FnCallBundlesMatching(EvalContext *ctx, const Policy *policy
 static FnCallResult FnCallPackagesMatching(ARG_UNUSED EvalContext *ctx, ARG_UNUSED const Policy *policy, const FnCall *fp, const Rlist *finalargs)
 {
     const bool installed_mode = (0 == strcmp(fp->name, "packagesmatching"));
+    pcre *matcher;
+    {
+        const char *regex_package = RlistScalarValue(finalargs);
+        const char *regex_version = RlistScalarValue(finalargs->next);
+        const char *regex_arch = RlistScalarValue(finalargs->next->next);
+        const char *regex_method = RlistScalarValue(finalargs->next->next->next);
+        char regex[CF_BUFSIZE];
 
-    char *regex_package = RlistScalarValue(finalargs);
-    char *regex_version = RlistScalarValue(finalargs->next);
-    char *regex_arch = RlistScalarValue(finalargs->next->next);
-    char *regex_method = RlistScalarValue(finalargs->next->next->next);
+        // Here we will truncate the regex if the parameters add up to over CF_BUFSIZE
+        snprintf(regex, sizeof(regex), "^%s,%s,%s,%s$",
+                 regex_package, regex_version, regex_arch, regex_method);
+        matcher = CompileRegex(regex);
+        if (matcher == NULL)
+        {
+            return FnFailure();
+        }
+    }
 
-    JsonElement *json = JsonArrayCreate(50);
-    char filename[CF_MAXVARSIZE], regex[CF_BUFSIZE];
-    FILE *fin;
-
+    char filename[CF_MAXVARSIZE];
     if (installed_mode)
     {
         GetSoftwareCacheFilename(filename);
@@ -1334,64 +1343,58 @@ static FnCallResult FnCallPackagesMatching(ARG_UNUSED EvalContext *ctx, ARG_UNUS
 
     Log(LOG_LEVEL_DEBUG, "%s: reading inventory from '%s'", fp->name, filename);
 
-    if ((fin = fopen(filename, "r")) == NULL)
+    FILE *const fin = fopen(filename, "r");
+    if (fin == NULL)
     {
-        Log(LOG_LEVEL_VERBOSE, "%s cannot open the %s packages inventory '%s' - "
-            "This is not necessarily an error. Either the inventory policy has not been included, "
-            "or it has not had time to have an effect yet. A future call may still succeed. (fopen: %s)",
-            fp->name, installed_mode ? "installed" : "available", filename, GetErrorStr());
-        JsonDestroy(json);
+        Log(LOG_LEVEL_VERBOSE,
+            "%s cannot open the %s packages inventory '%s' - "
+            "This is not necessarily an error. "
+            "Either the inventory policy has not been included, "
+            "or it has not had time to have an effect yet. "
+            "A future call may still succeed. (fopen: %s)",
+            fp->name,
+            installed_mode ? "installed" : "available",
+            filename,
+            GetErrorStr());
+
+        pcre_free(matcher);
         return FnFailure();
     }
 
+    JsonElement *json = JsonArrayCreate(50);
     int linenumber = 0;
+    char *line;
 
-    for(;;)
+    while (NULL != (line = GetCsvLineNext(fin)))
     {
-        char *line = GetCsvLineNext(fin);
-        if (NULL == line)
-        {
-            if (!feof(fin))
-            {
-                Log(LOG_LEVEL_ERR, "Unable to read package inventory from '%s'. (fread: %s)", filename, GetErrorStr());
-                fclose(fin);
-                JsonDestroy(json);
-                free(line);
-                return FnFailure();
-            }
-            else
-            {
-                break;
-            }
-        }
-
         if (strlen(line) > CF_BUFSIZE - 80)
         {
-            Log(LOG_LEVEL_ERR, "Line %d from package inventory '%s' is too long to be sensible", linenumber, filename);
-            break;
+            Log(LOG_LEVEL_ERR,
+                "Line %d from package inventory '%s' is too long (%zd) to be sensible",
+                linenumber, filename, strlen(line));
+            free(line);
+            break; /* or continue ? */
         }
 
-        memset(regex, 0, sizeof(regex));
-        // Here we will truncate the regex if the parameters add up to over CF_BUFSIZE
-        snprintf(regex, sizeof(regex)-1, "^%s,%s,%s,%s$", regex_package, regex_version, regex_arch, regex_method);
-
-        if (StringMatchFull(regex, line))
+        if (StringMatchFullWithPrecompiledRegex(matcher, line))
         {
             Seq *list = SeqParseCsvString(line);
-            JsonElement *line_obj = JsonObjectCreate(4);
             if (SeqLength(list) != 4)
             {
-                Log(LOG_LEVEL_ERR, "Line %d from package inventory '%s' did not yield 4 elements: %s", linenumber, filename, line);
-                JsonDestroy(line_obj);
+                Log(LOG_LEVEL_ERR,
+                    "Line %d from package inventory '%s' did not yield 4 elements: %s",
+                    linenumber, filename, line);
                 ++linenumber;
                 SeqDestroy(list);
+                free(line);
                 continue;
             }
 
-            JsonObjectAppendString(line_obj, "name", SeqAt(list, 0));
+            JsonElement *line_obj = JsonObjectCreate(4);
+            JsonObjectAppendString(line_obj, "name",    SeqAt(list, 0));
             JsonObjectAppendString(line_obj, "version", SeqAt(list, 1));
-            JsonObjectAppendString(line_obj, "arch", SeqAt(list, 2));
-            JsonObjectAppendString(line_obj, "method", SeqAt(list, 3));
+            JsonObjectAppendString(line_obj, "arch",    SeqAt(list, 2));
+            JsonObjectAppendString(line_obj, "method",  SeqAt(list, 3));
             SeqDestroy(list);
 
             JsonArrayAppendObject(json, line_obj);
@@ -1400,11 +1403,22 @@ static FnCallResult FnCallPackagesMatching(ARG_UNUSED EvalContext *ctx, ARG_UNUS
         ++linenumber;
         free(line);
     }
+    const char *errstr = GetErrorStr(); /* Only relevant if fail */
 
+    bool fail = !feof(fin);
     fclose(fin);
+    pcre_free(matcher);
+
+    if (fail)
+    {
+        Log(LOG_LEVEL_ERR,
+            "Unable to read (%s) package inventory from '%s'.",
+            errstr, filename);
+        JsonDestroy(json);
+        return FnFailure();
+    }
 
     return (FnCallResult) { FNCALL_SUCCESS, (Rval) { json, RVAL_TYPE_CONTAINER } };
-
 }
 
 /*********************************************************************/

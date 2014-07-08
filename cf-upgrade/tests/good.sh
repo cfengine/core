1ff9f11ac9349bc1a08db00e9267afeb0a7bb1d6
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2014-07-08 13:49:03 +0200

    cf-upgrade should not close STDOUT of all forked commands
    Now both STDOUT and STDERR of forked commands go to STDOUT and STDERR of
    cf-upgrade. Previously it was just closed and e.g. dpkg was failing
    because of that.
    
    Modify the test scripts to actually test that.

--- cf-upgrade/tests/good.sh
+++ cf-upgrade/tests/good.sh
@@ -1,3 +1,13 @@
-#!/bin/true
+#!/bin/sh
+
+# Exit with error on any failed command
+set -e
 
 
+echo running inside $0, arguments are: $@
+
+echo good.sh STDOUT
+echo good.sh STDERR >&2
+
+
+return 0

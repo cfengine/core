bac106d4130a54827101e1de4afae91c8bb9e2f7
Kristian Amlie <kristian.amlie@cfengine.com>
2014-07-11 14:03:16 +0200

    Move tricky output handling into elevate.sh instead of testall.
    This way it can be more reliably reused.
    
    (cherry picked from commit 7f41d44bebfe337005eaa6d295d2021b21c1a7b8)

--- tests/acceptance/tool_wrappers/elevate.sh
+++ tests/acceptance/tool_wrappers/elevate.sh
@@ -5,8 +5,11 @@
 # Be careful what you pass into this script. It cannot handle any redirection
 # or spaces in pathnames.
 
-$(dirname $0)/../elevate.exe -wait "$(dirname $0)\template.bat" "cd '`pwd`'; export PATH='$PATH'; $(dirname $0)/store-return-code.sh $@"
+touch output.$$
+$(dirname $0)/../elevate.exe -wait "$(dirname $0)\template.bat" "cd '`pwd`'; export PATH='$PATH'; $(dirname $0)/preserve-output-and-status.sh $$ $@" &
+tail -F output.$$ --pid=$! 2>/dev/null
+rm -f output.$$
 # Elevate does not preserve return code, so use the return code we stored above.
-return_code=`cat return-code.txt`
-rm -f return-code.txt
+return_code=`cat return-code.$$`
+rm -f return-code.$$
 exit $return_code

cfengine/core

commit d27add22c7488349b6de1178c4bfecbeb752cfeb

Explicitly expand this.promiser in copy_from.source
Include/update test case.

See Redmine#1554

cf-agent/verify_files_utils.c
@@ -2441,7 +2441,9 @@ static PromiseResult VerifyCopiedFileAttributes(EvalContext *ctx, const char *sr
 
 static PromiseResult CopyFileSources(EvalContext *ctx, char *destination, Attributes attr, const Promise *pp, AgentConnection *conn)
 {
-    const char *source = attr.copy.source;
+    Buffer *source = BufferNew();
+    // Expand this.promiser
+    ExpandScalar(ctx, PromiseGetBundle(pp)->ns, PromiseGetBundle(pp)->name, attr.copy.source, source);
     char vbuff[CF_BUFSIZE];
     struct stat ssb, dsb;
     struct timespec start;
@@ -2451,14 +2453,16 @@ static PromiseResult CopyFileSources(EvalContext *ctx, char *destination, Attrib
     {
         cfPS(ctx, LOG_LEVEL_ERR, PROMISE_RESULT_FAIL, pp, attr,
              "No authenticated source '%s' in files.copy_from promise",
-             source);
+             BufferData(source));
         return PROMISE_RESULT_FAIL;
     }
 
-    if (cf_stat(attr.copy.source, &ssb, attr.copy, conn) == -1)
+    if (cf_stat(BufferData(source), &ssb, attr.copy, conn) == -1)
     {
         cfPS(ctx, LOG_LEVEL_INFO, PROMISE_RESULT_FAIL, pp, attr,
-             "Can't stat file '%s' in files.copy_from promise", source);
+             "Can't stat file '%s' on '%s' in files.copy_from promise",
+             BufferData(source), conn ? conn->remoteip : "localhost");
+        BufferDestroy(source);
         return PROMISE_RESULT_FAIL;
     }
 
@@ -2477,6 +2481,7 @@ static PromiseResult CopyFileSources(EvalContext *ctx, char *destination, Attrib
         cfPS(ctx, LOG_LEVEL_ERR, PROMISE_RESULT_FAIL, pp, attr,
              "Can't make directories for '%s' in files.copy_from promise",
              vbuff);
+        BufferDestroy(source);
         return PROMISE_RESULT_FAIL;
     }
 
@@ -2490,31 +2495,32 @@ static PromiseResult CopyFileSources(EvalContext *ctx, char *destination, Attrib
             Log(LOG_LEVEL_VERBOSE, "Destination purging enabled");
         }
 
-        Log(LOG_LEVEL_VERBOSE, "Entering directory '%s'", source);
+        Log(LOG_LEVEL_VERBOSE, "Entering directory '%s'", BufferData(source));
 
-        result = PromiseResultUpdate(result, SourceSearchAndCopy(ctx, source, destination, attr.recursion.depth,
+        result = PromiseResultUpdate(result, SourceSearchAndCopy(ctx, BufferData(source), destination, attr.recursion.depth,
                                                                  attr, pp, ssb.st_dev, &inode_cache, conn));
 
         if (stat(destination, &dsb) != -1)
         {
             if (attr.copy.check_root)
             {
-                result = PromiseResultUpdate(result, VerifyCopiedFileAttributes(ctx, source, destination, &ssb, &dsb, attr, pp));
+                result = PromiseResultUpdate(result, VerifyCopiedFileAttributes(ctx, BufferData(source), destination, &ssb, &dsb, attr, pp));
             }
         }
     }
     else
     {
-        result = PromiseResultUpdate(result, VerifyCopy(ctx, source, destination, attr, pp, &inode_cache, conn));
+        result = PromiseResultUpdate(result, VerifyCopy(ctx, BufferData(source), destination, attr, pp, &inode_cache, conn));
     }
 
     DeleteCompressedArray(inode_cache);
 
     snprintf(eventname, CF_BUFSIZE - 1, "Copy(%s:%s > %s)",
-             conn ? conn->this_server : "localhost", source, destination);
+             conn ? conn->this_server : "localhost", BufferData(source), destination);
 
     EndMeasure(eventname, start);
 
+    BufferDestroy(source);
     return result;
 }
 

tests/acceptance/10_files/this_promiser.cf
@@ -45,6 +45,10 @@ bundle agent init
 {
   vars:
       "files" slist => { "aa", "ab", "ac", "ba", "bb", "bc" };
+
+  files:
+      "$(G.testdir)/copy_me_source"
+        create => "true";
 }
 
 #######################################################
@@ -78,6 +82,9 @@ bundle agent test
       depth_search => test_recurse,
       delete => tidyfiles,
       classes => promiser3_generic;
+
+    "$(G.testdir)/copy_me"
+      copy_from => local_cp("$(this.promiser)_source");
 
   methods:
     "template_test" usebundle => template_test;
@@ -136,8 +143,9 @@ body delete tidyfiles
 
 body file_select test_plain
 {
+      leaf_name => { "[a,b][a,b,c]" };
       file_types => { "plain" };
-      file_result => "file_types";
+      file_result => "file_types.leaf_name";
 }
 
 body file_select by_exec_cmd
@@ -167,13 +175,15 @@ bundle agent check
       "ok_4_count" int => countclassesmatching(".*_4_ok");
 
   classes:
-      "ok" expression => none("__this_promiser_[0,1,2,3]_ok", ok_promisers);
+      "ok_expand" expression => none("__this_promiser_[0,1,2,3]_ok", ok_promisers);
       "ok0" expression => strcmp("$(ok_0_count)", 1);
       "ok1" expression => strcmp("$(ok_1_count)", 6);
       "ok2" expression => strcmp("$(ok_2_count)", 5);
       "ok3" expression => strcmp("$(ok_3_count)", 7);
       "ok4" expression => strcmp("$(ok_4_count)", 4);
+      "ok5" expression => fileexists("$(G.testdir)/copy_me");
 
+      "ok" and => { "ok_expand", "ok0", "ok1", "ok2", "ok3", "ok4", "ok5" };
   reports:
     DEBUG::
       "$(ok_promisers)";
@@ -182,10 +192,12 @@ bundle agent check
       "2: $(ok_2_count)";
       "3: $(ok_3_count)";
       "4: $(ok_4_count)";
+    DEBUG.!ok5::
+      "5: file not copied";
 
-    ok.ok0.ok1.ok2.ok3.ok4::
+    ok::
       "$(this.promise_filename) Pass";
-    !ok|!ok0|!ok1|!ok2|!ok3|!ok4::
+    !ok::
       "$(this.promise_filename) FAIL";
 }
 ### PROJECT_ID: core

commit 26456b9ad51669e0fc64c6377a6321bee5a8b008

Squash memory leak introduced in d27add22c7488349b6de1178c4bfecbeb752cfeb

cf-agent/verify_files_utils.c
@@ -2454,6 +2454,7 @@ static PromiseResult CopyFileSources(EvalContext *ctx, char *destination, Attrib
         cfPS(ctx, LOG_LEVEL_ERR, PROMISE_RESULT_FAIL, pp, attr,
              "No authenticated source '%s' in files.copy_from promise",
              BufferData(source));
+        BufferDestroy(source);
         return PROMISE_RESULT_FAIL;
     }
 
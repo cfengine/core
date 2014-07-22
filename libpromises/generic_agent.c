e0c4ee647802e5f67e674f40f36af6d7c5aed304
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-07-22 15:01:27 +0200

    Only respect CFENGINE_COLOR environment if we are on a console.
    Otherwise we spam syslog and other targets with ANSI codes.
    
    Redmine #5388

--- libpromises/generic_agent.c
+++ libpromises/generic_agent.c
@@ -1409,7 +1409,7 @@ GenericAgentConfig *GenericAgentConfigNewDefault(AgentType agent_type)
     config->tty_interactive = isatty(0) && isatty(1);
 
     const char *color_env = getenv("CFENGINE_COLOR");
-    config->color = (color_env && 0 == strcmp(color_env, "1"));
+    config->color = config->tty_interactive && (color_env && 0 == strcmp(color_env, "1"));
 
     config->bundlesequence = NULL;
 

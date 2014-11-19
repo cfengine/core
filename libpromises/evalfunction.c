cfengine/core

commit c4eb2850ff10ac961cb4359626bc643457210ed9

readfile and textxform functions do not rely on 4k limit anymore

libpromises/evalfunction.c
@@ -1356,13 +1356,13 @@ static FnCallResult FnCallCanonify(ARG_UNUSED EvalContext *ctx, ARG_UNUSED const
 
 static FnCallResult FnCallTextXform(ARG_UNUSED EvalContext *ctx, ARG_UNUSED const Policy *policy, const FnCall *fp, const Rlist *finalargs)
 {
-    char buf[CF_BUFSIZE];
+    char *buf = NULL;
     char *string = RlistScalarValue(finalargs);
-    int len = 0;
+    size_t len = strlen(string);
+    size_t bufsiz = MAX(len + 1, PRINTSIZE(len));
 
-    memset(buf, 0, sizeof(buf));
-    strlcpy(buf, string, sizeof(buf));
-    len = strlen(buf);
+    buf = xcalloc(bufsiz, sizeof(char));
+    memcpy(buf, string, len + 1);
 
     if (!strcmp(fp->name, "string_downcase"))
     {
@@ -1392,7 +1392,7 @@ static FnCallResult FnCallTextXform(ARG_UNUSED EvalContext *ctx, ARG_UNUSED cons
     }
     else if (!strcmp(fp->name, "string_length"))
     {
-        xsnprintf(buf, sizeof(buf), "%d", len);
+        xsnprintf(buf, bufsiz, "%d", len);
     }
     else if (!strcmp(fp->name, "string_head"))
     {
@@ -1402,7 +1402,7 @@ static FnCallResult FnCallTextXform(ARG_UNUSED EvalContext *ctx, ARG_UNUSED cons
             Log(LOG_LEVEL_ERR, "string_head called with negative value %ld", max);
             return FnFailure();
         }
-        else if (max < sizeof(buf))
+        else if (max < bufsiz)
         {
             buf[max] = '\0';
         }
@@ -1417,16 +1417,17 @@ static FnCallResult FnCallTextXform(ARG_UNUSED EvalContext *ctx, ARG_UNUSED cons
         }
         else if (max < len)
         {
-            strncpy(buf, string + len - max, sizeof(buf) - 1);
+            memcpy(buf, string + len - max, max + 1);
         }
     }
     else
     {
         Log(LOG_LEVEL_ERR, "text xform with unknown call function %s, aborting", fp->name);
+        free(buf);
         return FnFailure();
     }
 
-    return FnReturn(buf);
+    return FnReturnNoCopy(buf);
 }
 
 /*********************************************************************/
@@ -5188,17 +5189,17 @@ static FnCallResult FnCallReadFile(ARG_UNUSED EvalContext *ctx, ARG_UNUSED const
 {
     char *filename = RlistScalarValue(finalargs);
     char *requested_max = RlistScalarValue(finalargs->next);
-    int maxsize = IntFromString(requested_max);
+    long maxsize = IntFromString(requested_max);
 
-    if (maxsize > CF_BUFSIZE)
+    if (maxsize > CF_INFINITY)
     {
-        Log(LOG_LEVEL_INFO, "%s: requested max size %s is more than the internal limit " TOSTRING(CF_BUFSIZE), fp->name, requested_max);
-        maxsize = CF_BUFSIZE;
+        Log(LOG_LEVEL_INFO, "%s: requested max size %s is more than the internal limit " TOSTRING(CF_INFINITY), fp->name, requested_max);
+        maxsize = CF_INFINITY;
     }
 
     if (maxsize == 0)
     {
-        maxsize = CF_BUFSIZE;
+        maxsize = CF_INFINITY;
     }
 
     if (maxsize < 0)

tests/acceptance/01_vars/02_functions/readfile.cf
@@ -24,13 +24,13 @@ bundle agent check
 {
   vars:
       "sizes" ilist => { @(test.sizes) };
-      "expected[0]" int => "4095";
+      "expected[0]" int => "10240";
       "expected[1]" int => "1";
       "expected[4094]" int => "4094";
       "expected[4095]" int => "4095";
-      "expected[4096]" int => "4095";
-      "expected[4097]" int => "4095";
-      "expected[99999999]" int => "4095";
+      "expected[4096]" int => "4096";
+      "expected[4097]" int => "4097";
+      "expected[99999999]" int => "10240";
 
   classes:
       "ok_$(sizes)" expression => strcmp("$(test.length_$(sizes))",

commit cdb2fd50d28f73ed0594bac8d61634f38bbd33d3

Fix memory leaks introduced by c4eb285 in FnCallTextXform

libpromises/evalfunction.c
@@ -1400,6 +1400,7 @@ static FnCallResult FnCallTextXform(ARG_UNUSED EvalContext *ctx, ARG_UNUSED cons
         if (max < 0)
         {
             Log(LOG_LEVEL_ERR, "string_head called with negative value %ld", max);
+			free(buf);
             return FnFailure();
         }
         else if (max < bufsiz)
@@ -1413,6 +1414,7 @@ static FnCallResult FnCallTextXform(ARG_UNUSED EvalContext *ctx, ARG_UNUSED cons
         if (max < 0)
         {
             Log(LOG_LEVEL_ERR, "string_tail called with negative value %ld", max);
+			free(buf);
             return FnFailure();
         }
         else if (max < len)
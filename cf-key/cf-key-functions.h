c8f8f79599a5bab6c0b5cc9a8154dd8c77526b46
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2015-04-15 17:36:50 +0200

    Merge branch 'issue-3074' of https://github.com/gc3-uzh-ch/cfengine into cfkey_trust_key-redmine3074
    Manual merge of pull request #1223 from @riccardomurri.
    
    Updated to have only one --trust-key option that works for both client
    and server, i.e. writes the lastseen database in both cases.
    
    Conflicts:
    	cf-key/cf-key-functions.c
    	cf-key/cf-key.c
    
    (cherry picked from commit 95a04561cfee5533db9ea68ea9d4f84954fab911)
    
    Conflicts:
    	cf-key/cf-key-functions.c

--- cf-key/cf-key-functions.h
+++ cf-key/cf-key-functions.h
@@ -40,9 +40,11 @@
 #include <crypto.h>
 
 RSA* LoadPublicKey(const char* filename);
-char* GetPubkeyDigest(const char* pubkey);
+char* LoadPubkeyDigest(const char* pubkey);
+char* GetPubkeyDigest(RSA* pubkey);
 int PrintDigest(const char* pubkey);
-int TrustKey(const char* pubkey);
+void ParseKeyArg(char* keyarg, char** filename, char** ipaddr, char** username);
+int TrustKey(const char* pubkey, const char* ipaddress, const char* username);
 bool ShowHost(const char *hostkey, const char *address, bool incoming, const KeyHostSeen *quality, void *ctx);
 void ShowLastSeenHosts();
 int RemoveKeys(const char *input, bool must_be_coherent);

0121894159c4f11486ee825e0aaf9b525f3030ce
Nick Anderson <nick@cmdln.org>
2015-06-29 15:24:23 -0500

    Change: Improve git based release id
    Now cf-promises -T is not limited to working when you are on a checked
    out branch. Now you get the commit that is checked out as the release id
    even if you are checked out from a specific commit or tag.
    (cherry picked from commit 1332ba372b55bfe61070028cc047d77f5d0a25eb)

--- libpromises/generic_agent.c
+++ libpromises/generic_agent.c
@@ -809,6 +809,8 @@ static bool GeneratePolicyReleaseIDFromGit(char *release_id_out,
     snprintf(git_filename, PATH_MAX, "%s/.git/HEAD", policy_dir);
     MapName(git_filename);
 
+    // Note: Probably we should not be reading all of these filenames directly,
+    // and should instead use git plumbing commands to retrieve the data.
     FILE *git_file = fopen(git_filename, "r");
     if (git_file)
     {
@@ -817,13 +819,30 @@ static bool GeneratePolicyReleaseIDFromGit(char *release_id_out,
         fclose(git_file);
 
         if (scanned == 1)
+        // Found HEAD Reference which means we are on a checked out branch
         {
             snprintf(git_filename, PATH_MAX, "%s/.git/%s", policy_dir, git_head);
             git_file = fopen(git_filename, "r");
+            Log(LOG_LEVEL_DEBUG, "Found a git HEAD ref");
         }
         else
         {
-            git_file = NULL;
+            Log(LOG_LEVEL_DEBUG, "Unable to find HEAD ref in %s. Looking for commit", git_file);
+            assert(out_size > 40);
+            git_file = fopen(git_filename, "r");
+            scanned = fscanf(git_file, "%40s", release_id_out);
+            fclose(git_file);
+
+            if (scanned == 1)
+            {
+                Log(LOG_LEVEL_DEBUG, "Found current git checkout pointing to %s", release_id_out);
+                return scanned == 1;
+            }
+            else
+            // We didnt find a commit sha in .git/HEAD, so we assume the git information is invalid
+            {
+                git_file = NULL;
+            }
         }
         if (git_file)
         {

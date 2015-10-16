501dd148a48297f90efe298f421d24394b9ef531
Benoît Peccatte <benoit.peccatte@normation.com>
2015-10-16 09:45:56 +0200

    Cleanup @if feature PR

--- libpromises/feature.c
+++ libpromises/feature.c
@@ -6,34 +6,34 @@
 
 static const char* features[] = {
 #ifdef HAVE_LIBYAML
-  "yaml",
+    "yaml",
 #endif
 #ifdef HAVE_LIBXML2
-  "xml2",
+    "xml2",
 #endif
 
-  NULL
+    NULL
 };
 
 int KnownFeature(const char *feature)
 {
-  // dumb algorithm, but still effective for a small number of features
-  for(int i=0 ; features[i]!=NULL ; i++) {
-    int r = strcmp(feature, features[i]);
-    if(r==0) {
-      return 1;
+    // dumb algorithm, but still effective for a small number of features
+    for(int i=0 ; features[i]!=NULL ; i++) {
+        int r = strcmp(feature, features[i]);
+        if(r==0) {
+            return 1;
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
-void CreateHardClassesFromfeatures(EvalContext *ctx, char *tags)
+void CreateHardClassesFromFeatures(EvalContext *ctx, char *tags)
 {
-  Buffer *buffer = BufferNew();
+    Buffer *buffer = BufferNew();
 
-  for(int i=0 ; features[i]!=NULL ; i++) {
-    BufferPrintf(buffer, "feature_%s", features[i]);
-    CreateHardClassesFromCanonification(ctx, BufferData(buffer), tags);
-  }
-  BufferDestroy(buffer);
+    for(int i=0 ; features[i]!=NULL ; i++) {
+        BufferPrintf(buffer, "feature_%s", features[i]);
+        CreateHardClassesFromCanonification(ctx, BufferData(buffer), tags);
+    }
+    BufferDestroy(buffer);
 }

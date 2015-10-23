656d9d3b39787eaa1d9787eb082d6139f4c777ed
root <root@jpmc-hub-1.(none)>
2015-10-23 12:48:16 +0200

    Adding support for 3.7.1 + tests

--- cf-profile.pl
+++ cf-profile.pl
@@ -30,6 +30,7 @@ my %data = ();
 my $debug = 0;
 my $line = "";
 my $cur_bundle = "";
+my $cur_bundle_name = "";
 my $cur_bundle_key = "";
 my $promise_type = "";
 my $iter = "";
@@ -40,9 +41,13 @@ my $is_edit_bundle = 0;
 my @b_log = ();
 my $tabber = " " x 4;
 my $branch = "-" x 4;
+my $version = 0;
+my $namespace = "";
+my $b_log_ref = undef;
+
 $line = <STDIN>;
 
-getopts("T:dhstva", \%opts);
+getopts("T:dhstvac", \%opts);
 
 if(defined($opts{h})){
 	usage();
@@ -65,13 +70,21 @@ $data{start} = Time::HiRes::gettimeofday();
 
 if($line =~ /^.*> /) {
 	debug("Found output version < 3.5.0");
+	$version = 1;
 	prelude_v1();
 	bundles_v1();
 }else{
-	die("This program is currently working with legacy output of cfengine < 3.5.0. If you are on
-cfengine >= 3.5.0, try using the -l or --legacy-output switches for cf-agent.");
+	debug("Trying to parse new format output");
+	$version = 2;
+	prelude_v2();
+	bundles_v2();
 }
 
+if(defined($opts{c}) or defined($opts{a})){
+	print "===============================================================================\n";
+	print "Classes defined:\n";
+	print "$data{all_classes}\n";
+}
 if(defined($opts{t}) or defined($opts{a})){
 	print "===============================================================================\n";
 	print "Execution tree\n";
@@ -84,10 +97,10 @@ if(defined($opts{t}) or defined($opts{a})){
 		my $rel_start = sprintf("%.5f", $data{bundles}{$b}{start} - $data{start});
 		my $rel_stop = sprintf("%.5f", $data{bundles}{$b}{stop} - $data{start});
 		my $tab = "$tabber"x$data{bundles}{$b}{level};
-	#	my $tab = "      "x$data{bundles}{$b}{level};
 		my $header = "$branch"x$data{bundles}{$b}{level};
-	#	my $header = "-----"x$data{bundles}{$b}{level};
-		print "|$header> Bundle $b\n";
+		my $fq_b = $data{bundles}{$b}{name};
+		$fq_b = (defined($data{bundles}{$b}{namespace}))? $data{bundles}{$b}{namespace}.":".$fq_b : $fq_b;
+		print "|$header> Bundle $fq_b\n";
 		print "|$tab"."$tab"."Start: $data{bundles}{$b}{start} s\n" if defined($opts{v});
 		print "|$tab"."$tab"."Stop: $data{bundles}{$b}{stop} s\n" if defined($opts{v});
 		print "|$tab"."$tab"."Elapsed: $elapsed s\n";
@@ -112,17 +125,17 @@ if(defined($opts{t}) or defined($opts{a})){
 	$data{stop} = Time::HiRes::gettimeofday();
 	print "|\n";
 	print "Stop: $data{stop} s\n";
-	print "===============================================================================\n";
-	print "\n";
 }
 
-if(defined($opts{s}) or defined($opts{a})){
-	print "===============================================================================\n";
-	print "Summary\n";
-	print "===============================================================================\n";
-	print "Top 10 worst, bundles:\n";
-	print "Top 10 worst, promise types:\n";
-}
+#if(defined($opts{s}) or defined($opts{a})){
+#	print "===============================================================================\n";
+#	print "Summary\n";
+#	print "===============================================================================\n";
+#	print "Top 10 worst, bundles:\n";
+#	print "Top 10 worst, promise types:\n";
+#}
+print "===============================================================================\n";
+print "\n";
 
 exit(0);
 
@@ -149,16 +162,31 @@ sub debug {
 	print "DEBUG: $msg\n" if $debug;
 }
 
+
+
 sub prelude_v1{
 	$line = <STDIN>;
 	do {
 		if($line =~ /(Defined|Hard)\s+classes\s+=\s+\{\s+(.*)\s*\}/){
-			$data{all_classes} = $2;
-			debug("Found classes: \"$2\"");
+			classes($2);
 			return 0;
 		}
 	} while($line = <STDIN>)
 }
+
+sub prelude_v2 {
+	$line = <STDIN>;
+	my @classes = ();
+	do {
+		if($line =~ /^\s*verbose:\s+C:\s+(discovered\s+hard|added\s+soft)\s+class\s+(.*)$/){
+			$classes[$#classes + 1] = $2;
+		}elsif($line =~ /^\s*verbose:\s+Preliminary\s+variable\/class-context\s+convergence\s*$/) {
+			classes(join(' ',@classes));
+			return 0;
+		}
+	} while($line = <STDIN>)
+}
+
 
 sub bundles_v1 {
 	do {
@@ -177,40 +205,102 @@ sub bundles_v1 {
 					}
 				}
 				chomp($args);
-				debug("Found bundle $bundle $args");
-				$cur_bundle = $bundle;
-				$cur_bundle_key = $bundle.":".$args;
-				$data{bundles}{$cur_bundle_key}{start} = Time::HiRes::gettimeofday();
-				$b_log[$#b_log + 1] = $cur_bundle_key;
+				bundle_start($bundle,$args);
 			}
 			$is_edit_bundle = 0;
-		} elsif ($line =~ /(\S+)\s+in\s+bundle\s+$cur_bundle\s+\((\d+)\)/){
+		} elsif ($line =~ /(\S+)\s+in\s+bundle\s+$cur_bundle_name\s+\((\d+)\)/){
 			$promise_type = $1;
 			$iter = $2;
-			$data{bundles}{$cur_bundle_key}{prtype}[$#{$data{bundles}{$cur_bundle_key}{prtype}} + 1] = $promise_type.":".$iter;
-			$data{bundles}{$cur_bundle_key}{promise_types}{$promise_type.":".$iter}{start} = Time::HiRes::gettimeofday();
-			if($promise_type =~ /^methods$/ && ! grep(/$cur_bundle/,@parent)){
-				debug("Registering parent $cur_bundle");
-				push(@parent,$cur_bundle);
-				push(@parent_keys,$cur_bundle_key);
-			}
-			debug("Found $promise_type in bundle $cur_bundle iter $iter");
+			bundle_promise($promise_type,$iter);
 		}elsif($line =~ /(Bundle\s+Accounting\s+Summary\s+for|Zero\s+promises\s+executed\s+for\s+bundle)\s+\"*\'*(\w+)\"*\'*/) {
 			my $b = $2;
-			debug("End $b");
-			if($#parent >= 0 && $parent[$#parent] =~ /$b/) {
-				pop(@parent);
-				my $p = pop(@parent_keys);
-				$data{bundles}{$p}{level} = $#parent + 2;
-				$data{bundles}{$p}{stop} = Time::HiRes::gettimeofday();
-			}else{
-				$data{bundles}{$cur_bundle_key}{stop} = Time::HiRes::gettimeofday();
-				$data{bundles}{$cur_bundle_key}{level} = $#parent + 2;
-			}
+			bundle_end($b);
 #		}elsif ($line =~ /\s+(\+|\-)\s+(\S+)$/){
 #			$data{bundles}{$cur_bundle_key}{promise_types}{$promise_type.":".$iter}{classes}[$#{$data{bundles}{$cur_bundle_key}{promise_types}{$promise_type.":".$iter}{classes}} + 1] = "$1$2";
 		}elsif($line =~ /(defining\s+explicit\s+local\s+bundle\s+class\s+|defining\s+promise\s+result\s+class\s+)(\S+)/){
 			$data{bundles}{$cur_bundle_key}{promise_types}{$promise_type.":".$iter}{classes}[$#{$data{bundles}{$cur_bundle_key}{promise_types}{$promise_type.":".$iter}{classes}} + 1] = "+$2";
 		}
 	} while($line = <STDIN>)
+}
+
+sub bundles_v2 {
+	$line = <STDIN>;
+	
+	do {
+		if($line =~ /BEGIN\s+bundle\s+(\w+)(\(?\s*\{?[^\}]+\}?\s?\)?)?/){
+			if(!$is_edit_bundle) {
+				my $bundle = $1;
+				#my $args = $2;
+				my $args = (defined($2))? $2 : "";
+				chomp $args;
+				#if($args) {
+				#	while($line !~ /\'\}\s+\)$/){
+				#		$line = <STDIN>;
+				#		$args .= $line;
+				#	}
+				#}
+				chomp($args);
+				bundle_start($bundle,$args);
+			}
+			$is_edit_bundle = 0;
+		} elsif ($line =~ /^\s+verbose:\s+P:\s+BEGIN\s+promise\s+'\w+'\s+of\s+type\s+"([^"]+)"\s+\(pass\s+(\d)\)\s*$/){
+			$promise_type = $1;
+			$iter = $2;
+			bundle_promise($promise_type,$iter);
+		} elsif($line =~ /^\s+verbose:\s+A:\s+Bundle\s+Accounting\s+Summary\s+for\s+'(\S+)'\s+in\s+namespace\s+(\S+).*$/){
+			$data{bundles}{$cur_bundle_key}{namespace} = $2;
+		} elsif($line =~ /^\s+verbose:\s+B:\s+END\s+bundle\s+(\w+)/) {
+			my $b = $1;
+			bundle_end($b);
+		}
+	} while($line = <STDIN>)
+}
+
+sub classes {
+	my $classes = shift;
+	$data{all_classes} = $classes;
+	debug("Found classes: \"$classes\"");
+	return 0;
+}
+
+sub bundle_start {
+	my ($bundle,$args) = @_;
+	debug("bundle_start: Found bundle $bundle $args");
+	$cur_bundle = "$bundle:l$.";
+	$cur_bundle_name = $bundle;
+	$cur_bundle_key = "${bundle}${args}:l$.";
+	$data{bundles}{$cur_bundle_key}{name} = "${bundle}${args}";
+	$data{bundles}{$cur_bundle_key}{start} = Time::HiRes::gettimeofday();
+	$b_log[$#b_log + 1] = $cur_bundle_key;
+}
+
+sub bundle_promise {
+	my ($promise_type,$iter) = @_;
+	if(!grep(/^$promise_type:$iter$/, @{$data{bundles}{$cur_bundle_key}{prtype}})){
+		$data{bundles}{$cur_bundle_key}{prtype}[$#{$data{bundles}{$cur_bundle_key}{prtype}} + 1] = $promise_type.":".$iter;
+		$data{bundles}{$cur_bundle_key}{promise_types}{$promise_type.":".$iter}{start} = Time::HiRes::gettimeofday();
+	}
+	if($promise_type =~ /^methods$/ && ! grep(/^$cur_bundle$/, @parent) && !defined($data{bundles}{$cur_bundle_key}{stop})){
+		debug("Registering parent $cur_bundle");
+		push(@parent,$cur_bundle);
+		push(@parent_keys,$cur_bundle_key);
+	}
+	debug("Found $promise_type in bundle $cur_bundle iter $iter");
+}
+
+sub bundle_end {
+	my $b = shift;
+	debug("End $b");
+	my $b_key = $cur_bundle_key;
+	if($#parent >= 0 && $parent[$#parent] =~ /^${b}:l\d+$/) {
+		debug("Found parent end $b");
+		pop(@parent);
+		$b_key = pop(@parent_keys);
+	}
+	debug("Registering end data for $b_key");
+	$data{bundles}{$b_key}{level} = $#parent + 2;
+	$data{bundles}{$b_key}{stop} = Time::HiRes::gettimeofday();
+	$cur_bundle_key = (@parent_keys)? $parent_keys[$#parent_keys] : $cur_bundle_key;
+	$cur_bundle = (@parent)? $parent[$#parent] : $cur_bundle;
+	$cur_bundle_name = (split/:l\d+/,$cur_bundle)[0];
 }

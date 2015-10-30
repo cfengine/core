fa2c33af0caa7dca70093b013a3c072bf99fc129
Kristian Amlie <kristian.amlie@cfengine.com>
2015-10-30 10:12:14 +0100

    Fix init_script_test_helper not being cleaned properly.
    Instead of making a separate clean target, list it in PROGRAMS
    instead, which is better automake style. This implicitly invokes
    libtool as well, so adapt the test to account for that.

--- tests/unit/init_script_test.sh
+++ tests/unit/init_script_test.sh
@@ -78,10 +78,12 @@ fi
 # Fail on any error.
 set -e
 
-cp init_script_test_helper $CFTEST_PREFIX/bin/cf-test-execd
-cp init_script_test_helper $CFTEST_PREFIX/bin/cf-test-serverd
-cp init_script_test_helper $CFTEST_PREFIX/bin/cf-test-monitord
-cp init_script_test_helper $CFTEST_PREFIX/bin/cf-test-agent
+INSTALL=${INSTALL:-/usr/bin/install}
+
+../../libtool --mode=install $INSTALL init_script_test_helper $CFTEST_PREFIX/bin/cf-test-execd
+../../libtool --mode=install $INSTALL init_script_test_helper $CFTEST_PREFIX/bin/cf-test-serverd
+../../libtool --mode=install $INSTALL init_script_test_helper $CFTEST_PREFIX/bin/cf-test-monitord
+../../libtool --mode=install $INSTALL init_script_test_helper $CFTEST_PREFIX/bin/cf-test-agent
 
 touch $CFTEST_PREFIX/inputs/promises.cf
 

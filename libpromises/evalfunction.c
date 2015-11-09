b0da9767e5819b6c61c70a1b0b8bed25a680a12c
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2015-11-09 17:49:42 +0100

    Redmine#7702: readfile() and read*list() should print an error if they fail to read file.
    Changelog: title
    (cherry picked from commit b27e1257016609ae5e14d6c386b23ad649362e75)

--- libpromises/evalfunction.c
+++ libpromises/evalfunction.c
@@ -5593,6 +5593,8 @@ static FnCallResult FnCallReadFile(ARG_UNUSED EvalContext *ctx, ARG_UNUSED const
         return FnReturnNoCopy(contents);
     }
 
+    Log(LOG_LEVEL_ERR, "Function '%s' failed to read file: %s",
+        fp->name, filename);
     return FnFailure();
 }
 
@@ -5609,6 +5611,8 @@ static FnCallResult ReadList(ARG_UNUSED EvalContext *ctx, ARG_UNUSED const FnCal
     char *file_buffer = CfReadFile(filename, maxsize);
     if (!file_buffer)
     {
+        Log(LOG_LEVEL_ERR, "Function '%s' failed to read file: %s",
+            fp->name, filename);
         return FnFailure();
     }
 

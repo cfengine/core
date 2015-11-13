cfengine/core

commit f8c7dca55ed8d99212564ea130666de4bd5e0931

Patch for bug 2315 and 1525 - non convergent editline with preserve block

cf-agent/files_editline.c
@@ -574,6 +574,7 @@ static PromiseResult VerifyLineInsertions(EvalContext *ctx, const Promise *pp, E
     /* Are we working in a restricted region? */
 
     PromiseResult result = PROMISE_RESULT_NOOP;
+
     if (!a.haveregion)
     {
         begin_ptr = *start;
@@ -736,6 +737,11 @@ static int InsertMultipleLinesToRegion(EvalContext *ctx, Item **start, Item *beg
     {
         for (ip = *start; ip != NULL; ip = ip->next)
         {
+            if (MatchRegion(ctx, pp->promiser, ip, end_ptr, true))
+            {
+                return false;
+            }
+
             if (ip->next != NULL && ip->next == end_ptr)
             {
                 return InsertMultipleLinesAtLocation(ctx, start, begin_ptr, end_ptr, ip, prev, a, pp, edcontext, result);

tests/acceptance/10_files/09_insert_lines/109.cf
@@ -1,0 +1,121 @@
+#######################################################
+#
+# Insert lines with preserve_block "after" and check convergence
+#
+#######################################################
+
+body common control
+{
+      inputs => { "../../default.cf.sub" };
+      bundlesequence  => { default("$(this.promise_filename)") };
+      version => "1.0";
+}
+
+#######################################################
+
+bundle agent init
+{
+  vars:
+      "states" slist => { "actual", "expected" };
+
+      "actual" string => "START";
+
+      "expected" string =>
+"START
+        1
+        x2x
+       xx3xx
+        x4x
+         5
+        x6x
+       xx7xx
+      xxx8xxx
+     xxxx9xxxx
+    xxxxx10xxxxx
+   xxxxxx11xxxxx
+  xxxxxxx12xxxxxx
+ xxxxxxxx13xxxxxxx
+xxxxxxxxx14xxxxxxxx
+xxxxxxxxx15xxxxxxxx
+      xxx16xx
+      xxx17xx
+      xxx18xx
+      xxx19xx
+      xxx20xx
+xxxxxxxxx21xxxxxxx";
+
+  files:
+      "$(G.testfile).$(states)"
+      create => "true",
+      edit_line => init_insert("$(init.$(states))"),
+      edit_defaults => init_empty;
+}
+
+bundle edit_line init_insert(str)
+{
+  insert_lines:
+      "$(str)";
+}
+
+body edit_defaults init_empty
+{
+      empty_file_before_editing => "true";
+}
+
+#######################################################
+
+bundle agent test
+{
+  files:
+      "$(G.testfile).actual"
+      edit_line => test_insert("$(init.insert)");
+}
+
+bundle edit_line test_insert(lines)
+{
+insert_lines:
+
+"        1
+        x2x
+       xx3xx
+        x4x
+         5
+        x6x
+       xx7xx
+      xxx8xxx
+     xxxx9xxxx
+    xxxxx10xxxxx
+   xxxxxx11xxxxx
+  xxxxxxx12xxxxxx
+ xxxxxxxx13xxxxxxx
+xxxxxxxxx14xxxxxxxx
+xxxxxxxxx15xxxxxxxx
+      xxx16xx
+      xxx17xx
+      xxx18xx
+      xxx19xx
+      xxx20xx
+xxxxxxxxx21xxxxxxx"
+
+      location => prepend,
+   insert_type => "preserve_block";
+}
+
+body location prepend
+{
+before_after => "after";
+}
+
+#######################################################
+
+bundle agent check
+{
+  methods:
+      "any" usebundle => default_check_diff("$(G.testfile).actual",
+                                            "$(G.testfile).expected",
+                                            "$(this.promise_filename)");
+}
+
+
+### PROJECT_ID: core
+### CATEGORY_ID: 27

commit a251ebd0ab34c5240b1b65a25dc3f3b402967110

Redmine #7460: Unable to edit the same value in multiple regions.
The issue was that even though we've been editing value in selected
region the whole file was checked to see if value was already edited.
This was broken by commit f8c7dca55ed8d99212564ea130666de4bd5e0931 when
check if line exists in region was added.

As a bonus some optimizations are added. Instead of iterating
through whole file we can now iterate only over selected region as
any operations will be done inside the region (no need to check whole
file).

Changelog: It is possible to edit the same value in multiple regions
of one file. (Redmine #7460)

cf-agent/files_editline.c
@@ -888,7 +888,10 @@ static int InsertMultipleLinesToRegion(EvalContext *ctx, Item **start, Item *beg
 
     if (a.location.before_after == EDIT_ORDER_BEFORE)
     {
-        for (ip = *start; ip != NULL; ip = ip->next)
+        /* As region was already selected by SelectRegion() and we know
+         * what are the region boundaries (begin_ptr and end_ptr) there
+         * is no reason to iterate over whole file. */
+        for (ip = begin_ptr; ip != NULL; ip = ip->next)
         {
             if (ip == begin_ptr)
             {
@@ -903,7 +906,13 @@ static int InsertMultipleLinesToRegion(EvalContext *ctx, Item **start, Item *beg
 
     if (a.location.before_after == EDIT_ORDER_AFTER)
     {
-        for (ip = *start; ip != NULL; ip = ip->next)
+        /* As region was already selected by SelectRegion() and we know
+         * what are the region boundaries (begin_ptr and end_ptr) there
+         * is no reason to iterate over whole file. It is safe to start from
+         * begin_ptr. 
+         * As a bonus Redmine #7640 is fixed as we are not interested in
+         * matching values outside of the region we are iterating over. */
+        for (ip = begin_ptr; ip != NULL; ip = ip->next)
         {
             if (!allow_multi_lines && MatchRegion(ctx, pp->promiser, ip, end_ptr, false))
             {

tests/acceptance/10_files/09_insert_lines/same_text_in_two_regions.cf
@@ -8,10 +8,6 @@ body common control
 
 bundle agent init
 {
-  meta:
-      "test_soft_fail" string => "any",
-        meta => { "redmine7460" };
-
   methods:
       "any" usebundle => file_make("$(G.testfile)", "block1
 block2
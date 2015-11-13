5872a2e07bf5f132bcb51e4068eadff3b294f845
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2015-11-13 20:16:31 +0100

    'test -e' is not valid under Solaris /bin/sh.

--- tests/acceptance/write_args.sh
+++ tests/acceptance/write_args.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# WARNING keep this /bin/sh compatible!
 
 # Outputs to file $1 all the rest of the arguments
 
@@ -8,13 +9,14 @@ then
     echo "ERROR: At least one argument OUTPUT_FILE expected!"
     exit 1
 fi
-if [ -e "$OUTFILE" ]
+
+OUTFILE="$1"
+if [ -f "$OUTFILE" ]
 then
-    echo "ERROR: output file $1 already exists, I refuse to overwrite it!"
+    echo "ERROR: already exists, I refuse to overwrite file: $OUTFILE"
     exit 1
 fi
 
 
-OUTFILE="$1"
 shift
 echo "$*" > "$OUTFILE"

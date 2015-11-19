1663def5d9a9965aa7b8e1742cb80f2bf9b91066
Mike Weilgart <mweilgart@directv.com>
2015-11-19 12:30:01 -0800

    Move naming of staging dir from common.sh to stage script
    Staging dir was called $TMP and named based on $PARAMS.
    
    The $PARAMS variable shouldn't be used within common.sh
    as it makes the functions non-reusable (not a "common" file
    which can be re-used)
    
    Rename TMP to STAGING_DIR to be more descriptive
    and set STAGING_DIR within masterfiles-stage.sh
    (based on $PARAMS as before).

--- contrib/masterfiles-stage/common.sh
+++ contrib/masterfiles-stage/common.sh
@@ -35,55 +35,45 @@ git_branch_masterstage() {
     error_exit "git not found on path: ${PATH}"
   fi
 
-  # We probably want a different temporary location for each remote repository
-  # so that we can avoid conflicts and potential confusion.
-  # Example: 
-  # ROOT="/opt/cfengine/masterfiles_staging"
-  # PARAMS="/var/cfengine/policychannel/production_1.sh"
-  # TMP=/opt/cfengine/masterfiles/staging/_tmp_var_cfengine_policychannel_production_1_sh
-
-  TRANSLATED_PARAMS=$(echo $PARAMS | tr [./] _)
-  TMP="${ROOT}/_tmp${TRANSLATED_PARAMS}"
-
   # If we have a git checkout ensure the origin is set properly, update it and
   # make sure all modified, extra, or missing files are reset so that we have a
   # clean checkout
 
-  if [ -d "${TMP}/.git" ]; then
-      cd "${TMP}" && git remote set-url origin "${GIT_URL}" && (
+  if [ -d "${STAGING_DIR}/.git" ]; then
+      cd "${STAGING_DIR}" && git remote set-url origin "${GIT_URL}" && (
           git fetch -q origin || error_exit "Failed: git fetch -q origin"
           git stash -q  || error_exit "Failed: git stash -q"
           git checkout -q "${GIT_BRANCH}" || error_exit "Failed: git checkout -q ${GIT_BRANCH}"
           git reset -q --hard "origin/${GIT_BRANCH}"
       )
   else
-      rm -rf "${TMP}"/* "${TMP}"/.??*
-      git clone --no-hardlinks "${GIT_URL}" "${TMP}" && cd "${TMP}" && git checkout "${GIT_BRANCH}"
+      rm -rf "${STAGING_DIR}"/* "${STAGING_DIR}"/.??*
+      git clone --no-hardlinks "${GIT_URL}" "${STAGING_DIR}" && cd "${STAGING_DIR}" && git checkout "${GIT_BRANCH}"
   fi
 
-  if /var/cfengine/bin/cf-promises -T "${TMP}"; then
-    if ! /var/cfengine/bin/cf-promises -cf "${TMP}/update.cf"; then
-        error_exit "Update policy staged in ${TMP} could not be validated, aborting."
+  if /var/cfengine/bin/cf-promises -T "${STAGING_DIR}"; then
+    if ! /var/cfengine/bin/cf-promises -cf "${STAGING_DIR}/update.cf"; then
+        error_exit "Update policy staged in ${STAGING_DIR} could not be validated, aborting."
     fi
 
       # you could abort here if DIFFLINES is over 100, for instance (too many changes)
-      #DIFFLINES=$(/usr/bin/diff -r  -x .git -x cf_promises_validated -x cf_promises_release_id "${TMP}" "${MASTERDIR}" |/usr/bin/wc -l)
+      #DIFFLINES=$(/usr/bin/diff -r  -x .git -x cf_promises_validated -x cf_promises_release_id "${STAGING_DIR}" "${MASTERDIR}" |/usr/bin/wc -l)
 
       # roll out the release if the release IDs are different
       # ALWALSY SYNC THEM but what is the negative side effect? POTENTIALY CLIENTS ALWWAYS UPDATE?
       # BUT CPV only triggered on change, so maybe thats what we want.
-      #if /usr/bin/diff -q "${TMP}/cf_promises_release_id" "${MASTERDIR}/cf_promises_release_id" ; then
+      #if /usr/bin/diff -q "${STAGING_DIR}/cf_promises_release_id" "${MASTERDIR}/cf_promises_release_id" ; then
       #    echo "No release needs to be made, the release IDs are the same."
-      #    touch "${TMP}"
+      #    touch "${STAGING_DIR}"
       #else
           /bin/mkdir -p "${MASTERDIR}" || error_exit "Failed: Creating '${MASTERDIR}'"
-          cd "${TMP}" && (
-          chown -R root:root "${TMP}" && \
-          rsync -rltDE -c --delete-after --chmod=u+rwX,go-rwx "${TMP}/" "${MASTERDIR}/" && echo "Successfully staged a policy release on $(date)"
+          cd "${STAGING_DIR}" && (
+          chown -R root:root "${STAGING_DIR}" && \
+          rsync -rltDE -c --delete-after --chmod=u+rwX,go-rwx "${STAGING_DIR}/" "${MASTERDIR}/" && echo "Successfully staged a policy release on $(date)"
       )
       #fi
   else
-    error_exit "The staged policies in ${TMP} could not be validated, aborting."
+    error_exit "The staged policies in ${STAGING_DIR} could not be validated, aborting."
   fi
 }
 
@@ -105,21 +95,11 @@ git_tag_or_commit_masterstage() {
     error_exit "git not found on path: ${PATH}"
   fi
 
-  # We probably want a different temporary location for each remote repository
-  # so that we can avoid conflicts and potential confusion.
-  # Example: 
-  # ROOT="/opt/cfengine/masterfiles_staging"
-  # PARAMS="/var/cfengine/policychannel/production_1.sh"
-  # TMP=/opt/cfengine/masterfiles/staging/_tmp_var_cfengine_policychannel_production_1_sh
-
-  TRANSLATED_PARAMS=$(echo $PARAMS | tr [./] _)
-  TMP="${ROOT}/_tmp${TRANSLATED_PARAMS}"
-
   # If we have a git checkout ensure the origin is set properly, update it and
   # make sure all modified, extra, or missing files are reset so that we have a
   # clean checkout
-  if [ -d "${TMP}/.git" ]; then
-      cd "${TMP}" && git remote set-url origin "${GIT_URL}" && (
+  if [ -d "${STAGING_DIR}/.git" ]; then
+      cd "${STAGING_DIR}" && git remote set-url origin "${GIT_URL}" && (
           git fetch -q origin || error_exit "Failed: git fetch -q origin"
           git stash -q  || error_exit "Failed: git stash -q"
           git checkout -q "${GIT_TAG_OR_COMMIT}" || error_exit "Failed: git checkout -q ${GIT_TAG_OR_COMMIT}"
@@ -132,25 +112,25 @@ git_tag_or_commit_masterstage() {
   # If we don't have a git clone wipe the directory and create a new clone and
   # ensure the proper branch/tag is checked out.
   else
-      rm -rf "${TMP}"/* "${TMP}"/.??*
-      git clone --no-hardlinks "${GIT_URL}" "${TMP}" && cd "${TMP}" && git checkout "${GIT_TAG_OR_COMMIT}" || error_exit "Failed to stage '${GIT_TAG_OR_COMMIT}' from '${GIT_URL}'"
+      rm -rf "${STAGING_DIR}"/* "${STAGING_DIR}"/.??*
+      git clone --no-hardlinks "${GIT_URL}" "${STAGING_DIR}" && cd "${STAGING_DIR}" && git checkout "${GIT_TAG_OR_COMMIT}" || error_exit "Failed to stage '${GIT_TAG_OR_COMMIT}' from '${GIT_URL}'"
   fi
 
-  if /var/cfengine/bin/cf-promises -T "${TMP}"; then
-    if ! /var/cfengine/bin/cf-promises -cf "${TMP}/update.cf"; then
-        error_exit "Update policy staged in ${TMP} could not be validated, aborting."
+  if /var/cfengine/bin/cf-promises -T "${STAGING_DIR}"; then
+    if ! /var/cfengine/bin/cf-promises -cf "${STAGING_DIR}/update.cf"; then
+        error_exit "Update policy staged in ${STAGING_DIR} could not be validated, aborting."
     fi
       # roll out the release if the release IDs are different
-#      if /usr/bin/diff -q "${TMP}/cf_promises_release_id" "${MASTERDIR}/cf_promises_release_id" ; then
+#      if /usr/bin/diff -q "${STAGING_DIR}/cf_promises_release_id" "${MASTERDIR}/cf_promises_release_id" ; then
 #          #echo "No release needs to be made, the release IDs are the same."
-#          touch "${TMP}"
+#          touch "${STAGING_DIR}"
 #      else
-          cd "${TMP}" && (
-          chown -R root:root "${TMP}" && rsync -rltDE -c --delete-after --chmod=u+rwX,go-rwx "${TMP}/" "${MASTERDIR}/" && echo "Successfully staged a policy release of '${GIT_TAG_OR_COMMIT}' from '${GIT_URL}' to '${MASTERDIR}' on $(date)"
+          cd "${STAGING_DIR}" && (
+          chown -R root:root "${STAGING_DIR}" && rsync -rltDE -c --delete-after --chmod=u+rwX,go-rwx "${STAGING_DIR}/" "${MASTERDIR}/" && echo "Successfully staged a policy release of '${GIT_TAG_OR_COMMIT}' from '${GIT_URL}' to '${MASTERDIR}' on $(date)"
       )
 #      fi
   else
-    error_exit "The staged policies in ${TMP} could not be validated, aborting."
+    error_exit "The staged policies in ${STAGING_DIR} could not be validated, aborting."
   fi
 }
 
@@ -163,40 +143,30 @@ svn_branch() {
 
     CHECKSUM_FILE="svn_promise_checksums"
 
-    # We probably want a different temporary location for each remote repository
-    # so that we can avoid conflicts and potential confusion.
-    # Example: 
-    # ROOT="/opt/cfengine/masterfiles_staging"
-    # PARAMS="/var/cfengine/policychannel/production_1.sh"
-    # TMP=/opt/cfengine/masterfiles/staging/_tmp_var_cfengine_policychannel_production_1_sh
-
-    TRANSLATED_PARAMS=$(echo $PARAMS | tr [./] _)
-    TMP="${ROOT}/_tmp${TRANSLATED_PARAMS}"
-
     # If we already have a checkout, update it, else make a new checkout.
-    if [ -d "${TMP}/.svn" ] ; then
-	svn update --quiet ${TMP}
+    if [ -d "${STAGING_DIR}/.svn" ] ; then
+	svn update --quiet ${STAGING_DIR}
     else
-	rm -rf "${TMP}"
-	svn checkout --quiet "${SVN_URL}"/"${SVN_BRANCH}"/inputs "${TMP}"
+	rm -rf "${STAGING_DIR}"
+	svn checkout --quiet "${SVN_URL}"/"${SVN_BRANCH}"/inputs "${STAGING_DIR}"
     fi
 
-    rm -f "${TMP}/cf_promises_release_id"
+    rm -f "${STAGING_DIR}/cf_promises_release_id"
 
-    if /var/cfengine/bin/cf-promises -T "${TMP}"; then
-	md5sum `find ${TMP} -type f -name \*.cf` >"${TMP}/${CHECKSUM_FILE}"
-	if /usr/bin/diff -q "${TMP}/${CHECKSUM_FILE}" "${MASTERDIR}/${CHECKSUM_FILE}" ; then
+    if /var/cfengine/bin/cf-promises -T "${STAGING_DIR}"; then
+	md5sum `find ${STAGING_DIR} -type f -name \*.cf` >"${STAGING_DIR}/${CHECKSUM_FILE}"
+	if /usr/bin/diff -q "${STAGING_DIR}/${CHECKSUM_FILE}" "${MASTERDIR}/${CHECKSUM_FILE}" ; then
             # echo "No release needs to be made, the checksum files are the same"
-            touch "${TMP}"
+            touch "${STAGING_DIR}"
 	else
-            cd "${TMP}" && (
-		chown -R root:root "${TMP}" && \
-                rsync -CrltDE -c --delete-after --chmod=u+rwX,go-rwx "${TMP}/" "${MASTERDIR}/" && \
-                rm -rf ${TMP}/.svn && \
+            cd "${STAGING_DIR}" && (
+		chown -R root:root "${STAGING_DIR}" && \
+                rsync -CrltDE -c --delete-after --chmod=u+rwX,go-rwx "${STAGING_DIR}/" "${MASTERDIR}/" && \
+                rm -rf ${STAGING_DIR}/.svn && \
                 echo "Successfully staged a policy release on $(date)"
 	    )
 	fi
     else
-       error_exit "The staged policies in ${TMP} could not be validated, aborting."
+       error_exit "The staged policies in ${STAGING_DIR} could not be validated, aborting."
     fi
 }

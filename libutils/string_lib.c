a5bc8eb42137a083970ebb44789bdc0cc421bd98
Soumyadip D. Mahapatra <smahapat@smahapat-mn2.linkedin.biz>
2016-02-02 20:46:46 +0530

    Redmine#7912: implement Murmur3 32bit hash & hash collision resolution for Class table hashes

--- libutils/string_lib.c
+++ libutils/string_lib.c
@@ -77,6 +77,80 @@ unsigned int StringHash(const char *str, unsigned int seed, unsigned int max)
     return (h & (max - 1));
 }
 
+/**
+ * Murmur3 32bit hash: https://en.wikipedia.org/wiki/MurmurHash
+ *
+ * Original C++ implementation of Murmur Hash v3:
+ * https://code.google.com/p/smhasher/wiki/MurmurHash3
+ *
+ * This 32bit C implementation can be found here:
+ * https://github.com/wolkykim/qlibc/blob/master/src/utilities/qhash.c#L258
+ *
+ * @parram #str   String to be hashed
+ * @parama #seed  Initial hash value
+ */
+uint32_t MurmurHash3_32(const char *str, uint32_t seed)
+{
+    if (str == NULL)
+    {
+        return 0;
+    }
+
+    size_t nbytes = strlen(str);
+    if (nbytes == 0)
+    {
+        return 0;
+    }
+
+    const uint32_t c1 = 0xcc9e2d51;
+    const uint32_t c2 = 0x1b873593;
+
+    const int nblocks = nbytes / 4;
+    const uint32_t *blocks = (const uint32_t *) (str);
+    const uint8_t *tail = (const uint8_t *) (str + (nblocks * 4));
+
+    uint32_t h = seed;
+
+    int i;
+    uint32_t k;
+    for (i = 0; i < nblocks; i++)
+    {
+        k = blocks[i];
+
+        k *= c1;
+        k = (k << 15) | (k >> (32 - 15));
+        k *= c2;
+
+        h ^= k;
+        h = (h << 13) | (h >> (32 - 13));
+        h = (h * 5) + 0xe6546b64;
+    }
+
+    k = 0;
+    switch (nbytes & 3)
+    {
+        case 3:
+            k ^= tail[2] << 16;
+        case 2:
+            k ^= tail[1] << 8;
+        case 1:
+            k ^= tail[0];
+            k *= c1;
+            k = (k << 15) | (k >> (32 - 15));
+            k *= c2;
+            h ^= k;
+    }
+
+    h ^= nbytes;
+
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+
+    return h;
+}
 
 char ToLower(char ch)
 {

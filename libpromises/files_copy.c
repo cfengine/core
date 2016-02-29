840a31ab9b8579604ae96bbd1c0e6123317d2f16
Eystein Måløy Stenberg <eystein.maloy.stenberg@cfengine.com>
2016-02-29 11:28:32 -0800

    Show errors when failing to copy extended attributes. https://cfengine.zendesk.com/agent/tickets/2792
    ChangeLog: Show errors regarding failure to copy extended attributes
    when doing a local file copy. Errors could happen when copying
    across two different mount points where the support for extended
    attributes is different between the mount points.

--- libpromises/files_copy.c
+++ libpromises/files_copy.c
@@ -214,7 +214,7 @@ bool CopyFileExtendedAttributesDisk(const char *source, const char *destination)
         }
         else
         {
-            Log(LOG_LEVEL_INFO, "Can't copy extended attributes from '%s' to '%s'. (listxattr: %s)",
+            Log(LOG_LEVEL_ERR, "Can't copy extended attributes from '%s' to '%s'. (listxattr: %s)",
                 source, destination, GetErrorStr());
             return false;
         }
@@ -236,7 +236,7 @@ bool CopyFileExtendedAttributesDisk(const char *source, const char *destination)
             }
             else
             {
-                Log(LOG_LEVEL_INFO, "Can't copy extended attributes from '%s' to '%s'. (getxattr: %s: %s)",
+                Log(LOG_LEVEL_ERR, "Can't copy extended attributes from '%s' to '%s'. (getxattr: %s: %s)",
                     source, destination, GetErrorStr(), current);
                 return false;
             }
@@ -251,7 +251,7 @@ bool CopyFileExtendedAttributesDisk(const char *source, const char *destination)
             }
             else
             {
-                Log(LOG_LEVEL_INFO, "Can't copy extended attributes from '%s' to '%s'. (setxattr: %s: %s)",
+                Log(LOG_LEVEL_ERR, "Can't copy extended attributes from '%s' to '%s'. (setxattr: %s: %s)",
                     source, destination, GetErrorStr(), current);
                 return false;
             }

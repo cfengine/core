76ac80194302426d81ef4b7b6e3c6001430d7be0
Ole Herman Schumacher Elgesem <ole.elgesem@cfengine.com>
2016-09-21 14:54:27 +0200

    Fixed memleak in unit test: addr_lib_test.c
    (cherry picked from commit 705839bd3bca6a8eceb1e6d5b8dfd00f6c5a8fee)

--- tests/unit/addr_lib_test.c
+++ tests/unit/addr_lib_test.c
@@ -4,143 +4,140 @@
 #include <addr_lib.h>
 
 
-/**
- *  @WARNING this test leaks all over the place. I'm strdup'ing because
- *           ParseHostPort() might modify the string, and a string literal
- *           will cause segfault since it is in read-only memory segment.
- */
 static void test_ParseHostPort()
 {
     char *hostname, *port;
 
+    char test_string[64];
+
     // Domain name:
-    ParseHostPort(xstrdup("www.cfengine.com"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "www.cfengine.com"), &hostname, &port);
     assert_string_equal(hostname, "www.cfengine.com");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("www.cfengine.com:80"), &hostname, &port);
-    assert_string_equal(hostname, "www.cfengine.com");
-    assert_string_equal(port, "80");
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("www.cfengine.com:"), &hostname, &port);
-    assert_string_equal(hostname, "www.cfengine.com");
-    assert_int_equal(port, NULL);
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("localhost"), &hostname, &port);
-    assert_string_equal(hostname, "localhost");
-    assert_int_equal(port, NULL);
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("localhost:"), &hostname, &port);
-    assert_string_equal(hostname, "localhost");
-    assert_int_equal(port, NULL);
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("localhost:80"), &hostname, &port);
-    assert_string_equal(hostname, "localhost");
-    assert_string_equal(port, "80");
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("[localhost]"), &hostname, &port);
-    assert_string_equal(hostname, "localhost");
-    assert_int_equal(port, NULL);
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("[localhost]:"), &hostname, &port);
-    assert_string_equal(hostname, "localhost");
-    assert_int_equal(port, NULL);
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("[localhost]:80"), &hostname, &port);
-    assert_string_equal(hostname, "localhost");
-    assert_string_equal(port, "80");
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("[www.cfengine.com]"), &hostname, &port);
-    assert_string_equal(hostname, "www.cfengine.com");
-    assert_int_equal(port, NULL);
-    hostname = NULL; port = NULL;
-
-    ParseHostPort(xstrdup("[www.cfengine.com]:80"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "www.cfengine.com:80"), &hostname, &port);
     assert_string_equal(hostname, "www.cfengine.com");
     assert_string_equal(port, "80");
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("[www.cfengine.com]:"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "www.cfengine.com:"), &hostname, &port);
+    assert_string_equal(hostname, "www.cfengine.com");
+    assert_int_equal(port, NULL);
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "localhost"), &hostname, &port);
+    assert_string_equal(hostname, "localhost");
+    assert_int_equal(port, NULL);
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "localhost:"), &hostname, &port);
+    assert_string_equal(hostname, "localhost");
+    assert_int_equal(port, NULL);
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "localhost:80"), &hostname, &port);
+    assert_string_equal(hostname, "localhost");
+    assert_string_equal(port, "80");
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "[localhost]"), &hostname, &port);
+    assert_string_equal(hostname, "localhost");
+    assert_int_equal(port, NULL);
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "[localhost]:"), &hostname, &port);
+    assert_string_equal(hostname, "localhost");
+    assert_int_equal(port, NULL);
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "[localhost]:80"), &hostname, &port);
+    assert_string_equal(hostname, "localhost");
+    assert_string_equal(port, "80");
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "[www.cfengine.com]"), &hostname, &port);
+    assert_string_equal(hostname, "www.cfengine.com");
+    assert_int_equal(port, NULL);
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "[www.cfengine.com]:80"), &hostname, &port);
+    assert_string_equal(hostname, "www.cfengine.com");
+    assert_string_equal(port, "80");
+    hostname = NULL; port = NULL;
+
+    ParseHostPort(strcpy(test_string, "[www.cfengine.com]:"), &hostname, &port);
     assert_string_equal(hostname, "www.cfengine.com");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
     // IPv4:
-    ParseHostPort(xstrdup("1.2.3.4"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "1.2.3.4"), &hostname, &port);
     assert_string_equal(hostname, "1.2.3.4");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("1.2.3.4:80"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "1.2.3.4:80"), &hostname, &port);
     assert_string_equal(hostname, "1.2.3.4");
     assert_string_equal(port, "80");
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("1.2.3.4:"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "1.2.3.4:"), &hostname, &port);
     assert_string_equal(hostname, "1.2.3.4");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
     // IPv6 with square brackets:
-    ParseHostPort(xstrdup("[ffff::dd:12:34]"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "[ffff::dd:12:34]"), &hostname, &port);
     assert_string_equal(hostname, "ffff::dd:12:34");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("[ffff::dd:12:34]:80"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "[ffff::dd:12:34]:80"), &hostname, &port);
     assert_string_equal(hostname, "ffff::dd:12:34");
     assert_string_equal(port, "80");
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("[ffff::dd:12:34]:"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "[ffff::dd:12:34]:"), &hostname, &port);
     assert_string_equal(hostname, "ffff::dd:12:34");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
     // IPv6 without square brackets:
-    ParseHostPort(xstrdup("ffff::dd:12:34"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "ffff::dd:12:34"), &hostname, &port);
     assert_string_equal(hostname, "ffff::dd:12:34");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
     // IPv4 mapped IPv6 addresses:
-    ParseHostPort(xstrdup("::ffff:192.0.2.128"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "::ffff:192.0.2.128"), &hostname, &port);
     assert_string_equal(hostname, "::ffff:192.0.2.128");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("[::ffff:192.0.2.128]"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "[::ffff:192.0.2.128]"), &hostname, &port);
     assert_string_equal(hostname, "::ffff:192.0.2.128");
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
     /***** CORNER CASES *****/
 
-    ParseHostPort(xstrdup(""), &hostname, &port);
+    ParseHostPort(strcpy(test_string, ""), &hostname, &port);
     assert_int_equal(hostname, NULL);
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("[]"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "[]"), &hostname, &port);
     assert_int_equal(hostname, NULL);
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup("[]:"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, "[]:"), &hostname, &port);
     assert_int_equal(hostname, NULL);
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;
 
-    ParseHostPort(xstrdup(":"), &hostname, &port);
+    ParseHostPort(strcpy(test_string, ":"), &hostname, &port);
     assert_int_equal(hostname, NULL);
     assert_int_equal(port, NULL);
     hostname = NULL; port = NULL;

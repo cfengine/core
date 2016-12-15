13b0b8b98d0b5bcfc690d9bacb15e178df8cea46
Ole Herman Schumacher Elgesem <ole.elgesem@cfengine.com>
2016-12-15 16:55:32 +0100

    Cleaned up .travis.yml and created travis-scripts folder
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@cfengine.com>
    (cherry picked from commit dfc40ca912d7a8876d7cb8d8f9b4c06ba49c61e9)

---
+++ travis-scripts/before_deploy.sh
@@ -0,0 +1,8 @@
+# A tag ending with "number.number" is not a prerelease
+if echo $DIST_TARBALL | grep -E -q '[0-9]{1,2}\.[0-9]{1,2}\.tar\.gz';
+then
+    IS_PRERELEASE=false;
+else
+    IS_PRERELEASE=true;
+fi;
+export IS_PRERELEASE
---
+++ travis-scripts/script.sh
@@ -0,0 +1,49 @@
+INSTDIR=$HOME/cf_install
+cd $TRAVIS_BUILD_DIR
+
+# if [ "$JOB_TYPE" = style_check ];
+#   then
+#       # sh tests/misc/style_check.sh;
+#       exit 0;
+#   fi
+
+# Fetch the tags from upstream, even if we are running on a foreign clone;
+# Needed for determine-version.py to work
+git remote add upstream https://github.com/cfengine/core.git  && git fetch -q upstream 'refs/tags/*:refs/tags/*'
+
+if [[ ${TRAVIS_OS_NAME} = osx ]]; then
+    # On osx the default gcc is actually LLVM
+    export CC=gcc-6
+    NO_CONFIGURE=1 ./autogen.sh
+    ./configure --enable-debug --prefix=$INSTDIR --with-init-script --with-lmdb=/usr/local/Cellar/lmdb/  --with-openssl=/usr/local/opt/openssl
+else
+    NO_CONFIGURE=1 ./autogen.sh
+    ./configure --enable-debug --with-tokyocabinet --prefix=$INSTDIR --with-init-script
+fi
+
+make dist
+export DIST_TARBALL=`echo cfengine-*.tar.gz`
+
+if [ "$JOB_TYPE" = compile_and_unit_test ];
+then
+    make CFLAGS=-Werror  &&
+    make -C tests/unit check;
+    return;
+else
+    make;
+fi
+
+cd tests/acceptance
+chmod -R go-w .
+
+if [ "$JOB_TYPE" = acceptance_tests_common ];
+then
+    ./testall --tests=common;
+    return;
+fi
+
+  # WARNING: the following job runs the selected tests as root!
+if [ "$JOB_TYPE" = acceptance_tests_unsafe_serial_network_etc ]; then
+    ./testall --gainroot=sudo --tests=timed,errorexit,libxml2,libcurl,serial,network,unsafe;
+    return;
+fi

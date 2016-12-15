ad33b2776e3a39c78685bf1e9d1f1f03b41258b8
Ole Herman Schumacher Elgesem <ole.elgesem@cfengine.com>
2016-12-15 16:55:32 +0100

    Travis improvements
    Added preliminary mac support.
    Moved travis bash scripts to travis-scripts folder.

---
+++ travis-scripts/before_install.sh
@@ -0,0 +1,19 @@
+if [[ "$TRAVIS_OS_NAME" == "osx" ]];
+then
+    brew update
+    brew install lmdb
+    brew install gcc
+    #brew install python
+    #brew install openssl
+    #brew install libxml2
+    #brew install fakeroot
+else
+    sudo apt-get -qq update
+    # Needed to build
+    sudo apt-get install -y libssl-dev libpam0g-dev libtokyocabinet-dev
+    # Optional
+    sudo apt-get install -y libxml2-dev libacl1-dev
+fi
+
+BLARGH="HAPPY"
+export BLARGH

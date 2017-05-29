a359b2fe47f7bf1a9d7313842f4bcf5c6c34e03e
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2017-05-29 17:09:58 +0200

    Fix: if the list is not empty.

--- misc/determine-version.py
+++ misc/determine-version.py
@@ -190,7 +190,8 @@ for tag in git_tag_list.stdout.readlines():
 all_tags = sorted(all_tags, cmp=version_cmp, reverse=True)
 verbose_print("all_tags   =", all_tags)
 
-if len(all_tags) > 1:
+
+if len(all_tags) >= 1:
     # This is a new minor version
     final_print("%s.%d.0a.%s" % (all_tags[0][0], int(all_tags[0][1]) + 1, abbrev_rev))
 else:

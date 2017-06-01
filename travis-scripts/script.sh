c12553f70353e4cc3785c609e2c92c22d2c511f7
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2017-06-01 15:32:55 +0200

    Fix nasty travis build breakage.
    (cherry picked from commit 4b54b3080059d73eda3043e61ff611b3bd032e1a)

--- travis-scripts/script.sh
+++ travis-scripts/script.sh
@@ -2,14 +2,18 @@ INSTDIR=$HOME/cf_install
 cd $TRAVIS_BUILD_DIR
 
 # if [ "$JOB_TYPE" = style_check ]
-#   then
-#       # sh tests/misc/style_check.sh
-#       exit 0
-#   fi
+# then
+#     # sh tests/misc/style_check.sh
+#     exit 0
+# fi
 
-# Fetch the tags from upstream, even if we are running on a foreign clone;
-# Needed for determine-version.py to work
-git remote add upstream https://github.com/cfengine/core.git  && git fetch -q upstream 'refs/tags/*:refs/tags/*'
+# Unshallow the clone. Fetch the tags from upstream even if we are on a
+# foreign clone. Needed for determine-version.py to work, specifically
+# `git describe --tags HEAD` was failing once the last tagged commit
+# became too old.
+git fetch --unshallow
+git remote add upstream https://github.com/cfengine/core.git  \
+    && git fetch upstream 'refs/tags/*:refs/tags/*'
 
 if [ "$TRAVIS_OS_NAME" = osx ]
 then

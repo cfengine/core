f74f9f86ecef0eb9c3f381fecc99b2f5695222ad
Jonas Bernoulli <jonas@bernoul.li>
2017-11-11 11:27:17 +0100

    Add permission statement to cfengine-code-style.el
    It is the convention to have such a header in every Emacs lisp
    library.  One benefit is that it helps tools that extract Emacs
    packages from repositories that contain much more than just the
    Emacs lisp.  Without this change the tools used to maintain the
    Emacsmirror does not see the license because it only extracts
    the library itself from the repository and discards all other
    history.

--- contrib/cfengine-code-style.el
+++ contrib/cfengine-code-style.el
@@ -3,6 +3,18 @@
 ;; Author: Mikhail Gusarov <mikhail.gusarov@cfengine.com>
 ;; URL: https://github.com/cfengine/core
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; version 3.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Usage:

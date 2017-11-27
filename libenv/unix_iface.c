49aca41560c497cf35e11d5c8d89618079ed6036
Nick Anderson <nick@cmdln.org>
2017-11-27 08:39:00 -0600

    ENT-3483: Do not tag large volatile variables for inventoy
    sys.interfaces_data, sys.inet and sys.inet6 are commonly larger than the
    maximum data size allowed to be collected by cf-hub. Data larger than 1k
    is truncated. Instead of reporting truncated data this change stops
    tagging the variable so that it will not be collected to the Enterprise
    hub and will not be available in Mission Portal.
    
    Changelog: Commit
    
    ENT-3483: Do not tag sys.inet and sys.inet6 for inventory
    (cherry picked from commit d4456da5d28ca53cc7dcc9b6c26388833cf3556b)

--- libenv/unix_iface.c
+++ libenv/unix_iface.c
@@ -1100,7 +1100,7 @@ JsonElement* GetProcFileInfo(EvalContext *ctx, const char* filename, const char*
                 Buffer *varname = BufferNew();
                 BufferPrintf(varname, "%s", key);
                 EvalContextVariablePutSpecial(ctx, SPECIAL_SCOPE_SYS, BufferData(varname), info, CF_DATA_TYPE_CONTAINER,
-                                              "inventory,networking,/proc,source=agent,attribute_name=none,procfs");
+                                              "networking,/proc,source=agent,procfs");
                 BufferDestroy(varname);
             }
 
@@ -1189,7 +1189,7 @@ void GetNetworkingInfo(EvalContext *ctx)
     }
 
     EvalContextVariablePutSpecial(ctx, SPECIAL_SCOPE_SYS, "inet", inet, CF_DATA_TYPE_CONTAINER,
-                                  "inventory,networking,/proc,source=agent,attribute_name=none,procfs");
+                                  "networking,/proc,source=agent,procfs");
     JsonDestroy(inet);
 
     JsonElement *inet6 = JsonObjectCreate(3);
@@ -1251,7 +1251,7 @@ void GetNetworkingInfo(EvalContext *ctx)
     }
 
     EvalContextVariablePutSpecial(ctx, SPECIAL_SCOPE_SYS, "inet6", inet6, CF_DATA_TYPE_CONTAINER,
-                                  "inventory,networking,/proc,source=agent,attribute_name=none,procfs");
+                                  "networking,/proc,source=agent,procfs");
     JsonDestroy(inet6);
 
     // Inter-|   Receive                                                |  Transmit

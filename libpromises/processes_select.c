8b12dcb9507c4876bd73aa64bf0ef991d68a7715
Craig Comstock <craig.comstock@northern.tech>
2018-01-26 11:50:08 -0600

    run one core acceptance test to speed things up. add logging of all process info when selecting a proc

--- libpromises/processes_select.c
+++ libpromises/processes_select.c
@@ -199,6 +199,8 @@ Item *SelectProcesses(const Item *processes, const char *process_name, ProcessSe
 
     GetProcessColumnNames(processes->name, &names[0], start, end);
 
+    log(LOG_LEVEL_DEBUG, "SelectProcesses() process_name=%s", process_name);
+
     pcre *rx = CompileRegex(process_name);
     if (rx)
     {
@@ -209,6 +211,7 @@ Item *SelectProcesses(const Item *processes, const char *process_name, ProcessSe
         {
             int s, e;
 
+            log(LOG_LEVEL_DEBUG, "SelectProcesses() ip->name=%s", ip->name);
             if (StringMatchWithPrecompiledRegex(rx, ip->name, &s, &e))
             {
                 if (NULL_OR_EMPTY(ip->name))

f8e14cc8e39efffc2147e464102215e2dcae8d81
Dimitrios Apostolou <jimis@northern.tech>
2018-02-20 13:22:16 +0100

    coverage fixes

--- travis-scripts/after_success.sh
+++ travis-scripts/after_success.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# For systems with sudo access, make all files readable so that codecovarage
+# data is readable even for tests that were run as root.
+sudo chown -R $USER  .  ||  true
+
 # Code coverage by codecov.io
 curl -s https://codecov.io/bash | bash

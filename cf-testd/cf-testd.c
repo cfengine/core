bd7cb5820a2a44b1a3032056f5ff47f56d3a6c39
Craig Comstock <craig.comstock@northern.tech>
2018-03-19 21:08:49 -0500

    ENT-3526: expand reporting tests. Need to fflush() log output to help cf-consumer complete log output for tests. Added cf-testd option -r/--report to injest a report from a file or (/dev/stdin).

--- cf-testd/cf-testd.c
+++ cf-testd/cf-testd.c
@@ -1,8 +1,7 @@
 /*
    Copyright 2018 Northern.tech AS
 
-   This file is part of CFEngine 3 - written and maintained by Northern.tech
-  AS.
+   This file is part of CFEngine 3 - written and maintained by Northern.tech AS.
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
@@ -25,41 +24,44 @@
 
 #include <platform.h>
 
-#include <client_code.h> // cfnet_init
-#include <crypto.h>      // CryptoInitialize
+#include <client_code.h>        // cfnet_init
+#include <crypto.h>             // CryptoInitialize
 
-#include <bootstrap.h> // GetAmPolicyHub
+#include <bootstrap.h>          // GetAmPolicyHub
 #include <cf-serverd-enterprise-stubs.h>
-#include <conversion.h> // MapAddress
-#include <exec_tools.h> // ActAsDaemon
-#include <item_lib.h>   // DeleteItemList
-#include <known_dirs.h> // GetInputDir
-#include <loading.h>    // LoadPolicy
-#include <locks.h>      // AcquireLock
-#include <man.h>        // ManPageWrite
-#include <mutex.h>      // ThreadLock
+#include <conversion.h>         // MapAddress
+#include <exec_tools.h>         // ActAsDaemon
+#include <item_lib.h>           // DeleteItemList
+#include <known_dirs.h>         // GetInputDir
+#include <loading.h>            // LoadPolicy
+#include <locks.h>              // AcquireLock
+#include <man.h>                // ManPageWrite
+#include <mutex.h>              // ThreadLock
 #include <net.h>
-#include <openssl/err.h>   // ERR_get_error
-#include <policy_server.h> // PolicyServerReadFile
-#include <printsize.h>     // PRINTSIZE
-#include <server_access.h> // acl_Free
-#include <server_code.h>   // InitServer
+#include <openssl/err.h>        // ERR_get_error
+#include <policy_server.h>      // PolicyServerReadFile
+#include <printsize.h>          // PRINTSIZE
+#include <server_access.h>      // acl_Free
+#include <server_code.h>        // InitServer
 #include <server_common.h>
-#include <server_tls.h>       // ServerTLSInitialize
-#include <server_transform.h> // Summarize
-#include <signals.h>          // ReloadConfigRequested
-#include <sysinfo.h>          // DetectEnvironment
-#include <systype.h>          // CLASSTEXT
-#include <time_classes.h>     // UpdateTimeClasses
-#include <timeout.h>          // SetReferenceTime
-#include <tls_generic.h>      // TLSLogError
+#include <server_tls.h>         // ServerTLSInitialize
+#include <server_transform.h>   // Summarize
+#include <signals.h>            // ReloadConfigRequested
+#include <string_lib.h>         // StringFormat, etc.
+#include <sysinfo.h>            // DetectEnvironment
+#include <systype.h>            // CLASSTEXT
+#include <time_classes.h>       // UpdateTimeClasses
+#include <timeout.h>            // SetReferenceTime
+#include <tls_generic.h>        // TLSLogError
 
 #define CFTESTD_QUEUE_SIZE 10
 
 // ============================= CFTestD_Config ==============================
 typedef struct
 {
-    char *file;
+    char *report_file;
+    const char *report;
+    int report_len;
 } CFTestD_Config;
 
 /*******************************************************************/
@@ -69,10 +71,10 @@ typedef struct
 static const struct option OPTIONS[] = {
     {"address", required_argument, 0, 'a'},
     {"debug", no_argument, 0, 'd'},
-    {"file", required_argument, 0, 'f'},
     {"help", no_argument, 0, 'h'},
     {"inform", no_argument, 0, 'I'},
     {"port", required_argument, 0, 'p'},
+    {"report", required_argument, 0, 'r'},
     {"timestamp", no_argument, 0, 'l'},
     {"verbose", no_argument, 0, 'v'},
     {"version", no_argument, 0, 'V'},
@@ -81,10 +83,10 @@ static const struct option OPTIONS[] = {
 static const char *const HINTS[] = {
     "Bind to a specific address",
     "Enable debugging output",
-    "Read report from file",
     "Print the help message",
     "Print basic information about what cf-testd does",
     "Set the port cf-testd will listen on",
+    "Read report from file",
     "Log timestamps on each line of log output",
     "Output verbose information about the behaviour of the agent",
     "Output the version of the software",
@@ -99,7 +101,8 @@ CFTestD_Config *CFTestD_ConfigInit()
 
 void CFTestD_ConfigDestroy(CFTestD_Config *config)
 {
-    free(config->file);
+    free(config->report_file);
+    free(config->report);
     free(config);
 }
 
@@ -121,11 +124,18 @@ CFTestD_Config *CFTestD_CheckOpts(int argc, char **argv)
     {
         switch (c)
         {
-        case 'a': SetBindInterface(optarg); break;
-        case 'd': LogSetGlobalLevel(LOG_LEVEL_DEBUG); break;
-        case 'f': config->file = xstrdup(optarg); break;
-        case 'h': CFTestD_Help(); exit(EXIT_SUCCESS);
-        case 'I': LogSetGlobalLevel(LOG_LEVEL_INFO); break;
+        case 'a':
+            SetBindInterface(optarg);
+            break;
+        case 'd':
+            LogSetGlobalLevel(LOG_LEVEL_DEBUG);
+            break;
+        case 'h':
+            CFTestD_Help();
+            exit(EXIT_SUCCESS);
+        case 'I':
+            LogSetGlobalLevel(LOG_LEVEL_INFO);
+            break;
         case 'p':
         {
             bool ret = SetCfenginePort(optarg);
@@ -136,8 +146,15 @@ CFTestD_Config *CFTestD_CheckOpts(int argc, char **argv)
             }
             break;
         }
-        case 'l': LoggingEnableTimestamps(true); break;
-        case 'v': LogSetGlobalLevel(LOG_LEVEL_VERBOSE); break;
+        case 'l':
+            LoggingEnableTimestamps(true);
+            break;
+        case 'r':
+            config->report_file = xstrdup(optarg);
+            break;
+        case 'v':
+            LogSetGlobalLevel(LOG_LEVEL_VERBOSE);
+            break;
         case 'V':
         {
             Writer *w = FileWriter(stdout);
@@ -145,7 +162,9 @@ CFTestD_Config *CFTestD_CheckOpts(int argc, char **argv)
             FileWriterDetach(w);
         }
             exit(EXIT_SUCCESS);
-        default: CFTestD_Help(); exit(EXIT_FAILURE);
+        default:
+            CFTestD_Help();
+            exit(EXIT_FAILURE);
         }
     }
 
@@ -188,8 +207,11 @@ bool CFTestD_TLSSessionEstablish(ServerConnectionState *conn)
     /* Send/Receive "CFE_v%d" version string, agree on version, receive
        identity (username) of peer. */
     char username[sizeof(conn->username)] = "";
-    bool id_success                       = ServerIdentificationDialog(
-        conn->conn_info, username, sizeof(username));
+    bool id_success = ServerIdentificationDialog(
+        conn->conn_info,
+        username,
+        sizeof(username)
+    );
     if (!id_success)
     {
         return false;
@@ -203,9 +225,13 @@ bool CFTestD_TLSSessionEstablish(ServerConnectionState *conn)
     return true;
 }
 
-bool CFTestD_GetServerQuery(ServerConnectionState *conn, char *recvbuffer)
+bool CFTestD_GetServerQuery(
+    ServerConnectionState *conn, char *recvbuffer, CFTestD_Config *config)
 {
     char query[CF_BUFSIZE];
+    const int report_len            = config->report_len;
+    const char *const report        = config->report;
+    ConnectionInfo *const conn_info = conn->conn_info;
 
     query[0] = '\0';
     sscanf(recvbuffer, "QUERY %255[^\n]", query);
@@ -215,7 +241,47 @@ bool CFTestD_GetServerQuery(ServerConnectionState *conn, char *recvbuffer)
         return false;
     }
 
-    return CFTestD_ReturnQueryData(conn, query);
+    if (report_len == 0)
+    {
+        Log(LOG_LEVEL_INFO,
+            "No report file argument so returning canned data from enterprise plugin server\n");
+        return CFTestD_ReturnQueryData(conn, query);
+    }
+
+    Log(LOG_LEVEL_INFO,
+        "Report file argument specified. Returning report of length %d",
+        report_len);
+
+    // TODO for variables reports we will eventually need to split on \m
+    // instead of \n since variable values can contain '\n's.
+
+    size_t num_items = StringCountTokens(report, report_len, "\n");
+
+    if (num_items < 3)
+    {
+        Log(LOG_LEVEL_ERR,
+            "report file must have a CFR timestamp header line and at least two lines of items\n");
+        return false;
+    }
+
+    StringRef ts_ref = StringGetToken(report, report_len, 0, "\n");
+    char *ts         = xstrndup(ts_ref.data, ts_ref.len);
+    char *header     = StringFormat("CFR: 0 %s %d\n", ts, report_len);
+    SendTransaction(conn_info, header, SafeStringLength(header), CF_MORE);
+
+    for (ssize_t i = 1; i < num_items; i++)
+    {
+        StringRef item_ref =
+            StringGetToken(report, report_len, i, "\n");
+        char *item = xstrndup(item_ref.data, item_ref.len);
+
+        SendTransaction(conn_info, item, SafeStringLength(item), CF_MORE);
+    }
+
+    const char end_reply[] = "QUERY complete";
+    SendTransaction(conn_info, end_reply, SafeStringLength(end_reply), CF_DONE);
+
+    return true;
 }
 
 static bool CFTestD_ProtocolError(
@@ -229,7 +295,8 @@ static bool CFTestD_ProtocolError(
     return false;
 }
 
-static bool CFTestD_BusyLoop(ServerConnectionState *conn)
+static bool CFTestD_BusyLoop(
+    ServerConnectionState *conn, CFTestD_Config *config)
 {
     char recvbuffer[CF_BUFSIZE + CF_BUFEXT]        = "";
     char sendbuffer[CF_BUFSIZE - CF_INBAND_OFFSET] = "";
@@ -275,7 +342,7 @@ static bool CFTestD_BusyLoop(ServerConnectionState *conn)
             return CFTestD_ProtocolError(conn, recvbuffer, sendbuffer);
         }
 
-        if (CFTestD_GetServerQuery(conn, recvbuffer))
+        if (CFTestD_GetServerQuery(conn, recvbuffer, config))
         {
             return true;
         }
@@ -298,10 +365,10 @@ static ServerConnectionState *CFTestD_NewConn(ConnectionInfo *info)
      * it to fill in conn->ipaddr ? */
     struct sockaddr_storage addr;
     socklen_t size = sizeof(addr);
+    int sockfd     = ConnectionInfoSocket(info);
+    int sockname   = getsockname(sockfd, (struct sockaddr *)&addr, &size);
 
-    if (getsockname(
-            ConnectionInfoSocket(info), (struct sockaddr *)&addr, &size) ==
-        -1)
+    if (sockname == -1)
     {
         Log(LOG_LEVEL_ERR,
             "Could not obtain socket address. (getsockname: '%s')",
@@ -337,7 +404,7 @@ static void CFTestD_DeleteConn(ServerConnectionState *conn)
     free(conn);
 }
 
-static void *CFTestD_HandleConnection(void *c)
+static void *CFTestD_HandleConnection(void *c, CFTestD_Config *config)
 {
     ServerConnectionState *conn = c;
 
@@ -368,7 +435,7 @@ static void *CFTestD_HandleConnection(void *c)
         Log(LOG_LEVEL_INFO, "Hostname (reverse looked up): %s", conn->revdns);
     }
 
-    while (CFTestD_BusyLoop(conn))
+    while (CFTestD_BusyLoop(conn, config))
     {
     }
 
@@ -376,18 +443,19 @@ static void *CFTestD_HandleConnection(void *c)
     return NULL;
 }
 
-static void CFTestD_SpawnConnection(const char *ipaddr, ConnectionInfo *info)
+static void CFTestD_SpawnConnection(
+    const char *ipaddr, ConnectionInfo *info, CFTestD_Config *config)
 {
     ServerConnectionState *conn = CFTestD_NewConn(info);
     ConnectionInfoSocket(info);
     strlcpy(conn->ipaddr, ipaddr, CF_MAX_IP_LEN);
 
     Log(LOG_LEVEL_WARNING, "Connection is being handled from main loop!");
-    CFTestD_HandleConnection(conn);
+    CFTestD_HandleConnection(conn, config);
 }
 
 /* Try to accept a connection; handle if we get one. */
-static void CFTestD_AcceptAndHandle(int sd)
+static void CFTestD_AcceptAndHandle(int sd, CFTestD_Config *config)
 {
     /* TODO embed ConnectionInfo into ServerConnectionState. */
     ConnectionInfo *info = ConnectionInfoNew(); /* Uses xcalloc() */
@@ -418,10 +486,10 @@ static void CFTestD_AcceptAndHandle(int sd)
 
     /* IPv4 mapped addresses (e.g. "::ffff:192.168.1.2") are
      * hereby represented with their IPv4 counterpart. */
-    CFTestD_SpawnConnection(MapAddress(ipaddr), info);
+    CFTestD_SpawnConnection(MapAddress(ipaddr), info, config);
 }
 
-int CFTestD_StartServer()
+int CFTestD_StartServer(CFTestD_Config *config)
 {
     bool tls_init_ok = ServerTLSInitialize();
     if (!tls_init_ok)
@@ -441,7 +509,7 @@ int CFTestD_StartServer()
         if (selected > 0)
         {
             Log(LOG_LEVEL_DEBUG, "select(): %d", selected);
-            CFTestD_AcceptAndHandle(sd);
+            CFTestD_AcceptAndHandle(sd, config);
         }
     }
     Log(LOG_LEVEL_ERR,
@@ -458,23 +526,70 @@ int CFTestD_StartServer()
     return 0;
 }
 
+static void HandleSignal(int signum)
+{
+    switch (signum)
+    {
+    case SIGTERM:
+    case SIGINT:
+        // flush all logging before process ends.
+        fflush(stdout);
+        exit(EXIT_FAILURE);
+        break;
+    default:
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    signal(SIGINT, HandleSignal);
+    signal(SIGTERM, HandleSignal);
+
     Log(LOG_LEVEL_VERBOSE, "Starting cf-testd");
     CryptoInitialize();
     LoadSecretKeys();
     cfnet_init(NULL, NULL);
     CFTestD_Config *config = CFTestD_CheckOpts(argc, argv);
+    char *report_file      = config->report_file;
 
-    if (config->file != NULL)
+    if (report_file != NULL)
     {
-        Log(LOG_LEVEL_NOTICE, "Got file argument: '%s'", config->file);
-        Log(LOG_LEVEL_ERR, "File input not yet supported");
-        // TODO: implement file input
-        exit(EXIT_FAILURE);
+        Log(LOG_LEVEL_NOTICE, "Got file argument: '%s'", report_file);
+        if (!FileCanOpen(report_file, "r"))
+        {
+            Log(LOG_LEVEL_ERR,
+                "Can't open file '%s' for reading",
+                report_file);
+            exit(EXIT_FAILURE);
+        }
+
+        Writer *contents = FileRead(report_file, SIZE_MAX, NULL);
+        if (!contents)
+        {
+            Log(LOG_LEVEL_ERR, "Error reading report file '%s'", report_file);
+            exit(EXIT_FAILURE);
+        }
+        config->report     = StringWriterData(contents);
+        config->report_len = SafeStringLength(config->report);
+
+        Log(LOG_LEVEL_NOTICE,
+            "Read %d bytes for report contents",
+            config->report_len);
+
+        if (config->report_len <= 0)
+        {
+            Log(LOG_LEVEL_ERR, "Report file contained no bytes");
+            exit(EXIT_FAILURE);
+        }
+
+        Log(LOG_LEVEL_DEBUG, "Got report file contents: %s", config->report);
     }
 
-    int r = CFTestD_StartServer();
+    Log(LOG_LEVEL_INFO, "Starting server...");
+    fflush(stdout); // for debugging startup
+
+    int r = CFTestD_StartServer(config);
     CFTestD_ConfigDestroy(config);
 
     return r;

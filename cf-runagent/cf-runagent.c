0b4e8adfdf8c9917d99377547b2ef132441d0d5b
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2018-08-15 13:28:29 +0200

    Fix the argument for the '-b' option of cf-runagent to really be optional
    The '--help' output says it is an optional argument, and the code
    says 'optional_argument' as well. But for this to work with
    'getopt_long()' two colons have to be used after the short option
    character.

--- cf-runagent/cf-runagent.c
+++ cf-runagent/cf-runagent.c
@@ -263,7 +263,7 @@ static GenericAgentConfig *CheckOpts(int argc, char **argv)
     REMOTEBUNDLES[0] = '\0';
 
     int longopt_idx;
-    while ((c = getopt_long(argc, argv, "t:q:db:vnKhIif:g:D:VSxo:s:MH:C::l",
+    while ((c = getopt_long(argc, argv, "t:q:db::vnKhIif:g:D:VSxo:s:MH:C::l",
                             OPTIONS, &longopt_idx))
            != -1)
     {

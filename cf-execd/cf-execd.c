72dd5d0b82e24f888a18a83f53b1c2af4b7e4fa4
Ole Herman Schumacher Elgesem <olehermanse@users.noreply.github.com>
2018-10-13 03:56:44 +0200

    Revert "Revert "CFE-2918: Fixed small memory leaks of environment variable strings""

--- cf-execd/cf-execd.c
+++ cf-execd/cf-execd.c
@@ -162,7 +162,7 @@ int main(int argc, char *argv[])
     GenericAgentFinalize(ctx, config);
     ExecConfigDestroy(exec_config);
     ExecdConfigDestroy(execd_config);
-
+    putenv_static_destroy();
     return 0;
 }
 
@@ -174,7 +174,6 @@ static GenericAgentConfig *CheckOpts(int argc, char **argv)
 {
     extern char *optarg;
     int c;
-    char ld_library_path[CF_BUFSIZE];
 
     GenericAgentConfig *config = GenericAgentConfigNewDefault(AGENT_TYPE_EXECUTOR, GetTTYInteractive());
 
@@ -247,10 +246,13 @@ static GenericAgentConfig *CheckOpts(int argc, char **argv)
             break;
 
         case 'L':
-            snprintf(ld_library_path, CF_BUFSIZE - 1, "LD_LIBRARY_PATH=%s", optarg);
-            putenv(xstrdup(ld_library_path));
-            break;
-
+            {
+                char env_var[CF_BUFSIZE];
+                snprintf(env_var, CF_BUFSIZE - 1, "LD_LIBRARY_PATH=%s", optarg);
+                Log(LOG_LEVEL_VERBOSE, "Setting '%s'", env_var);
+                putenv_static(xstrdup(env_var));
+                break;
+            }
         case 'W':
             WINSERVICE = false;
             break;

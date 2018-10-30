e655b6a414d97ff5d7e8e11f46a23fe957bd7c22
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2018-10-30 11:42:47 +0100

    ENT-4023: change GIT_BRANCH to GIT_REFSPEC and remove Design Center vars
    Signed-off-by: Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>

--- contrib/masterfiles-stage/example_params/PARAMS_example_git_branch.sh
+++ contrib/masterfiles-stage/example_params/PARAMS_example_git_branch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ROOT="/tmp/masterfiles_staging"
 GIT_URL="https://gitlab.com/nickanderson/masterfiles_demo_3.7.git"
-GIT_BRANCH="master"
-# GIT_BRANCH can also be a git tag, or a git commit hash.
+GIT_REFSPEC="master"
+# GIT_REFSPEC can be a git branch, tag, or commit hash.
 # It will be staged correctly regardless.
 GIT_WORKING_BRANCH="CF_WORKING_BRANCH"
 VCS_TYPE="GIT"

a4508ecc3c6f4a7318d0362713ad39418d45fc30
Karl Hole Totland <karl.totland@northern.tech>
2018-12-18 16:25:47 +0100

    CFE-2478: Allow forward slash character in module protocol commands
    Previously, the function only allowed alphanumerical and
    characters `_`, `.`, `-`, `[` and `]`. Forward slash is not used
    for anything in module protocol commands, and is therefore added. Also
    reenabled the test for slashes in module commands.
    
    Ticket: CFE-2478
    Changelog: Title

--- libpromises/evalfunction.c
+++ libpromises/evalfunction.c
@@ -8055,7 +8055,8 @@ static FnCallResult FnCallCFEngineCallers(EvalContext *ctx, ARG_UNUSED const Pol
 
 static bool CheckIDChar(const char ch)
 {
-    return isalnum((int) ch) || (ch == '.') || (ch == '-') || (ch == '_') || (ch == '[') || (ch == ']');
+    return isalnum((int) ch) || (ch == '.') || (ch == '-') || (ch == '_') ||
+                                (ch == '[') || (ch == ']') || (ch == '/');
 }
 
 static bool CheckID(const char *id)

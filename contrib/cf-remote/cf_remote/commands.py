d197a778303ebdc595703cd3595095b80e6882e5
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-01-14 15:35:59 +0100

    cf-remote: install command
    The first version of this command does not pick a package
    automatically, the file has to be specified with
    --package argument.
    
    Changelog: Commit
    Ticket: CFE-2889
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- contrib/cf-remote/cf_remote/commands.py
+++ contrib/cf-remote/cf_remote/commands.py
@@ -1,4 +1,4 @@
-from cf_remote.remote import get_info, print_info
+from cf_remote.remote import get_info, print_info, install_host
 
 
 def info(hosts, users=None):
@@ -6,3 +6,11 @@ def info(hosts, users=None):
     for host in hosts:
         data = get_info(host, users)
         print_info(data)
+
+
+def install(hub, clients, bootstrap=None, package=None):
+    assert hub or clients
+    if hub:
+        install_host(hub, hub=True, package=package)
+    for host in (clients or []):
+        install_host(host, hub=False, package=package)

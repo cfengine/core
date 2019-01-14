3477a0799fa9b53a5cb55d04dae59214bc443035
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-01-14 16:25:08 +0100

    cf-remote: packages command
    This command can download the necessary packages for use with
    install command. For example:
    
    $ cf-remote packages deb hub
    
    Will download the default version hub package for debian.
    
    Changelog: Commit
    Ticket: CFE-2889
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- contrib/cf-remote/cf_remote/commands.py
+++ contrib/cf-remote/cf_remote/commands.py
@@ -1,4 +1,6 @@
 from cf_remote.remote import get_info, print_info, install_host
+from cf_remote.packages import Releases
+from cf_remote.web import download_package
 
 
 def info(hosts, users=None):
@@ -14,3 +16,18 @@ def install(hub, clients, bootstrap=None, package=None):
         install_host(hub, hub=True, package=package)
     for host in (clients or []):
         install_host(host, hub=False, package=package)
+
+
+def packages(tags=None):
+    releases = Releases()
+    print(releases)
+
+    release = releases.default
+    print("Using {}:".format(release))
+    artifacts = release.find(tags)
+
+    if len(artifacts) == 0:
+        print("No suitable packages found")
+    else:
+        for artifact in artifacts:
+            download_package(artifact.url)

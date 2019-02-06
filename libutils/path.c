752db767ddf73fec35c0f279dfbbc28d9e5269ce
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2019-02-06 14:30:54 +0100

    Add a function to get a quoted path (if needed)
    Sometimes we need to quote a path, e.g. when passing it as an
    argument to command execution in shell. However, the unqoted path
    is much nicer in logs and other output if it doesn't need to be
    quoted. So let's be nice on ourselves and the users and only
    quote when we are not 100% sure quoting is not necessary.
    
    Ticket: ENT-4432
    Changelog: None

--- libutils/path.c
+++ libutils/path.c
@@ -1,5 +1,6 @@
 #include <path.h>
 #include <string_lib.h>
+#include <file_lib.h>
 
 const char *Path_Basename(const char *path)
 {
@@ -31,4 +32,39 @@ char *Path_JoinAlloc(const char *dir, const char *leaf)
         return StringConcatenate(2, dir, leaf);
     }
     return StringConcatenate(3, dir, "/", leaf);
+}
+
+char *Path_GetQuoted(const char *path)
+{
+    if (path == NULL)
+    {
+        return NULL;
+    }
+
+    size_t path_len = strlen(path);
+    if ((path[0] == '"') && (path[path_len - 1] == '"'))
+    {
+        /* already quoted, just duplicate */
+        return SafeStringDuplicate(path);
+    }
+
+    bool needs_quoting = false;
+    for (const char *cp = path; !needs_quoting && (*cp != '\0'); cp++)
+    {
+        /* let's quote everything that's not just alphanumerics, underscores and
+         * dashes */
+        needs_quoting = !(((*cp >= 'a') && (*cp <= 'z')) ||
+                          ((*cp >= 'A') && (*cp <= 'Z')) ||
+                          ((*cp >= '0') && (*cp <= '9')) ||
+                          (*cp == '_') || (*cp == '-')   ||
+                          IsFileSep(*cp));
+    }
+    if (needs_quoting)
+    {
+        return StringConcatenate(3, "\"", path, "\"");
+    }
+    else
+    {
+        return SafeStringDuplicate(path);
+    }
 }

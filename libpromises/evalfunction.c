f7849cf41bc12fd4e34911c979ed3116048fea99
Nick Anderson <nick@cmdln.org>
2019-02-20 07:55:11 -0600

    CFE-3008 Message about invalid class characters from module protocol moved to VERBOSE
    In CFE-2887 the module protocol began automatically canonifying classes
    that were defined in order to align with the behaviour of policy. This
    change moves a warning about the class containing invalid characters to
    VERBOSE as to align with policy.
    
    (cherry picked from commit 829afc3d3b786cc3b5d8c469f1023d589b9dc33c)

--- libpromises/evalfunction.c
+++ libpromises/evalfunction.c
@@ -8064,7 +8064,7 @@ static bool CheckID(const char *id)
     {
         if (!CheckIDChar(*sp))
         {
-            Log(LOG_LEVEL_WARNING,
+            Log(LOG_LEVEL_VERBOSE,
                   "Module protocol contained an illegal character '%c' in class/variable identifier '%s'.", *sp,
                   id);
         }

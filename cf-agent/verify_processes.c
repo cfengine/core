04cfcd9fbb2da40ce1f7cd5bcd31a8ab26118d31
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-04-24 14:22:16 +0200

    Removed programming error in handling of process_count body
    Previously, having a failing function call inside in_range_define
    or out_of_range_define would cause a programming error when
    trying to define that as a class. Fixed it by detecting the
    case, printing a normal error, and skipping defining the class.
    
    Changelog: Commit
    Ticket: CFE-2067
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>
    (cherry picked from commit 25533d8ddd95980d1b68613939a66b2fa930b9a0)

--- cf-agent/verify_processes.c
+++ cf-agent/verify_processes.c
@@ -131,8 +131,28 @@ static PromiseResult VerifyProcesses(EvalContext *ctx, Attributes a, const Promi
     return result;
 }
 
-static void ProcessCountDefineClass(EvalContext *const ctx, const Rlist *const rp)
+static void ProcessCountMaybeDefineClass(
+    EvalContext *const ctx,
+    const Rlist *const rp,
+    const Promise *const pp,
+    const char *const attribute_name)
 {
+    assert(ctx != NULL);
+    assert(rp != NULL);
+    assert(pp != NULL);
+    assert(attribute_name != NULL && strlen(attribute_name) > 0);
+
+    if (rp->val.type == RVAL_TYPE_FNCALL)
+    {
+        Log(LOG_LEVEL_ERR,
+            "Body (process_count), for processes promise %s, has a failing function call (%s) in the %s attribute - cannot define the class",
+            pp->promiser,
+            RlistFnCallValue(rp)->name,
+            attribute_name);
+        return;
+    }
+
+    assert(rp->val.type == RVAL_TYPE_SCALAR);
     ClassRef ref = ClassRefParse(RlistScalarValue(rp));
     EvalContextClassPutSoft(
         ctx, RlistScalarValue(rp), CONTEXT_SCOPE_NAMESPACE, "source=promise");
@@ -160,7 +180,7 @@ static PromiseResult VerifyProcessOp(EvalContext *ctx, Attributes a, const Promi
             result = PromiseResultUpdate(result, PROMISE_RESULT_CHANGE);
             for (const Rlist *rp = a.process_count.out_of_range_define; rp != NULL; rp = rp->next)
             {
-                ProcessCountDefineClass(ctx, rp);
+                ProcessCountMaybeDefineClass(ctx, rp, pp, "out_of_range_define");
             }
             out_of_range = true;
         }
@@ -168,7 +188,7 @@ static PromiseResult VerifyProcessOp(EvalContext *ctx, Attributes a, const Promi
         {
             for (const Rlist *rp = a.process_count.in_range_define; rp != NULL; rp = rp->next)
             {
-                ProcessCountDefineClass(ctx, rp);
+                ProcessCountMaybeDefineClass(ctx, rp, pp, "in_range_define");
             }
             cfPS(ctx, LOG_LEVEL_VERBOSE, PROMISE_RESULT_NOOP, pp, a, "Process promise for '%s' is kept", pp->promiser);
             out_of_range = false;

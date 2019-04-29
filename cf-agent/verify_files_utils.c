53f13fba1b3ca27d196520e0d98a5abfda2df1b4
Craig Comstock <craig.comstock@northern.tech>
2019-04-29 13:20:49 +0200

    Ensure agent log file names are lowercase
    Policy in masterfiles expects this to be true.
    See: https://github.com/cfengine/core/pull/3326#discussion_r277785265
    
    Changelog: Title
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>
    (cherry picked from commit 750225eb2d5764eb743a6054001e33ed40fc9aa3)

--- cf-agent/verify_files_utils.c
+++ cf-agent/verify_files_utils.c
@@ -3189,6 +3189,7 @@ static void LoadSetxid(void)
 
     char filename[CF_BUFSIZE];
     snprintf(filename, CF_BUFSIZE, "%s/cfagent.%s.log", GetLogDir(), VSYSNAME.nodename);
+    ToLowerStrInplace(filename);
     MapName(filename);
 
     VSETXIDLIST = RawLoadItemList(filename);
@@ -3205,6 +3206,7 @@ static void SaveSetxid(bool modified)
     {
         char filename[CF_BUFSIZE];
         snprintf(filename, CF_BUFSIZE, "%s/cfagent.%s.log", GetLogDir(), VSYSNAME.nodename);
+        ToLowerStrInplace(filename);
         MapName(filename);
 
         PurgeItemList(&VSETXIDLIST, "SETUID/SETGID");

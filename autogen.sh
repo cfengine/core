89f93edb3bb9a4ad71ab3e1e28470181243af48e
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-08-07 13:38:33 +0200

    autogen.sh: Added checking that submodule exists
    Changelog: None
    Ticket: ENT-4885
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- autogen.sh
+++ autogen.sh
@@ -23,6 +23,14 @@
 # included file COSL.txt.
 #
 
+if [ ! -f libntech/libutils/sequence.h ] ; then
+    echo "Error: libntech/libutils/sequence.h is missing"
+    echo "       You probably forgot to use the --recursive option when cloning"
+    echo "       To fix it now, run:"
+    echo "       git submodule init && git submodule update"
+    exit 1
+fi
+
 #
 # Detect and replace non-POSIX shell
 #

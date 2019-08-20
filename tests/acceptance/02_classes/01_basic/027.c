3249e31d5dea11ad82d8d544201ef13ee4b79f82
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-08-20 09:15:40 +0200

    Reformatted 2 test sources using clang-format
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- tests/acceptance/02_classes/01_basic/027.c
+++ tests/acceptance/02_classes/01_basic/027.c
@@ -4,52 +4,57 @@
 #define SPLAY_PSEUDO_RANDOM_CONSTANT 8192
 
 #define HOURLY 12
-main() {
+main()
+{
     char s[] = "a";
     char c;
     int hash, box, minblocks;
-    int period=HOURLY-1;
+    int period = HOURLY - 1;
     char *boxes[HOURLY];
-    for (c=0; c<HOURLY; c++) {
-	boxes[c] = 0;
+    for (c = 0; c < HOURLY; c++)
+    {
+        boxes[c] = 0;
     }
-    for (c='a';c <='z'; c++){
-	*s = c;
-	// The block algorithm is copied from evalfunction.c
-	hash=OatHash(s);
-	box = (int)(0.5 + period*hash/(double)SPLAY_PSEUDO_RANDOM_CONSTANT);
-	minblocks = box % HOURLY;
-	// Back to original code
-	if (!boxes[minblocks]) {
-	    boxes[minblocks] = strncpy((char *)malloc(2),s,2);
-	}
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        *s = c;
+        // The block algorithm is copied from evalfunction.c
+        hash = OatHash(s);
+        box =
+            (int) (0.5 + period * hash / (double) SPLAY_PSEUDO_RANDOM_CONSTANT);
+        minblocks = box % HOURLY;
+        // Back to original code
+        if (!boxes[minblocks])
+        {
+            boxes[minblocks] = strncpy((char *) malloc(2), s, 2);
+        }
     }
     printf("    \"ok\" xor => {\n");
-    for (c=0; c<HOURLY; c++) {
-	printf("\tsplayclass(\"%s\",\"hourly\"), # Box %d\n", boxes[c], c);
+    for (c = 0; c < HOURLY; c++)
+    {
+        printf("\tsplayclass(\"%s\",\"hourly\"), # Box %d\n", boxes[c], c);
     }
     printf("\t};\n");
 }
 
 // This is copied from files_hashes.c
 int OatHash(char *key)
-       
-{ unsigned int hashtablesize = SPLAY_PSEUDO_RANDOM_CONSTANT;
-unsigned char *p = key;
-unsigned h = 0;
-int i, len = strlen(key);
-	 
-for ( i = 0; i < len; i++ )
-    {       
-    h += p[i];
-    h += ( h << 10 );
-    h ^= ( h >> 6 );
-    }       
-     
-  h += ( h << 3 );
-  h ^= ( h >> 11 );
-  h += ( h << 15 );
-     
-  return (h & (hashtablesize-1));
-}  
+{
+    unsigned int hashtablesize = SPLAY_PSEUDO_RANDOM_CONSTANT;
+    unsigned char *p = key;
+    unsigned h = 0;
+    int i, len = strlen(key);
 
+    for (i = 0; i < len; i++)
+    {
+        h += p[i];
+        h += (h << 10);
+        h ^= (h >> 6);
+    }
+
+    h += (h << 3);
+    h ^= (h >> 11);
+    h += (h << 15);
+
+    return (h & (hashtablesize - 1));
+}

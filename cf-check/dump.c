021489ecdae0bf6bb495ead4afad17e16d786b1c
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-09-12 14:04:41 +0200

    dump.c: Cleaned up code, function nammes, static, etc.
    Changelog: None
    Ticket: CFE-2989
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- cf-check/dump.c
+++ cf-check/dump.c
@@ -10,11 +10,11 @@
 
 typedef enum
 {
-    DUMP_MODE_NICE, // Print strings in a nice way, and file specific structs
-    DUMP_MODE_PORTABLE, // Portable and unambiguous, structs and raw strings
-    DUMP_MODE_SIMPLE, // Fallback mode for arbitrary / unrecognized data
-    DUMP_MODE_KEYS,
-    DUMP_MODE_VALUES,
+    DUMP_NICE, // Print strings in a nice way, and file specific structs
+    DUMP_PORTABLE, // Portable and unambiguous, structs and raw strings
+    DUMP_SIMPLE, // Fallback mode for arbitrary / unrecognized data
+    DUMP_KEYS,
+    DUMP_VALUES,
 } dump_mode;
 
 static void print_usage(void)
@@ -23,13 +23,13 @@ static void print_usage(void)
     printf("Example: cf-check dump /var/cfengine/state/cf_lastseen.lmdb\n");
 }
 
-static int dump_report_error(int rc)
+static int dump_report_error(const int rc)
 {
     printf("err(%d): %s\n", rc, mdb_strerror(rc));
     return rc;
 }
 
-void dump_print_json_string(
+static void print_json_string(
     const char *const data, size_t size, const bool strip_strings)
 {
     assert(data != NULL);
@@ -47,21 +47,21 @@ void dump_print_json_string(
 
     if (strip_strings)
     {
-        const size_t length = strnlen(data, size);
+        const size_t len = strnlen(data, size);
 
         // Unfortunately, the packages_installed_apt_get.lmdb database
         // has unterminated strings:
-        assert((size == 1) || data[size -1] == '\n' || (length == (size - 1)));
+        assert((size == 1) || (len == (size - 1)) || (data[size - 1] == '\n'));
 
-        // Most of what we store are C strings
-        // except 1 byte '0' or '1', and some structs
-        // So in nice mode, we try to default to printing C strings in a nice way
-        // This means it can be ambiguous (we chop off the NUL byte sometimes)
-        // Use --simple for correct, unambiguous, uglier output
-        if (size > 1 && length == (size - 1))
+        // Most of what we store are C strings except 1 byte '0' or '1',
+        // and some structs. So in nice mode, we try to default to printing
+        // C strings in a nice way. This means it can be ambiguous (we chop
+        // off the NUL byte sometimes). Use --simple for correct, unambiguous,
+        // uglier output.
+        if (size > 1 && len == (size - 1))
         {
             // Looks like a normal string, let's remove NUL byte (nice mode)
-            size = length;
+            size = len;
         }
     }
 
@@ -73,25 +73,14 @@ void dump_print_json_string(
     printf("\"");
 }
 
-void dump_print_opening_bracket(const dump_mode mode)
-{
-    if (mode == DUMP_MODE_VALUES || mode == DUMP_MODE_KEYS)
-    {
-        printf("[\n");
-    }
-    else
-    {
-        printf("{\n");
-    }
-}
-
-void print_lastseen_quality(const MDB_val value, const bool strip_strings)
+static void print_struct_lastseen_quality(
+    const MDB_val value, const bool strip_strings)
 {
     assert(sizeof(KeyHostSeen) == value.mv_size);
     if (sizeof(KeyHostSeen) != value.mv_size)
     {
         // Fall back to simple printing in release builds:
-        dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+        print_json_string(value.mv_data, value.mv_size, strip_strings);
     }
     else
     {
@@ -117,13 +106,14 @@ void print_lastseen_quality(const MDB_val value, const bool strip_strings)
     }
 }
 
-void print_lock_data(const MDB_val value, const bool strip_strings)
+static void print_struct_lock_data(
+    const MDB_val value, const bool strip_strings)
 {
     assert(sizeof(LockData) == value.mv_size);
     if (sizeof(LockData) != value.mv_size)
     {
         // Fall back to simple printing in release builds:
-        dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+        print_json_string(value.mv_data, value.mv_size, strip_strings);
     }
     else
     {
@@ -146,13 +136,14 @@ void print_lock_data(const MDB_val value, const bool strip_strings)
 }
 
 // Used to print values in /var/cfengine/state/cf_observations.lmdb:
-void print_averages(const MDB_val value, const bool strip_strings)
+static void print_struct_averages(
+    const MDB_val value, const bool strip_strings)
 {
     assert(sizeof(Averages) == value.mv_size);
     if (sizeof(Averages) != value.mv_size)
     {
         // Fall back to simple printing in release builds:
-        dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+        print_json_string(value.mv_data, value.mv_size, strip_strings);
     }
     else
     {
@@ -187,7 +178,8 @@ void print_averages(const MDB_val value, const bool strip_strings)
     }
 }
 
-void print_persistent_class(const MDB_val value, const bool strip_strings)
+static void print_struct_persistent_class(
+    const MDB_val value, const bool strip_strings)
 {
     // Value from db should always be bigger than sizeof struct
     // Since tags is not counted in sizeof. `tags` is variable size,
@@ -196,7 +188,7 @@ void print_persistent_class(const MDB_val value, const bool strip_strings)
     if (value.mv_size <= sizeof(PersistentClassInfo))
     {
         // Fall back to simple printing in release builds:
-        dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+        print_json_string(value.mv_data, value.mv_size, strip_strings);
     }
     else
     {
@@ -233,7 +225,7 @@ void print_persistent_class(const MDB_val value, const bool strip_strings)
         {
             // String is not terminated, abort or fall back to default:
             debug_abort_if_reached();
-            dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+            print_json_string(value.mv_data, value.mv_size, strip_strings);
             return;
         }
 
@@ -251,7 +243,7 @@ void print_persistent_class(const MDB_val value, const bool strip_strings)
     }
 }
 
-void dump_print_value(
+static void print_struct_or_string(
     const MDB_val key,
     const MDB_val value,
     const char *const file,
@@ -264,11 +256,11 @@ void dump_print_value(
         if (StringEndsWith(file, "cf_lastseen.lmdb")
             && StringStartsWith(key.mv_data, "q"))
         {
-            print_lastseen_quality(value, strip_strings);
+            print_struct_lastseen_quality(value, strip_strings);
         }
         else if (StringEndsWith(file, "cf_lock.lmdb"))
         {
-            print_lock_data(value, strip_strings);
+            print_struct_lock_data(value, strip_strings);
         }
         else if (StringEndsWith(file, "cf_observations.lmdb"))
         {
@@ -280,16 +272,16 @@ void dump_print_value(
             }
             else
             {
-                print_averages(value, strip_strings);
+                print_struct_averages(value, strip_strings);
             }
         }
         else if (StringEndsWith(file, "history.lmdb"))
         {
-            print_averages(value, strip_strings);
+            print_struct_averages(value, strip_strings);
         }
         else if (StringEndsWith(file, "cf_state.lmdb"))
         {
-            print_persistent_class(value, strip_strings);
+            print_struct_persistent_class(value, strip_strings);
         }
         else
         {
@@ -300,11 +292,11 @@ void dump_print_value(
     const bool was_printed = (structs && !fallback);
     if (!was_printed)
     {
-        dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+        print_json_string(value.mv_data, value.mv_size, strip_strings);
     }
 }
 
-void dump_print_object_line(
+static void print_line_key_value(
     const MDB_val key,
     const MDB_val value,
     const char *const file,
@@ -312,22 +304,35 @@ void dump_print_object_line(
     const bool structs)
 {
     printf("\t");
-    dump_print_json_string(key.mv_data, key.mv_size, strip_strings);
+    print_json_string(key.mv_data, key.mv_size, strip_strings);
     printf(": ");
-    dump_print_value(key, value, file, strip_strings, structs);
+    print_struct_or_string(key, value, file, strip_strings, structs);
     printf(",\n");
 }
 
-void dump_print_array_line(const MDB_val value, const bool strip_strings)
+static void print_line_array_element(
+    const MDB_val value, const bool strip_strings)
 {
     printf("\t");
-    dump_print_json_string(value.mv_data, value.mv_size, strip_strings);
+    print_json_string(value.mv_data, value.mv_size, strip_strings);
     printf(",\n");
 }
 
-void dump_print_closing_bracket(const dump_mode mode)
+static void print_opening_bracket(const dump_mode mode)
 {
-    if (mode == DUMP_MODE_KEYS || mode == DUMP_MODE_VALUES)
+    if (mode == DUMP_VALUES || mode == DUMP_KEYS)
+    {
+        printf("[\n");
+    }
+    else
+    {
+        printf("{\n");
+    }
+}
+
+static void print_closing_bracket(const dump_mode mode)
+{
+    if (mode == DUMP_KEYS || mode == DUMP_VALUES)
     {
         printf("]\n");
     }
@@ -337,13 +342,13 @@ void dump_print_closing_bracket(const dump_mode mode)
     }
 }
 
-int dump_db(const char *file, const dump_mode mode)
+static int dump_db(const char *file, const dump_mode mode)
 {
     assert(file != NULL);
 
-    const bool strip_strings = ((mode == DUMP_MODE_NICE) ? true : false);
+    const bool strip_strings = ((mode == DUMP_NICE) ? true : false);
     const bool structs =
-        ((mode == DUMP_MODE_NICE || mode == DUMP_MODE_PORTABLE) ? true : false);
+        ((mode == DUMP_NICE || mode == DUMP_PORTABLE) ? true : false);
 
     int r;
     MDB_env *env;
@@ -361,28 +366,27 @@ int dump_db(const char *file, const dump_mode mode)
     }
 
     MDB_val key, value;
-    dump_print_opening_bracket(mode);
+    print_opening_bracket(mode);
     while ((r = mdb_cursor_get(cursor, &key, &value, MDB_NEXT)) == MDB_SUCCESS)
     {
         switch (mode) {
-            case DUMP_MODE_NICE:
-            case DUMP_MODE_PORTABLE:
-            case DUMP_MODE_SIMPLE:
-                dump_print_object_line(key, value, file, strip_strings, structs);
+            case DUMP_NICE:
+            case DUMP_PORTABLE:
+            case DUMP_SIMPLE:
+                print_line_key_value(key, value, file, strip_strings, structs);
                 break;
-            case DUMP_MODE_KEYS:
-                dump_print_array_line(key, strip_strings);
+            case DUMP_KEYS:
+                print_line_array_element(key, strip_strings);
                 break;
-            case DUMP_MODE_VALUES:
-                dump_print_array_line(value, strip_strings);
-                break;
+            case DUMP_VALUES:
+                print_line_array_element(value, strip_strings);
                 break;
             default:
                 debug_abort_if_reached();
                 break;
         }
     }
-    dump_print_closing_bracket(mode);
+    print_closing_bracket(mode);
 
     if (r != MDB_NOTFOUND)
     {
@@ -398,7 +402,7 @@ int dump_db(const char *file, const dump_mode mode)
     return 0;
 }
 
-int dump_dbs(Seq *const files, const dump_mode mode)
+static int dump_dbs(Seq *const files, const dump_mode mode)
 {
     assert(files != NULL);
     const size_t length = SeqLength(files);
@@ -454,7 +458,7 @@ int dump_main(int argc, const char *const *const argv)
     assert(argv != NULL);
     assert(argc >= 1);
 
-    dump_mode mode = DUMP_MODE_NICE;
+    dump_mode mode = DUMP_NICE;
     const char *const *filenames = argv + 1;
     size_t filenames_len = argc - 1;
     if (filenames_len > 0 && filenames[0] != NULL && filenames[0][0] == '-')
@@ -466,23 +470,23 @@ int dump_main(int argc, const char *const *const argv)
 
         if (matches_option(option, "--keys", "-k"))
         {
-            mode = DUMP_MODE_KEYS;
+            mode = DUMP_KEYS;
         }
         else if (matches_option(option, "--values", "-v"))
         {
-            mode = DUMP_MODE_VALUES;
+            mode = DUMP_VALUES;
         }
         else if (matches_option(option, "--nice", "-n"))
         {
-            mode = DUMP_MODE_NICE;
+            mode = DUMP_NICE;
         }
         else if (matches_option(option, "--simple", "-s"))
         {
-            mode = DUMP_MODE_SIMPLE;
+            mode = DUMP_SIMPLE;
         }
         else if (matches_option(option, "--portable", "-p"))
         {
-            mode = DUMP_MODE_PORTABLE;
+            mode = DUMP_PORTABLE;
         }
         else
         {

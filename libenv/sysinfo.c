3c0695797cf3763387495f3896a651d84ed6b034
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-09-12 20:18:01 +0200

    redhat_8 and redhat_8_0 are now defined on RHEL 8
    The format of the /etc/redhat-release file was changed:
    
    ```
    Red Hat Linux release 6.2 (Zoot)
    Red Hat Enterprise Linux release 8.0 (Ootpa)
    ```
    
    (Our code didn't expect the Enterprise part).
    
    **Before:**
    
    ```
    [ec2-user@ip-172-31-38-120 core]$ sudo /var/cfengine/bin/cf-promises --show-classes | grep redhat
    inventory_redhat_have_python_symlink                         source=promise
    redhat                                                       inventory,attribute_name=none,source=agent,hardclass
    redhat_pure                                                  source=promise,inventory,attribute_name=none
    ```
    
    **After:**
    
    ```
    [ec2-user@ip-172-31-38-120 core]$ /var/cfengine/bin/cf-promises --show-classes | grep redhat
    inventory_redhat_have_python_symlink                         source=promise
    redhat                                                       inventory,attribute_name=none,source=agent,derived-from-file=/etc/redhat-release,hardclass
    redhat_8                                                     inventory,attribute_name=none,source=agent,derived-from-file=/etc/redhat-release,hardclass
    redhat_8_0                                                   inventory,attribute_name=none,source=agent,derived-from-file=/etc/redhat-release,hardclass
    redhat_pure                                                  source=promise,inventory,attribute_name=none
    ```
    
    Changelog: Title
    Ticket: CFE-3140
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>
    (cherry picked from commit 60b1c0d8552d05370b1046e9113f1af1d43cbe5a)

--- libenv/sysinfo.c
+++ libenv/sysinfo.c
@@ -1724,6 +1724,7 @@ static int Linux_Fedora_Version(EvalContext *ctx)
 static int Linux_Redhat_Version(EvalContext *ctx)
 {
 #define REDHAT_ID "Red Hat Linux"
+#define REDHAT_ENT_ID "Red Hat Enterprise Linux"
 #define REDHAT_AS_ID "Red Hat Enterprise Linux AS"
 #define REDHAT_AS21_ID "Red Hat Linux Advanced Server"
 #define REDHAT_ES_ID "Red Hat Enterprise Linux ES"
@@ -1745,6 +1746,7 @@ static int Linux_Redhat_Version(EvalContext *ctx)
 /* We are looking for one of the following strings...
  *
  * Red Hat Linux release 6.2 (Zoot)
+ * Red Hat Enterprise Linux release 8.0 (Ootpa)
  * Red Hat Linux Advanced Server release 2.1AS (Pensacola)
  * Red Hat Enterprise Linux AS release 3 (Taroon)
  * Red Hat Enterprise Linux WS release 3 (Taroon)
@@ -1812,6 +1814,10 @@ static int Linux_Redhat_Version(EvalContext *ctx)
         edition = "cn";
     }
     else if (!strncmp(relstring, REDHAT_ID, strlen(REDHAT_ID)))
+    {
+        vendor = "redhat";
+    }
+    else if (!strncmp(relstring, REDHAT_ENT_ID, strlen(REDHAT_ENT_ID)))
     {
         vendor = "redhat";
     }

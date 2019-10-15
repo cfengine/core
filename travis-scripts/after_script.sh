e06758de49bd8f46497ba89c01f844389fbe4feb
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-10-15 14:14:33 +0200

    upload artifacts to file.io

--- travis-scripts/after_script.sh
+++ travis-scripts/after_script.sh
@@ -6,3 +6,10 @@ gzip config.log  &&  mv config.log.gz artifacts/
 gzip tests/acceptance/summary.log  &&  mv tests/acceptance/summary.log.gz artifacts/acceptance_summary.log.gz
 gzip tests/acceptance/test.log     &&  mv tests/acceptance/test.log.gz    artifacts/acceptance.log.gz
 zip -r artifacts/acceptance_workdir.zip tests/acceptance/workdir
+
+for filename in artifacts/*; do
+    echo ===== uploading $filename to file.io =====
+    curl -F "file=@$filename" https://file.io
+    echo 'Note that file.io DELETES file from their servers after first download,'
+    echo "so don't delete it from your machine if you still need it!"
+done

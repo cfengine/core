2660fc47bc0a925ce3e59f46f7a49cf3d64a8503
Karl Hole Totland <karl.totland@northern.tech>
2019-11-01 13:06:22 +0100

    CFE-3087: Added Get and StatGet for protocol API
    Receives a file with the request `GET <block_size> <remote_path>`.
    The StatGet variant first remotely gets statistics for the file and
    uses that, while the normal variant requires size to be specified.
    
    Ticket: CFE-3087
    Changelog: None

--- libcfnet/protocol.c
+++ libcfnet/protocol.c
@@ -28,6 +28,7 @@
 #include <client_protocol.h>
 #include <definitions.h>
 #include <net.h>
+#include <stat_cache.h>
 #include <string_lib.h>
 #include <tls_generic.h>
 
@@ -87,4 +88,123 @@ Seq *ProtocolOpenDir(AgentConnection *conn, const char *path)
     }
 
     return seq;
+}
+
+bool ProtocolGet(AgentConnection *conn, const char *remote_path,
+                 const char *local_path, const uint32_t file_size, int perms)
+{
+    assert(conn != NULL);
+    assert(remote_path != NULL);
+    assert(local_path != NULL);
+    assert(file_size != 0);
+
+    perms = (perms == 0) ? CF_PERMS_DEFAULT : perms;
+
+    unlink(local_path);
+    FILE *file_ptr = safe_fopen_create_perms(local_path, "wx", perms);
+    if (file_ptr == NULL)
+    {
+        Log(LOG_LEVEL_WARNING, "Failed to open file %s (fopen: %s)",
+            local_path, GetErrorStr());
+        return false;
+    }
+
+    char buf[CF_MSGSIZE] = {0};
+    int to_send = snprintf(buf, CF_MSGSIZE, "GET %d %s",
+                           CF_MSGSIZE, remote_path);
+
+
+    int ret = SendTransaction(conn->conn_info, buf, to_send, CF_DONE);
+    if (ret == -1)
+    {
+        Log(LOG_LEVEL_WARNING, "Failed to send request for remote file %s:%s",
+            conn->this_server, remote_path);
+        unlink(local_path);
+        fclose(file_ptr);
+        return false;
+    }
+
+    char cfchangedstr[sizeof(CF_CHANGEDSTR1 CF_CHANGEDSTR2)];
+    snprintf(cfchangedstr, sizeof(cfchangedstr), "%s%s",
+             CF_CHANGEDSTR1, CF_CHANGEDSTR2);
+
+    bool success = true;
+    uint32_t received_bytes = 0;
+    while (received_bytes < file_size)
+    {
+        int len = TLSRecv(conn->conn_info->ssl, buf, CF_MSGSIZE);
+        if (len == -1)
+        {
+            Log(LOG_LEVEL_WARNING, "Failed to GET file %s:%s",
+                conn->this_server, remote_path);
+            success = false;
+            break;
+        }
+        else if (len > CF_MSGSIZE)
+        {
+            Log(LOG_LEVEL_WARNING,
+                "Incorrect length of incoming packet "
+                "while retrieving %s:%s, %d > %d",
+                conn->this_server, remote_path, len, CF_MSGSIZE);
+            success = false;
+            break;
+        }
+
+        if (BadProtoReply(buf))
+        {
+            Log(LOG_LEVEL_ERR,
+                "Error from server while retrieving file %s:%s: %s",
+                conn->this_server, remote_path, buf);
+            success = false;
+            break;
+        }
+
+        if (StringSafeEqualN(buf, cfchangedstr, sizeof(cfchangedstr) - 1))
+        {
+            Log(LOG_LEVEL_ERR,
+                "Remote file %s:%s changed during file transfer",
+                conn->this_server, remote_path);
+            success = false;
+            break;
+        }
+
+        ret = fwrite(buf, sizeof(char), len, file_ptr);
+        if (ret < 0)
+        {
+            Log(LOG_LEVEL_ERR,
+                "Failed to write during retrieval of file %s:%s (fwrite: %s)",
+                conn->this_server, remote_path, GetErrorStr());
+            success = false;
+            break;
+        }
+
+        received_bytes += len;
+    }
+
+    if (!success)
+    {
+        unlink(local_path);
+    }
+
+    fclose(file_ptr);
+    return success;
+}
+
+bool ProtocolStatGet(AgentConnection *conn, const char *remote_path,
+                     const char *local_path, int perms)
+{
+    assert(conn != NULL);
+    assert(remote_path != NULL);
+
+    struct stat sb;
+    int ret = cf_remote_stat(conn, false, remote_path, &sb, "file");
+    if (ret == -1)
+    {
+        Log(LOG_LEVEL_ERR,
+            "%s: Failed to stat remote file %s:%s",
+            __func__, conn->this_server, remote_path);
+        return false;
+    }
+
+    return ProtocolGet(conn, remote_path, local_path, sb.st_size, perms);
 }

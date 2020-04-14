f11a34928608c98b08e5f93d341b59d0d65b4353
Craig Comstock <craig.comstock@northern.tech>
2020-04-14 12:20:57 -0500

    reverted my first hack

--- libpromises/fncall.c
+++ libpromises/fncall.c
@@ -364,7 +364,7 @@ if (expargs->next != NULL)
   Log(LOG_LEVEL_WARNING, "CRAIG expargs->next->next is %p", expargs->next->next);
   Log(LOG_LEVEL_WARNING, "CRAIG RlistIsUnresolved(expargs->next->next) is %d", RlistIsUnresolved(expargs->next->next));
 }
-#if 0
+#if 1
         if (strcmp(fp->name, "ifelse") == 0 &&
             RlistLen(expargs) == 3 &&
             strcmp("!any", RlistScalarValueSafe(expargs)) == 0 &&

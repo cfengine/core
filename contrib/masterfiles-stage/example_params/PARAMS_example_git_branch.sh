22bafac9878db77959d11f9aa120b4b210738cc7
Nick Anderson <nick@cmdln.org>
2020-04-16 11:13:04 -0500

    Updated masterfiles-stage integration and usage docs

--- contrib/masterfiles-stage/example_params/PARAMS_example_git_branch.sh
+++ contrib/masterfiles-stage/example_params/PARAMS_example_git_branch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-ROOT="/tmp/masterfiles_staging"
-GIT_URL="https://gitlab.com/nickanderson/masterfiles_demo_3.7.git"
-GIT_REFSPEC="master"
-# GIT_REFSPEC can be a git branch, tag, or commit hash.
-# It will be staged correctly regardless.
+ROOT="/opt/cfengine/masterfiles_staging"
+GIT_URL="https://gitlab.com/nickanderson/masterfiles-vcs"
+GIT_REFSPEC="3.15.x"
+GIT_USERNAME=""
+GIT_PASSWORD=""
 GIT_WORKING_BRANCH="CF_WORKING_BRANCH"
+PKEY=""
+SCRIPT_DIR="/var/cfengine/httpd/htdocs/api/dc-scripts"
 VCS_TYPE="GIT"
-export PATH="${PATH}:/var/cfengine/bin"
+
+export PATH="/var/cfengine/bin:${PATH}"
+export PKEY

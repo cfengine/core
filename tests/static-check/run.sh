e8f229b0b77b087d34cf564b07685e87bdf053cc
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2020-08-05 08:58:22 +0200

    Add a simple way to run static checks on the sources
    Right now just compiling with new GCC and Clang with '-WError'
    and running cppcheck, all in their very recent versions available
    on Fedora 32.
    
    Ticket: ENT-6043
    Changelog: None

---
+++ tests/static-check/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+trap "echo FAILURE" ERR
+
+set -x
+BASE_IMG="fedora:32"
+
+function create_image() {
+  local c=$(buildah from -q $BASE_IMG)
+  buildah run $c -- dnf -q -y install "@C Development Tools and Libraries" clang cppcheck which >/dev/null 2>&1
+  buildah run $c -- dnf -q -y install pcre-devel openssl-devel libxml2-devel pam-devel lmdb-devel libacl-devel libyaml-devel curl-devel libvirt-devel >/dev/null 2>&1
+  buildah run $c -- dnf clean all >/dev/null 2>&1
+  buildah commit $c cfengine-static-checker >/dev/null 2>&1
+  echo $c
+}
+
+# TODO: check how old the image is and recreate if it's too old
+if buildah inspect cfengine-static-checker >/dev/null 2>&1; then
+  c=$(buildah from cfengine-static-checker)
+else
+  c=$(create_image)
+fi
+trap "buildah rm $c >/dev/null" EXIT
+
+buildah copy $c "$(dirname $0)/../../" /tmp/core/ >/dev/null 2>&1
+buildah run $c /tmp/core/tests/static-check/run_checks.sh

5ba35d1579d63731813ce732d16076c8e5a50033
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2020-08-05 08:58:22 +0200

    Add a simple way to run static checks on the sources
    Right now just compiling with new GCC and Clang with '-WError'
    and running cppcheck, all in their very recent versions available
    on Fedora 32.
    
    Ticket: ENT-6043
    Changelog: None

---
+++ tests/static-check/run_checks.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x
+
+n_procs="$(getconf _NPROCESSORS_ONLN)"
+
+function check_with_gcc() {
+  rm -f config.cache
+  make clean
+  ./configure -C --enable-debug CC=gcc
+  make -j -l${n_procs} --keep-going CFLAGS="-Wno-format-truncation -Werror"
+}
+
+function check_with_clang() {
+  rm -f config.cache
+  make clean
+  ./configure -C --enable-debug CC=clang
+  # no-pointer-sign => CFE-3389
+  make -j -l${n_procs} --keep-going CFLAGS="-Wno-pointer-sign -Werror"
+}
+
+function check_with_cppcheck() {
+  rm -f config.cache
+  ./configure -C --enable-debug
+
+  # cppcheck options:
+  #   -I -- include paths
+  #   -i -- ignored files/folders
+  # Identified issues are printed to stderr
+  cppcheck --quiet -j${n_procs} --error-exitcode=1 ./ \
+           --suppressions-list=tests/static-check/cppcheck_suppressions.txt \
+           -I cf-serverd/ -I libpromises/ -I libcfnet/ -I libntech/libutils/ \
+           -i 3rdparty -i .lgtm -i libntech/.lgtm -i tests -i libpromises/cf3lex.c \
+           2>&1 1>/dev/null
+}
+
+cd "$(dirname $0)"/../../
+
+failure=0
+check_with_gcc              || { echo "FAIL: GCC check failed"; failure=1; }
+check_with_clang            || { echo "FAIL: Clang check failed"; failure=1; }
+check_with_cppcheck         || { echo "FAIL: cppcheck failed"; failure=1; }
+
+exit $failure

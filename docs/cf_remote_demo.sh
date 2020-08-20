6c4a58a7ef0c9cbbbcefee9ca4e7f76719f0e243
Ole Herman Schumacher Elgesem <ole@northern.tech>
2020-08-20 17:31:44 +0200

    Added demo script for testing CFEngine on a group of hosts
    I use this quite often when I want to test or demo something
    on a real setup.
    
    Changelog: None
    Ticket: None
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

---
+++ docs/cf_remote_demo.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cf-remote spawn --platform ubuntu-18-04-x64 --count 1 --name hub --role hub
+cf-remote spawn --platform ubuntu-18-04-x64 --count 1 --name ubuntu18 --role client
+cf-remote spawn --platform ubuntu-16-04-x64 --count 1 --name ubuntu16 --role client
+cf-remote spawn --platform ubuntu-14-04-x64 --count 1 --name ubuntu14 --role client
+cf-remote spawn --platform centos-6-x64 --count 1 --name centos6 --role client
+cf-remote spawn --platform centos-7-x64 --count 1 --name centos7 --role client
+cf-remote spawn --platform debian-7-x64 --count 1 --name debian7 --role client
+cf-remote spawn --platform debian-8-x64 --count 1 --name debian8 --role client
+cf-remote spawn --platform debian-9-x64 --count 1 --name debian9 --role client
+cf-remote spawn --platform rhel-5-x64 --count 1 --name rhel5 --role client
+cf-remote spawn --platform rhel-6-x64 --count 1 --name rhel6 --role client
+cf-remote spawn --platform rhel-7-x64 --count 1 --name rhel7 --role client
+
+cf-remote --version master install --demo --bootstrap hub --hub hub
+
+cf-remote deploy --hub hub "$SCRIPTPATH/../../masterfiles"
+
+cf-remote --version master install --demo --bootstrap hub --clients ubuntu18
+cf-remote --version master install --demo --bootstrap hub --clients ubuntu16
+cf-remote --version master install --demo --bootstrap hub --clients ubuntu14
+cf-remote --version master install --demo --bootstrap hub --clients centos6
+cf-remote --version master install --demo --bootstrap hub --clients centos7
+cf-remote --version master install --demo --bootstrap hub --clients debian7
+cf-remote --version master install --demo --bootstrap hub --clients debian8
+cf-remote --version master install --demo --bootstrap hub --clients debian9
+cf-remote --version master install --demo --bootstrap hub --clients rhel5
+cf-remote --version master install --demo --bootstrap hub --clients rhel6
+cf-remote --version master install --demo --bootstrap hub --clients rhel7
+
+cf-remote sudo -H centos6 "service iptables stop"
+cf-remote sudo -H rhel5 "/sbin/service iptables stop"
+
+cf-remote info -H hub

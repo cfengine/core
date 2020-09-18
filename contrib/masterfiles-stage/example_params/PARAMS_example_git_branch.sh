e91d0820195d37604a51bdb84966aea91a4d0c62
Craig Comstock <craig.comstock@northern.tech>
2020-09-18 15:41:44 -0500

    Updated contrib/masterfiles-stage scripts and instructions to be accurate
    This contrib dir had lagged behind masterfiles and enterprise versions.
    
    Ticket: ENT-6165
    Changelog: title

--- contrib/masterfiles-stage/example_params/PARAMS_example_git_branch.sh
+++ contrib/masterfiles-stage/example_params/PARAMS_example_git_branch.sh
@@ -5,9 +5,13 @@ GIT_REFSPEC="3.15.x"
 GIT_USERNAME=""
 GIT_PASSWORD=""
 GIT_WORKING_BRANCH="CF_WORKING_BRANCH"
-PKEY=""
+PKEY="/opt/cfengine/userworkdir/admin/.ssh/id_rsa.pvt"
 SCRIPT_DIR="/var/cfengine/httpd/htdocs/api/dc-scripts"
 VCS_TYPE="GIT"
 
 export PATH="/var/cfengine/bin:${PATH}"
 export PKEY
+export GIT_USERNAME
+export GIT_PASSWORD
+export GIT_SSH="${SCRIPT_DIR}/ssh-wrapper.sh"
+export GIT_ASKPASS="${SCRIPT_DIR}/git-askpass.sh"

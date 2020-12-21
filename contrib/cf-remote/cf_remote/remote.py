cfengine/core

commit 5af180d957087416658c003e532c236e7e8e8e0f

cf-remote: Added better error message when a release is empty
Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

contrib/cf-remote/cf_remote/remote.py
@@ -232,6 +232,10 @@ def _package_from_releases(tags, extension, version, edition, remote_download):
     if version:
         release = releases.pick_version(version)
 
+    if not release.artifacts:
+        log.error(f"The {version} {edition} release is empty, visit tracker.mender.io to file a bug report")
+        return None
+
     artifacts = release.find(tags, extension)
     if not artifacts:
         log.error(

commit 293c977daa14005a6f7d8cb76371e02e0c612b59

cf-remote: Fixed bug with package selection
Introduced in:
5af180d957087416658c003e532c236e7e8e8e0f

Changelog: None
Ticket: ENT-6633
Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

contrib/cf-remote/cf_remote/remote.py
@@ -232,6 +232,8 @@ def _package_from_releases(tags, extension, version, edition, remote_download):
     if version:
         release = releases.pick_version(version)
 
+    release.init_download()
+
     if not release.artifacts:
         log.error(f"The {version} {edition} release is empty, visit tracker.mender.io to file a bug report")
         return None
5655278028f87e874ba9dfbd028a0eb160fc183a
Craig Comstock <craig.comstock@northern.tech>
2021-01-22 17:10:45 -0600

    Added pypi packaging for cf-remote

--- contrib/cf-remote/setup.py
+++ contrib/cf-remote/setup.py
@@ -4,5 +4,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cf-remote-craig.comstock",
-    version="0.0.1"
+    name="cf-remote",
+    version="0.1.0",
+    author="Northern.tech, Inc.",
+    author_email="contact@northern.tech",
+    description="Tooling to deploy CFEngine (and much more)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cfengine/core/contrib/cf-remote",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "cf-remote = cf_remote:main"
+        ]
+    },
+)

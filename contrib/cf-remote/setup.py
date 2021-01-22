4ba1cfb029786c45d5b0231caf34b5c3ac1fc4bb
Craig Comstock <craig@unreasonablefarm.org>
2021-01-22 15:20:04 -0600

    wip

---
+++ contrib/cf-remote/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cf-remote-craig.comstock",
+    version="0.0.1"

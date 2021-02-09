2aa3fa1f93ec2717534e88c8d037e63a87c1190f
Benoît Peccatte <benoit.peccatte@normation.com>
2021-02-09 17:35:53 +0100

    Add systemd socket activation option

--- libcfnet/server_code.c
+++ libcfnet/server_code.c
@@ -8,6 +8,9 @@
 #include <signals.h>                    // GetSignalPipe
 #include <cleanup.h>                    // DoCleanupAndExit
 #include <ctype.h>                      // isdigit
+#if HAVE_SYSTEMD_SD_DAEMON_H
+#include <systemd/sd-daemon.h>          // sd_listen_fds
+#endif // HAVE_SYSTEMD_SD_DAEMON_H
 
 /* Wait up to a minute for an in-coming connection.
  *
@@ -192,20 +195,32 @@ static int OpenReceiverChannel(char *bind_address)
  */
 int InitServer(size_t queue_size, char *bind_address)
 {
-    int sd = OpenReceiverChannel(bind_address);
+#if HAVE_SYSTEMD_SD_DAEMON_H
+    int n = sd_listen_fds(0);
+    if (n > 1) {
+        Log(LOG_LEVEL_ERR, "Too many file descriptors received from systemd");
+    } else if (n == 1) {
+        // we can check here that we have a socket with sd_is_socket_inet(3)
+        // but why should we limit ourselves
+        return SD_LISTEN_FDS_START;
+    } else 
+#endif // HAVE_SYSTEMD_SD_DAEMON_H
+    {
+        int sd = OpenReceiverChannel(bind_address);
 
-    if (sd == -1)
-    {
-        Log(LOG_LEVEL_ERR, "Unable to start server");
-    }
-    else if (listen(sd, queue_size) == -1)
-    {
-        Log(LOG_LEVEL_ERR, "listen failed. (listen: %s)", GetErrorStr());
-        cf_closesocket(sd);
-    }
-    else
-    {
-        return sd;
+        if (sd == -1)
+        {
+            Log(LOG_LEVEL_ERR, "Unable to start server");
+        }
+        else if (listen(sd, queue_size) == -1)
+        {
+            Log(LOG_LEVEL_ERR, "listen failed. (listen: %s)", GetErrorStr());
+            cf_closesocket(sd);
+        }
+        else
+        {
+            return sd;
+        }
     }
 
     DoCleanupAndExit(EXIT_FAILURE);

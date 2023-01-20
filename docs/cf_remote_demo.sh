463be984a931b55d79417990e59a6dc193ceeb7e
Ole Herman Schumacher Elgesem <ole@northern.tech>
2023-01-20 14:43:25 +0100

    cf_remote_demo.sh: Updated Debian 9 to Debian 10
    Has been out for a while and should be working well.
    
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- docs/cf_remote_demo.sh
+++ docs/cf_remote_demo.sh
@@ -9,7 +9,7 @@ cf-remote spawn --platform ubuntu-18-04-x64 --count 1 --name hub --role hub
 cf-remote spawn --platform ubuntu-18-04-x64 --count 1 --name ubuntu18 --role client
 cf-remote spawn --platform ubuntu-16-04-x64 --count 1 --name ubuntu16 --role client
 cf-remote spawn --platform centos-7-x64 --count 1 --name centos7 --role client
-cf-remote spawn --platform debian-9-x64 --count 1 --name debian9 --role client
+cf-remote spawn --platform debian-10-x64 --count 1 --name debian10 --role client
 cf-remote spawn --platform rhel-7-x64 --count 1 --name rhel7 --role client
 
 cf-remote --version master install --demo --bootstrap hub --hub hub
@@ -19,7 +19,7 @@ cf-remote --version master install --demo --bootstrap hub --hub hub
 cf-remote --version master install --demo --bootstrap hub --clients ubuntu18
 cf-remote --version master install --demo --bootstrap hub --clients ubuntu16
 cf-remote --version master install --demo --bootstrap hub --clients centos7
-cf-remote --version master install --demo --bootstrap hub --clients debian9
+cf-remote --version master install --demo --bootstrap hub --clients debian10
 cf-remote --version master install --demo --bootstrap hub --clients rhel7
 
 cf-remote info -H hub

89cdb345114216f316a28238277f6ea67b0fe431
Alex Miliukov <oleksandr.miliukov@northern.tech>
2023-04-04 14:52:08 +0200

    test(static-check): adjust exclude folders list
    Changelog: None
    Ticket: ENT-9922
    Signed-off-by: Alex Miliukov <oleksandr.miliukov@northern.tech>
    (cherry picked from commit b15e86f87d6bdb56bbec910ea7c3e7456c5fd256)

--- tests/static-check/run_checks.sh
+++ tests/static-check/run_checks.sh
@@ -32,7 +32,7 @@ function check_with_cppcheck() {
            --suppressions-list=tests/static-check/cppcheck_suppressions.txt \
            --include=config.h \
            -I cf-serverd/ -I libpromises/ -I libcfnet/ -I libntech/libutils/ \
-           -i 3rdparty -i .lgtm -i libntech/.lgtm -i tests -i libpromises/cf3lex.c \
+           -i 3rdparty -i .github/codeql -i libntech/.lgtm -i tests -i libpromises/cf3lex.c \
            2>&1 1>/dev/null
 }
 

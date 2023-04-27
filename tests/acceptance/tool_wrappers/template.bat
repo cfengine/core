04626356188d0c971e4518c0070e58534513d88f
Aleksei Shpakovskii <aleksei.shpakovskii@northern.tech>
2023-04-27 13:48:05 +0200

    Amend acceptance tests for testsuite to pass on Windows
    For now, majority of tests are marked as soft-fail (a failure is expected).
    
    Follow-up tasks are created (and linked) to investigate these failures
    one-by-one.
    
    Changelog: none
    
    Ticket: ENT-2664
    (cherry picked from commit 895c3caefad8d15dcc41a8317738b64e1251c1f7)
    
     Conflicts:
    	tests/acceptance/10_files/01_create/cfengine_create_by_default.cf
    	tests/acceptance/30_custom_promise_types/22_what_module_gets.cf
    	tests/acceptance/30_custom_promise_types/22_what_module_gets.cf.expected
    
    These tests are for features not backported to 3.18.x
    
    (cherry picked from commit fd7fc14f9a3e048828d7d3424bb601bf1f63615e)

--- tests/acceptance/tool_wrappers/template.bat
+++ tests/acceptance/tool_wrappers/template.bat
@@ -1,6 +1,5 @@
 @echo off
-if exist c:\mingw\msys\1.0\bin\sh.exe c:\mingw\msys\1.0\bin\sh.exe -c "%*" & goto end
-if exist c:\msys\1.0\bin\sh.exe c:\msys\1.0\bin\sh.exe -c "%*" & goto end
-if exist c:\msys\bin\sh.exe c:\msys\bin\sh.exe -c "%*" & goto end
+if exist c:\msys64\usr\bin\sh.exe c:\msys64\usr\bin\sh.exe -c "%*" & goto end
+if exist d:\a\_temp\msys64\usr\bin\sh.exe d:\a\_temp\msys64\usr\bin\sh.exe -c "%*" & goto end
 
 :end

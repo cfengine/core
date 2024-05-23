fd38001c8ca35185a6dd5e0f98226914dc83a71e
Craig Comstock <craig.comstock@northern.tech>
2024-05-23 11:01:25 -0500

    try to make the test fail and then check the error

--- hello.c
+++ hello.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <Windows.h>
 
 int main()
 {
   printf("Hello world\n");
+
+  const char *filename = "/d/a/core/core/tests/acceptance/workdir/__05_processes_01_matching_process_count_found_cf/cf.events.dll";
+  DWORD fileAttr;
+  fileAttr = GetFileAttributes(filename);
+  DWORD err = GetLastError();
+  printf("err=%d\n", err);
+
+  char msg[256];
+  FormatMessage(
+      FORMAT_MESSAGE_FROM_SYSTEM,
+      NULL,
+      err,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      msg,
+      255,
+      NULL);
+  printf("msg=%s\n", msg);
+  if (fileAttr == INVALID_FILE_ATTRIBUTES)
+  {
+      printf("The file \"%s\" does not exist?\n", filename);
+  }
+
   return 0;
 }

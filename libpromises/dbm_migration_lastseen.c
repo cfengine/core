0321fd8f4b34e6d0785cf2919aba6d876afeb4c1
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-10-23 09:33:52 +0200

    Re-enabled DB migration support for LMDB
    For some reason DB migration for LMDB was removed in 2014 (in commit
    8611970bfa33be7b3cf0724eb684833e08582850). Unfortunately there is no
    mention as to why this was done. I will try to re-enable it.
    
    Ticket: None
    Changelog: Title
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>
    (cherry picked from commit 9703d8e7deaeb8ca33347a5a002c229f8c119788)

--- libpromises/dbm_migration_lastseen.c
+++ libpromises/dbm_migration_lastseen.c
@@ -48,138 +48,19 @@ typedef struct
 
 static bool LastseenMigrationVersion0(DBHandle *db)
 {
-    bool errors = false;
-    DBCursor *cursor;
-
-    if (!NewDBCursor(db, &cursor))
-    {
-        return false;
-    }
-
-    char *key;
-    void *value;
-    int ksize, vsize;
-
-    while (NextDB(cursor, &key, &ksize, &value, &vsize))
-    {
-        if (ksize == 0)
-        {
-            Log(LOG_LEVEL_INFO, "LastseenMigrationVersion0: Database structure error -- zero-length key.");
-            continue;
-        }
-
-        /* Only look for old [+-]kH -> IP<QPoint> entries */
-        if ((key[0] != '+') && (key[0] != '-'))
-        {
-            /* Warn about completely unexpected keys */
-
-            if ((key[0] != 'q') && (key[0] != 'k') && (key[0] != 'a'))
-            {
-                Log(LOG_LEVEL_INFO, "LastseenMigrationVersion0: Malformed key found '%s'", key);
-            }
-
-            continue;
-        }
-
-        bool incoming = key[0] == '-';
-        const char *hostkey = key + 1;
-
-        /* Only migrate sane data */
-        if (vsize != QPOINT0_OFFSET + sizeof(QPoint0))
-        {
-            Log(LOG_LEVEL_INFO,
-                "LastseenMigrationVersion0: invalid value size for key '%s', entry is deleted",
-                key);
-            DBCursorDeleteEntry(cursor);
-            continue;
-        }
-
-        /* Properly align the data */
-        const char *old_data_address = (const char *) value;
-        QPoint0 old_data_q;
-        memcpy(&old_data_q, (const char *) value + QPOINT0_OFFSET,
-               sizeof(QPoint0));
-
-        char hostkey_key[CF_BUFSIZE];
-        snprintf(hostkey_key, CF_BUFSIZE, "k%s", hostkey);
-
-        if (!WriteDB(db, hostkey_key, old_data_address, strlen(old_data_address) + 1))
-        {
-            Log(LOG_LEVEL_INFO, "Unable to write version 1 lastseen entry for '%s'", key);
-            errors = true;
-            continue;
-        }
-
-        char address_key[CF_BUFSIZE];
-        snprintf(address_key, CF_BUFSIZE, "a%s", old_data_address);
-
-        if (!WriteDB(db, address_key, hostkey, strlen(hostkey) + 1))
-        {
-            Log(LOG_LEVEL_INFO, "Unable to write version 1 reverse lastseen entry for '%s'", key);
-            errors = true;
-            continue;
-        }
-
-        char quality_key[CF_BUFSIZE];
-        snprintf(quality_key, CF_BUFSIZE, "q%c%s", incoming ? 'i' : 'o', hostkey);
-
-        /*
-           Ignore malformed connection quality data
-        */
-
-        if ((!isfinite(old_data_q.q))
-            || (old_data_q.q < 0)
-            || (!isfinite(old_data_q.expect))
-            || (!isfinite(old_data_q.var)))
-        {
-            Log(LOG_LEVEL_INFO, "Ignoring malformed connection quality data for '%s'", key);
-            DBCursorDeleteEntry(cursor);
-            continue;
-        }
-
-        KeyHostSeen data = {
-            .lastseen = (time_t)old_data_q.q,
-            .Q = {
-                /*
-                   Previously .q wasn't stored in database, but was calculated
-                   every time as a difference between previous timestamp and a
-                   new timestamp. Given we don't have this information during
-                   the database upgrade, just assume that last reading is an
-                   average one.
-                */
-                .q = old_data_q.expect,
-                .dq = 0,
-                .expect = old_data_q.expect,
-                .var = old_data_q.var,
-            }
-        };
-
-        if (!WriteDB(db, quality_key, &data, sizeof(data)))
-        {
-            Log(LOG_LEVEL_INFO, "Unable to write version 1 connection quality key for '%s'", key);
-            errors = true;
-            continue;
-        }
-
-        if (!DBCursorDeleteEntry(cursor))
-        {
-            Log(LOG_LEVEL_INFO, "Unable to delete version 0 lastseen entry for '%s'", key);
-            errors = true;
-        }
-    }
-
-    if (DeleteDBCursor(cursor) == false)
-    {
-        Log(LOG_LEVEL_ERR, "LastseenMigrationVersion0: Unable to close cursor");
-        errors = true;
-    }
-
-    if ((!errors) && (!WriteDB(db, "version", "1", sizeof("1"))))
-    {
-        errors = true;
-    }
-
-    return !errors;
+    /* For some reason DB migration for LMDB was disabled in 2014 (in commit
+     * 8611970bfa33be7b3cf0724eb684833e08582850). Unfortunately there is no
+     * mention as to why this was done. Maybe it was not working?
+     *
+     * However, we're re-enabling it now (10 years later). Since this
+     * migration function has not been active for the last 10 years, the
+     * safest thing is to remove the migration logic, and only update the
+     * version number.
+     *
+     * If you have not upgraded CFEngine in the last 10 years, this will be
+     * the least of your problems.
+     */
+    return WriteDB(db, "version", "1", sizeof("1"));
 }
 
 const DBMigrationFunction dbm_migration_plan_lastseen[] =

cfengine/core

commit e2c9f00c0df21dec7d5cb6e4e6850ca1ab1765f2

Use a proper 'bool' variable for a condition in GetNameInfo3()
Ticket: ENT-6043
Changelog: None

libenv/sysinfo.c
@@ -419,7 +419,7 @@ void DiscoverVersion(EvalContext *ctx)
 
 static void GetNameInfo3(EvalContext *ctx)
 {
-    int i, found = false;
+    int i;
     char *sp, workbuf[CF_BUFSIZE];
     time_t tloc;
     struct hostent *hp;
@@ -518,7 +518,8 @@ static void GetNameInfo3(EvalContext *ctx)
         }
     }
 
-    for (i = 0; i < PLATFORM_CONTEXT_MAX; i++)
+    bool found = false;
+    for (i = 0; !found && (i < PLATFORM_CONTEXT_MAX); i++)
     {
         char sysname[CF_BUFSIZE];
         strlcpy(sysname, VSYSNAME.sysname, CF_BUFSIZE);
@@ -541,7 +542,6 @@ static void GetNameInfo3(EvalContext *ctx)
                     VSYSTEMHARDCLASS = (PlatformContext) i;
                     VPSHARDCLASS = (PlatformContext) i; /* this one can be overriden at vz detection */
                     EvalContextVariablePutSpecial(ctx, SPECIAL_SCOPE_SYS, "class", CLASSTEXT[i], CF_DATA_TYPE_STRING, "inventory,source=agent,attribute_name=OS type");
-                    break;
                 }
             }
             else

commit 22ad5d6418db960f5b830c35544ce67c57bb2d28

libenv/sysinfo: Fix detected os description
Use platform context variable also for logging the os class description.
Before this change, it were using loop counter variable, which is
incremented again. This led to verbose logging message:
 verbose: CFEngine detected operating system description is qnx
on Darwin/macOS.

Also use already lowercased sysname from utsname struct.

Fixes: e2c9f00c0 ("Use a proper 'bool' variable for a condition in GetNameInfo3()")

libenv/sysinfo.c
@@ -549,13 +549,9 @@ static void GetNameInfo3(EvalContext *ctx)
     bool found = false;
     for (i = 0; !found && (i < PLATFORM_CONTEXT_MAX); i++)
     {
-        char sysname[CF_BUFSIZE];
-        strlcpy(sysname, VSYSNAME.sysname, CF_BUFSIZE);
-        ToLowerStrInplace(sysname);
-
         /* FIXME: review those strcmps. Moved out from StringMatch */
-        if (!strcmp(CLASSATTRIBUTES[i][0], sysname)
-            || StringMatchFull(CLASSATTRIBUTES[i][0], sysname))
+        if (!strcmp(CLASSATTRIBUTES[i][0], VSYSNAME.sysname)
+            || StringMatchFull(CLASSATTRIBUTES[i][0], VSYSNAME.sysname))
         {
             if (!strcmp(CLASSATTRIBUTES[i][1], VSYSNAME.machine)
                 || StringMatchFull(CLASSATTRIBUTES[i][1], VSYSNAME.machine))
@@ -580,16 +576,10 @@ static void GetNameInfo3(EvalContext *ctx)
         }
     }
 
-    if (!found)
-    {
-        i = 0;
-    }
-
     Log(LOG_LEVEL_VERBOSE, "%s - ready", NameVersion());
     Banner("Environment discovery");
 
-    snprintf(workbuf, CF_BUFSIZE, "%s", CLASSTEXT[i]); // See: cppcheck_suppressions.txt
-
+    snprintf(workbuf, CF_BUFSIZE, "%s", CLASSTEXT[VSYSTEMHARDCLASS]);
 
     Log(LOG_LEVEL_VERBOSE, "Host name is: %s", VSYSNAME.nodename);
     Log(LOG_LEVEL_VERBOSE, "Operating System Type is %s", VSYSNAME.sysname);
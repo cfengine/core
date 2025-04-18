e28fa7d7273c7ee06e7e6ca542479b9fbfab0a0d
Alexis Mousset <alexis.mousset@rudder.io>
2025-04-18 11:52:50 +0200

    Define suse class on SLED systems
    (cherry picked from commit aadc43cf81d585b53385bd4b50ea5a81124d8159)
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- libenv/sysinfo.c
+++ libenv/sysinfo.c
@@ -1183,7 +1183,8 @@ static void OSReleaseParse(EvalContext *ctx, const char *file_path)
                 alias = "redhat";
             }
             else if (StringEqual(os_release_id, "opensuse") ||
-                     StringEqual(os_release_id, "sles"))
+                     StringEqual(os_release_id, "sles") ||
+                     StringEqual(os_release_id, "sled"))
             {
                 alias = "suse";
             }

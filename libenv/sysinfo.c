cfengine/core

commit 22ad5d6418db960f5b830c35544ce67c57bb2d28

libenv/sysinfo: Fix detected os description
Use platform context variable also for logging the os class description.
Before this change, it were using loop counter variable, which is
incremented again. This led to verbose logging message:
 verbose: CFEngine detected operating system description is qnx
on Darwin/macOS.

Also use already lowercased sysname from utsname struct.

Fixes: e2c9f00c0 ("Use a proper 'bool' variable for a condition in GetNameInfo3()")

libenv/sysinfo.c
@@ -549,13 +549,9 @@ static void GetNameInfo3(EvalContext *ctx)
     bool found = false;
     for (i = 0; !found && (i < PLATFORM_CONTEXT_MAX); i++)
     {
-        char sysname[CF_BUFSIZE];
-        strlcpy(sysname, VSYSNAME.sysname, CF_BUFSIZE);
-        ToLowerStrInplace(sysname);
-
         /* FIXME: review those strcmps. Moved out from StringMatch */
-        if (!strcmp(CLASSATTRIBUTES[i][0], sysname)
-            || StringMatchFull(CLASSATTRIBUTES[i][0], sysname))
+        if (!strcmp(CLASSATTRIBUTES[i][0], VSYSNAME.sysname)
+            || StringMatchFull(CLASSATTRIBUTES[i][0], VSYSNAME.sysname))
         {
             if (!strcmp(CLASSATTRIBUTES[i][1], VSYSNAME.machine)
                 || StringMatchFull(CLASSATTRIBUTES[i][1], VSYSNAME.machine))
@@ -580,16 +576,10 @@ static void GetNameInfo3(EvalContext *ctx)
         }
     }
 
-    if (!found)
-    {
-        i = 0;
-    }
-
     Log(LOG_LEVEL_VERBOSE, "%s - ready", NameVersion());
     Banner("Environment discovery");
 
-    snprintf(workbuf, CF_BUFSIZE, "%s", CLASSTEXT[i]); // See: cppcheck_suppressions.txt
-
+    snprintf(workbuf, CF_BUFSIZE, "%s", CLASSTEXT[VSYSTEMHARDCLASS]);
 
     Log(LOG_LEVEL_VERBOSE, "Host name is: %s", VSYSNAME.nodename);
     Log(LOG_LEVEL_VERBOSE, "Operating System Type is %s", VSYSNAME.sysname);

commit 25c49fbcebc7e6cbf1a22e58bb4424fcab4a28e1

Fixed bug in system detection code caused by wrong assumption
In commit 22ad5d6418db960f5b830c35544ce67c57bb2d28 an assumption was
made that the `sysname` attribute of `struct utsname` is always lower
case. This assumption proved to be wrong for Windows Server 2019.

Since the commit also contains other changes, I will do a manual revert.
See https://github.com/cfengine/core/pull/5751 for more information.

Ticket: CFE-4519
Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

libenv/sysinfo.c
@@ -555,9 +555,13 @@ static void GetNameInfo3(EvalContext *ctx)
     bool found = false;
     for (i = 0; !found && (i < PLATFORM_CONTEXT_MAX); i++)
     {
+        char sysname[CF_BUFSIZE];
+        strlcpy(sysname, VSYSNAME.sysname, CF_BUFSIZE);
+        ToLowerStrInplace(sysname);
+
         /* FIXME: review those strcmps. Moved out from StringMatch */
-        if (!strcmp(CLASSATTRIBUTES[i][0], VSYSNAME.sysname)
-            || StringMatchFull(CLASSATTRIBUTES[i][0], VSYSNAME.sysname))
+        if (!strcmp(CLASSATTRIBUTES[i][0], sysname)
+            || StringMatchFull(CLASSATTRIBUTES[i][0], sysname))
         {
             if (!strcmp(CLASSATTRIBUTES[i][1], VSYSNAME.machine)
                 || StringMatchFull(CLASSATTRIBUTES[i][1], VSYSNAME.machine))
7fee0309ec74b54790ce44aeb2970d22f1d8cafb
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-05-27 14:18:14 +0200

    verify_files.c: Removed trailing white space
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cf-agent/verify_files.c
+++ cf-agent/verify_files.c
@@ -742,7 +742,7 @@ static PromiseResult RenderTemplateCFEngine(EvalContext *ctx,
     {
         if (!file_exists && !CfCreateFile(ctx, edcontext->changes_filename,
                                           pp, attr, &result))
-        { 
+        {
             RecordFailure(ctx, pp, attr,
                           "Failed to create file '%s' for rendering cfengine template '%s'",
                           edcontext->filename, attr->edit_template);
@@ -854,7 +854,7 @@ static PromiseResult RenderTemplateMustache(EvalContext *ctx,
                 if (!file_exists && !CfCreateFile(ctx,
                                                   edcontext->changes_filename,
                                                   pp, attr, &result))
-                { 
+                {
                     RecordFailure(ctx, pp, attr,
                                   "Failed to create file '%s' for rendering mustache template '%s'",
                                   edcontext->filename, message);

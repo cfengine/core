1d0259bae2d0bfd6242c0b217d1f60d27cbd8a6c
Nick Anderson <nick@cmdln.org>
2025-08-20 16:48:26 -0500

    Fixed invalid colon separating host and path from cifs and panfs in fstab
    This commit introduces a conditional check to format the device string for CIFS and PanFS without a colon (:), as required by their specific syntax. It also updates the logging to show the full fstab entry that is being added.
    
    Additionally, the log message has been updated to be more descriptive, now displaying the full fstab entry that is being added.
    
    Ticket: CFE-4494
    (cherry picked from commit 582c6c28ba0691169790a9b1b0509cee9917b5a3)

--- cf-agent/nfs.c
+++ cf-agent/nfs.c
@@ -468,29 +468,49 @@ int VerifyInFstab(EvalContext *ctx, char *name, const Attributes *a, const Promi
     mountpt = name;
     fstype = a->mount.mount_type;
 
+    char device[CF_BUFSIZE];
+    if (StringEqual(fstype, "cifs") || StringEqual(fstype, "panfs"))
+    {
+        NDEBUG_UNUSED int ret = snprintf(device, sizeof(device), "%s%s", host, rmountpt);
+        assert(ret >= 0 && ret < CF_BUFSIZE);
+    }
+    else
+    {
+        NDEBUG_UNUSED int ret = snprintf(device, sizeof(device), "%s:%s", host, rmountpt);
+        assert(ret >= 0 && ret < CF_BUFSIZE);
+    }
+
 #if defined(__QNX__) || defined(__QNXNTO__)
-    snprintf(fstab, CF_BUFSIZE, "%s:%s \t %s %s\t%s 0 0", host, rmountpt, mountpt, fstype, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "%s \t %s %s\t%s 0 0", device, mountpt, fstype, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #elif defined(_CRAY)
     char fstype_upper[CF_BUFSIZE];
     strlcpy(fstype_upper, fstype, CF_BUFSIZE);
     ToUpperStrInplace(fstype_upper);
 
-    snprintf(fstab, CF_BUFSIZE, "%s:%s \t %s %s\t%s", host, rmountpt, mountpt, fstype_upper, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "%s \t %s %s\t%s", device, mountpt, fstype_upper, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
     break;
 #elif defined(__hpux)
-    snprintf(fstab, CF_BUFSIZE, "%s:%s %s \t %s \t %s 0 0", host, rmountpt, mountpt, fstype, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "%s %s \t %s \t %s 0 0", device, mountpt, fstype, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #elif defined(_AIX)
-    snprintf(fstab, CF_BUFSIZE,
-             "%s:\n\tdev\t= %s\n\ttype\t= %s\n\tvfs\t= %s\n\tnodename\t= %s\n\tmount\t= true\n\toptions\t= %s\n\taccount\t= false\n",
-             mountpt, rmountpt, fstype, fstype, host, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE,
+                                     "%s:\n\tdev\t= %s\n\ttype\t= %s\n\tvfs\t= %s\n\tnodename\t= %s\n\tmount\t= true\n\toptions\t= %s\n\taccount\t= false\n",
+                                     mountpt, rmountpt, fstype, fstype, host, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #elif defined(__linux__)
-    snprintf(fstab, CF_BUFSIZE, "%s:%s \t %s \t %s \t %s", host, rmountpt, mountpt, fstype, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "%s \t %s \t %s \t %s", device, mountpt, fstype, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #elif defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__APPLE__)
-    snprintf(fstab, CF_BUFSIZE, "%s:%s \t %s \t %s \t %s 0 0", host, rmountpt, mountpt, fstype, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "%s \t %s \t %s \t %s 0 0", device, mountpt, fstype, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #elif defined(__sun) || defined(sco) || defined(__SCO_DS)
-    snprintf(fstab, CF_BUFSIZE, "%s:%s - %s %s - yes %s", host, rmountpt, mountpt, fstype, opts);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "%s - %s %s - yes %s", device, mountpt, fstype, opts);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #elif defined(__CYGWIN__)
-    snprintf(fstab, CF_BUFSIZE, "/bin/mount %s:%s %s", host, rmountpt, mountpt);
+    NDEBUG_UNUSED int ret = snprintf(fstab, CF_BUFSIZE, "/bin/mount %s %s", device, mountpt);
+    assert(ret >= 0 && ret < CF_BUFSIZE);
 #else
   #error "Could not determine format of fstab entry on this platform."
 #endif
@@ -503,7 +523,7 @@ int VerifyInFstab(EvalContext *ctx, char *name, const Attributes *a, const Promi
     {
         AppendItem(&FSTABLIST, fstab, NULL);
         FSTAB_EDITS++;
-        cfPS(ctx, LOG_LEVEL_INFO, PROMISE_RESULT_CHANGE, pp, a, "Adding file system '%s:%s' to '%s'", host, rmountpt,
+        cfPS(ctx, LOG_LEVEL_INFO, PROMISE_RESULT_CHANGE, pp, a, "Adding file system entry '%s' to '%s'", fstab,
              VFSTAB[VSYSTEMHARDCLASS]);
         *result = PromiseResultUpdate(*result, PROMISE_RESULT_CHANGE);
         changes += 1;

f28e7b29e97d5865b2890fb4e5fc41e696d87fa4
Craig Comstock <craig.comstock@northern.tech>
2025-10-10 11:41:30 -0400

    Added Termux Linux Environment (Android) for sys.os_name_human
    We already supported and included the termux class.
    This change removes noise when running components like cf-agent.
    
    Ticket: CFE-4427
    Changelog: title

--- libenv/sysinfo.c
+++ libenv/sysinfo.c
@@ -3809,6 +3809,12 @@ static void SysOSNameHuman(EvalContext *ctx)
                                       "Alpine", CF_DATA_TYPE_STRING,
                                       "source=agent,derived-from=alpine");
     }
+    else if (EvalContextClassGet(ctx, NULL, "termux") != NULL)
+    {
+        EvalContextVariablePutSpecial(ctx, SPECIAL_SCOPE_SYS, lval,
+                                      "Termux", CF_DATA_TYPE_STRING,
+                                      "source=agent,derived-from=termux");
+    }
     else if (EvalContextClassGet(ctx, NULL, "gentoo") != NULL)
     {
         EvalContextVariablePutSpecial(ctx, SPECIAL_SCOPE_SYS, lval,

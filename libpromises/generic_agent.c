9b3d91243fd05aff26b8d3f9737e8fea13e4d94c
Craig Comstock <craig.comstock@northern.tech>
2025-11-22 07:31:05 -0600

    In case of too many args on command line be more clear about what was provided
    We have a case where in Mission Portal the Run Agent button causes a Too many arguments error from Windows agents.
    This change seeks to understand why.
    
    Ticket: ENT-13530
    Changelog: none

--- libpromises/generic_agent.c
+++ libpromises/generic_agent.c
@@ -2476,6 +2476,17 @@ bool GenericAgentConfigParseArguments(GenericAgentConfig *config, int argc, char
 
     if (argc > 1)
     {
+Log(LOG_LEVEL_ERR, "Special 1-arg <policy-file> expected, got %d arguments instead. See debug output for details.", argc);
+for (int i=0; i<argc; i++) {
+char raw[256] = "";
+for(int j=0; j<20; j++) {
+  char chunk[256];
+  snprintf(chunk, 256, "0x%02x ", argv[i][j]);
+  StringAppend(raw, chunk, 256);
+  if (argv[i][j] == 0) break;
+}
+Log(LOG_LEVEL_DEBUG, "Too many args, argv[%d] is '%s' [%s]", i, argv[i], raw);
+}
         return false;
     }
 

312bf2345e143e2d4981afa13015cb4edca1d476
Craig Comstock <craig.comstock@northern.tech>
2026-05-05 15:00:17 -0500

    Adjusted ci/ configure, build and install scripts for termux platform
    Ticket: none
    Changelog: none

--- ci/configure.sh
+++ ci/configure.sh
@@ -6,13 +6,22 @@ thisdir="$(dirname "$0")"
 cd "$thisdir"/..
 OPTS="--enable-debug"
 
-source /etc/os-release
-if [ "$ID" = "alpine" ]; then
-    OPTS="" # we don't want --enable-debug so that libpromises/dbm_test_api is not built due to lack of srand48_r() and friends on alpine linux libmusl
+if [ -f /etc/os-release ]; then
+  source /etc/os-release
+  if [ "$ID" = "alpine" ]; then
+      OPTS="" # we don't want --enable-debug so that libpromises/dbm_test_api is not built due to lack of srand48_r() and friends on alpine linux libmusl
+  fi
 fi
 
 if [ -n "$TERMUX_VERSION" ] || [ "$ID" = "alpine" ]; then
     OPTS="$OPTS --without-pam"
+fi
+if [ -n "$TERMUX_VERSION" ]; then
+    export LDFLAGS+=" -landroid-glob"
+    OPTS="$OPTS --prefix=$PREFIX\
+      --with-workdir=$PREFIX/var/lib/cfengine \
+      --without-selinux-policy \
+      --without-systemd-service"
 fi
 
 ./autogen.sh $OPTS

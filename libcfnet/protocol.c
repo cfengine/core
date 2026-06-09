9b4db99bba823b401aaec6d62c7cd4a7438359b8
aizu-m <aizumusheer2@gmail.com>
2026-06-09 12:15:12 +0530

    fix off-by-one in ProtocolGet and ProtocolOpenDir recv buffers
    (cherry picked from commit 2d5f4ae31db5465fd2c16551eff9c4a0393fcde9)

--- libcfnet/protocol.c
+++ libcfnet/protocol.c
@@ -37,7 +37,7 @@ Seq *ProtocolOpenDir(AgentConnection *conn, const char *path)
     assert(conn != NULL);
     assert(path != NULL);
 
-    char buf[CF_MSGSIZE] = {0};
+    char buf[CF_BUFSIZE] = {0};
     int tosend = snprintf(buf, CF_MSGSIZE, "OPENDIR %s", path);
     if (tosend < 0 || tosend >= CF_MSGSIZE)
     {
@@ -109,7 +109,7 @@ bool ProtocolGet(AgentConnection *conn, const char *remote_path,
         return false;
     }
 
-    char buf[CF_MSGSIZE] = {0};
+    char buf[CF_BUFSIZE] = {0};
     int to_send = snprintf(buf, CF_MSGSIZE, "GET %d %s",
                            CF_MSGSIZE, remote_path);
 

1fa36768f2151f9f9967302eccd1f7850009d6ca
Ole Herman Schumacher Elgesem <ole@northern.tech>
2026-06-24 16:58:57 +0200

    tests/unit/policy_test.c: Manual edits for new line numbers
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- tests/unit/policy_test.c
+++ tests/unit/policy_test.c
@@ -287,13 +287,13 @@ static void test_policy_json_offsets(void)
     {
         JsonElement *main_bundle = JsonArrayGetAsObject(json_bundles, 0);
         int line = JsonPrimitiveGetAsInteger(JsonObjectGet(main_bundle, "line"));
-        assert_int_equal(9, line);
+        assert_int_equal(8, line);
 
         JsonElement *json_promise_types = JsonObjectGetAsArray(main_bundle, "promiseTypes");
         {
             JsonElement *json_reports_type = JsonArrayGetAsObject(json_promise_types, 0);
             line = JsonPrimitiveGetAsInteger(JsonObjectGet(json_reports_type, "line"));
-            assert_int_equal(11, line);
+            assert_int_equal(10, line);
 
             JsonElement *json_contexts = JsonObjectGetAsArray(json_reports_type, "contexts");
             JsonElement *cf_context = JsonArrayGetAsObject(json_contexts, 0);
@@ -301,13 +301,13 @@ static void test_policy_json_offsets(void)
             JsonElement *hello_cf_promise = JsonArrayGetAsObject(cf_context_promises, 0);
 
             line = JsonPrimitiveGetAsInteger(JsonObjectGet(hello_cf_promise, "line"));
-            assert_int_equal(13, line);
+            assert_int_equal(12, line);
             JsonElement *hello_cf_attribs = JsonObjectGetAsArray(hello_cf_promise, "attributes");
             {
                 JsonElement *friend_pattern_attrib = JsonArrayGetAsObject(hello_cf_attribs, 0);
 
                 line = JsonPrimitiveGetAsInteger(JsonObjectGet(friend_pattern_attrib, "line"));
-                assert_int_equal(14, line);
+                assert_int_equal(12, line);
             }
         }
     }
@@ -316,11 +316,11 @@ static void test_policy_json_offsets(void)
     {
         JsonElement *control_body = JsonArrayGetAsObject(json_bodies, 0);
         int line = JsonPrimitiveGetAsInteger(JsonObjectGet(control_body, "line"));
-        assert_int_equal(4, line);
+        assert_int_equal(3, line);
 
         JsonElement *myperms_body = JsonArrayGetAsObject(json_bodies, 1);
         line = JsonPrimitiveGetAsInteger(JsonObjectGet(myperms_body, "line"));
-        assert_int_equal(29, line);
+        assert_int_equal(27, line);
 
         JsonElement *myperms_contexts = JsonObjectGetAsArray(myperms_body, "contexts");
         JsonElement *any_context = JsonArrayGetAsObject(myperms_contexts, 0);
@@ -328,7 +328,7 @@ static void test_policy_json_offsets(void)
         {
             JsonElement *mode_attrib = JsonArrayGetAsObject(any_attribs, 0);
             line = JsonPrimitiveGetAsInteger(JsonObjectGet(mode_attrib, "line"));
-            assert_int_equal(31, line);
+            assert_int_equal(29, line);
         }
     }
 

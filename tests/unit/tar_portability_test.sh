a83bad3aacbceab399e46af3fa277963e0a474d9
Lars Erik Wik <lars.erik.wik@northern.tech>
2026-08-05 17:25:34 +0200

    Made the source tarball reproducible
    Two builds of the same source tree now produce byte-identical tarballs,
    following GNU tar's reproducibility guidance. The same change 72c04dd9
    made to masterfiles.
    
    Changelog: The source tarball is now reproducible
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- tests/unit/tar_portability_test.sh
+++ tests/unit/tar_portability_test.sh
@@ -8,6 +8,8 @@ fi
 
 cd "$(dirname "$0")"/../.. || exit 1
 
-tar --exclude="tests/acceptance/workdir" --format=ustar -cf /dev/null *
+# Clear TAR_OPTIONS: Makefile.am sets it for "make dist", and the --pax-option
+# in there only works on posix archives, not on the ustar one made here.
+TAR_OPTIONS='' tar --exclude="tests/acceptance/workdir" --format=ustar -cf /dev/null *
 
 exit $?
